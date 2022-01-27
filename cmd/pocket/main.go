9f56d05bd9a0b024e6836d40fc2afbf03a051e61
iajrz <axel.rivas@gmail.com>
2022-01-27 15:50:17 -0400

    example implementation of version number injection

--- cmd/pocket/main.go
+++ cmd/pocket/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main(){}
+import (
+	"flag"
+	"fmt"
+)
+
+var version string
+
+func main() {
+	v := flag.Bool("version", false, "")
+	flag.Parse()
+	if *v {
+		fmt.Println(version)
+	}
+}
--- mage.go
+++ mage.go
@@ -8,21 +8,25 @@ import (
 )
 
 const pocketPackage = "github.com/pokt-network/pocket/cmd/pocket"
+const ldflag = "-X main.version=$POCKET_VERSION_STRING"
 
 func init() {
 	os.Setenv("GO111MODULE", "on")
 }
 
 func Build() error {
-	return sh.Run("go", "build", pocketPackage)
+	env := map[string]string{"POCKET_VERSION_STRING": "1.0.0-alpha"}
+	return sh.RunWith(env, "go", "build", "-ldflags", ldflag, pocketPackage)
 }
 
 func BuildRace() error {
-	return sh.Run("go", "build", "-race", pocketPackage)
+	env := map[string]string{"POCKET_VERSION_STRING": "1.0.0-alpha+race"}
+	return sh.RunWith(env, "go", "build", "-ldflags", ldflag, "-race", pocketPackage)
 }
 
 func Install() error {
-	return sh.Run("go", "install", pocketPackage)
+	env := map[string]string{"POCKET_VERSION_STRING": "1.0.0-alpha"}
+	return sh.RunWith(env, "go", "install", "-ldflags", ldflag, pocketPackage)
 }
 
 func Uninstall() error {
