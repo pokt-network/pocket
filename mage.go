20d36b07c7b713a14c2df9bfd4356378c0fad128
iajrz <axel.rivas@gmail.com>
2022-01-28 15:54:25 -0400

    adds version info to show the source for the binary

--- mage.go
+++ mage.go
@@ -3,29 +3,44 @@
 package main
 
 import (
+	"fmt"
 	"github.com/magefile/mage/sh"
-	"os"
 )
 
+const versionStringEnvVarName = "POCKET_VERSION_STRING"
 const pocketPackage = "github.com/pokt-network/pocket/cmd/pocket"
-const ldflag = "-X main.version=$POCKET_VERSION_STRING"
+const ldflag = "-X main.version=$" + versionStringEnvVarName
+
+var env = map[string]string{}
 
 func init() {
-	os.Setenv("GO111MODULE", "on")
+	env["GO111MODULE"] = "on"
+
+	fmt.Println("Getting branch and commit information")
+	branch, _ := sh.Output("git", "branch", "--show-current")
+	hash, _ := sh.Output("git", "rev-parse", "--short", "HEAD")
+	_, dirty := sh.Output("git", "diff", "--quiet")
+
+	if branch != "" {
+		branch = "-" + branch
+		hash = "/" + hash
+	}
+	if dirty.Error() == "running \"git diff --quiet\" failed with exit code 1" {
+		hash += "+dirty"
+	}
+	env[versionStringEnvVarName] = "0.0.0" + branch + hash
 }
 
 func Build() error {
-	env := map[string]string{"POCKET_VERSION_STRING": "1.0.0-alpha"}
 	return sh.RunWith(env, "go", "build", "-ldflags", ldflag, pocketPackage)
 }
 
 func BuildRace() error {
-	env := map[string]string{"POCKET_VERSION_STRING": "1.0.0-alpha+race"}
+	env["versionStringEnvVarName"] += "+race"
 	return sh.RunWith(env, "go", "build", "-ldflags", ldflag, "-race", pocketPackage)
 }
 
 func Install() error {
-	env := map[string]string{"POCKET_VERSION_STRING": "1.0.0-alpha"}
 	return sh.RunWith(env, "go", "install", "-ldflags", ldflag, pocketPackage)
 }
 
