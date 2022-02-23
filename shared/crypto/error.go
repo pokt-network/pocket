510a49b657d72b3ca9c1965ed9b111f0e55e021e
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-02-23 14:13:55 -0800

    Replaced 'errors.New(fmt.Sprintf' with 'fmt.Errorf'

--- shared/crypto/error.go
+++ shared/crypto/error.go
@@ -2,7 +2,6 @@ package crypto
 
 import (
 	"crypto/ed25519"
-	"errors"
 	"fmt"
 )
 
@@ -17,29 +16,29 @@ const (
 )
 
 func ErrInvalidAddressLen() error {
-	return errors.New(fmt.Sprintf("%s, expected length %d", InvalidAddressLenError, AddressLen))
+	return fmt.Errorf("%s, expected length %d", InvalidAddressLenError, AddressLen)
 }
 
 func ErrInvalidHashLen() error {
-	return errors.New(fmt.Sprintf("%s, expected length %d", InvalidHashLenError, SHA3HashLen))
+	return fmt.Errorf("%s, expected length %d", InvalidHashLenError, SHA3HashLen)
 }
 
 func ErrCreateAddress(err error) error {
-	return errors.New(fmt.Sprintf("%s; %s", CreateAddressError, err.Error()))
+	return fmt.Errorf("%s; %s", CreateAddressError, err.Error())
 }
 
 func ErrInvalidPrivateKeyLen() error {
-	return errors.New(fmt.Sprintf("%s, expected length %d", InvalidPrivateKeyLenError, ed25519.PrivateKeySize))
+	return fmt.Errorf("%s, expected length %d", InvalidPrivateKeyLenError, ed25519.PrivateKeySize)
 }
 
 func ErrCreatePrivateKey(err error) error {
-	return errors.New(fmt.Sprintf("%s; %s", CreatePrivateKeyError, err.Error()))
+	return fmt.Errorf("%s; %s", CreatePrivateKeyError, err.Error())
 }
 
 func ErrInvalidPublicKeyLen() error {
-	return errors.New(fmt.Sprintf("%s, expected length %d", InvalidPublicKeyLenError, ed25519.PrivateKeySize))
+	return fmt.Errorf("%s, expected length %d", InvalidPublicKeyLenError, ed25519.PrivateKeySize)
 }
 
 func ErrCreatePublicKey(err error) error {
-	return errors.New(fmt.Sprintf("%s; %s", CreatePublicKeyError, err.Error()))
+	return fmt.Errorf("%s; %s", CreatePublicKeyError, err.Error())
 }
