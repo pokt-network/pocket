5b1f7609a61956046f2f8e2ac8fcd4812137ce3c
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-02-25 13:24:33 -0800

    Added pre2p handlers

--- pre2p/types/network.go
+++ pre2p/types/network.go
@@ -5,13 +5,10 @@ import (
 	"net"
 )
 
-// TODO(olshansky): Try to find a way to remove `NodeId` from the entire codebase altogether. This is a stop-gap prototype measure.
+// TODO(olshansky): Try to find a way to remove `NodeId` from the entire codebase altogether; this is a stop-gap prototype measure.
 type Network interface {
 	NetworkBroadcast(data []byte, self NodeId) error
 	NetworkSend(data []byte, node NodeId) error
-
-	ConnectToValidator(nodeId NodeId, v *Validator) error
-
 	GetAddrBook() []*NetworkPeer
 }
 
