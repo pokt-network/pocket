afa6937f730f127b1ea9621f608d86f881ea4ebf
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-02-28 12:36:26 -0800

    Simplified events.go

--- shared/types/events.go
+++ shared/types/events.go
@@ -14,10 +14,6 @@ const (
 	PERSISTENCE SourceModule = "persistence"
 	UTILITY     SourceModule = "utility"
 
-	// Consensus
-	STATESYNC       SourceModule = "statesync"
-	LEADER_ELECTION SourceModule = "leader_election"
-
 	// Auxiliary
 	TEST  SourceModule = "test"
 	DEBUG SourceModule = "debug"
@@ -25,27 +21,11 @@ const (
 
 const (
 	// Consensus
-	CONSENSUS_MESSAGE           EventTopic = "CONSENSUS_MESSAGE"
-	CONSENSUS_TELEMETRY_MESSAGE EventTopic = "CONSENSUS_TELEMETRY_MESSAGE"
-
-	// Consensus auxilary
-	STATE_SYNC_MESSAGE      EventTopic = "STATE_SYNC_MESSAGE"
-	LEADER_ELECTION_MESSAGE EventTopic = "LEADER_ELECTION_MESSAGE"
-
-	// Utility
-	UTILITY_TX_MESSAGE       EventTopic = "TRANSACTION_MESSAGE"
-	UTILITY_EVIDENCE_MESSAGE EventTopic = "EVIDENCE_MESSAGE"
-
-	// P2P
-	P2P_SEND_MESSAGE      EventTopic = "p2p_send_message"
-	P2P_BROADCAST_MESSAGE EventTopic = "p2p_broadcast_message"
+	CONSENSUS_MESSAGE EventTopic = "CONSENSUS_MESSAGE"
 )
 
 type Event struct {
 	SourceModule SourceModule
-	// TODO(olshansky): Either add this back in or remove it altogether.
-	// Destination  types.NodeId
-
-	PocketTopic string
-	MessageData *anypb.Any
+	PocketTopic  EventTopic
+	MessageData  *anypb.Any
 }
