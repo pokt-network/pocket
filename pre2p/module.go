b08cfc932235caf0f593282ff3e0e54a0a10d907
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-02-28 11:57:24 -0800

    Fixed the pre2p main module

--- pre2p/module.go
+++ pre2p/module.go
@@ -5,53 +5,120 @@ package pre2p
 // is meant to be a "real" replacement for now.
 
 import (
+	"fmt"
 	"log"
+	"net"
+	"pocket/pre2p/types"
 	"pocket/shared/config"
 	"pocket/shared/modules"
 
 	"google.golang.org/protobuf/types/known/anypb"
+
+	"strconv"
+
+	"google.golang.org/protobuf/proto"
 )
 
 var _ modules.P2PModule = &p2pModule{}
 
 type p2pModule struct {
-	bus       modules.Bus
-	p2pConfig *config.P2PConfig
+	pocketBusMod modules.Bus
+
+	listener *net.TCPListener
+	network  types.Network
+	nodeId   types.NodeId
 }
 
-func Create(config *config.Config) (modules.P2PModule, error) {
-	return &p2pModule{
-		bus:       nil,
-		p2pConfig: config.P2P,
-	}, nil
+func Create(cfg *config.Config) (m modules.P2PModule, err error) {
+	log.Println("Creating network module")
+
+	p2pState := GetTestState()
+	p2pState.LoadStateFromConfig(cfg)
+
+	tcpAddr, _ := net.ResolveTCPAddr("tcp4", fmt.Sprintf(":%d", cfg.PREP2P.ConsensusPort))
+	l, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	m = &p2pModule{
+		listener: l,
+		network:  ConnectToValidatorNetwork(state.ValidatorMap),
+		nodeId:   types.NodeId(cfg.Consensus.NodeId),
+	}
+
+	return m, nil
 }
 
-func (p *p2pModule) Start() error {
-	// TODO(olshansky): Add a test that bus is set
-	log.Println("Starting P2P module...")
-	return nil
-}
-
-func (p *p2pModule) Stop() error {
-	log.Println("Stopping P2P module...")
-	return nil
-}
-
-func (m *p2pModule) SetBus(bus modules.Bus) {
-	m.bus = bus
+func (m *p2pModule) SetBus(pocketBus modules.Bus) {
+	m.pocketBusMod = pocketBus
 }
 
 func (m *p2pModule) GetBus() modules.Bus {
-	if m.bus == nil {
+	if m.pocketBusMod == nil {
 		log.Fatalf("PocketBus is not initialized")
 	}
-	return m.bus
+	return m.pocketBusMod
+}
+
+func (m *p2pModule) Start() error {
+	log.Println("Starting network module")
+
+	go func() {
+		for {
+			conn, err := m.listener.AcceptTCP()
+			if err != nil {
+				log.Println("Error accepting connection: ", err)
+				continue
+			}
+			go m.handleNetworkMessage(conn)
+		}
+	}()
+
+	return nil
+}
+
+func (m *p2pModule) Stop() error {
+	log.Println("Stopping network module")
+	if err := m.listener.Close(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *p2pModule) BroadcastMessage(msg *anypb.Any, topic string) error {
-	panic("BroadcastMessage not implemented")
+	c := &types.P2PMessage{
+		Topic: topic, // TODO topic is either P2P (from this module) or consensus
+		Data:  msg,
+	}
+	data, err := proto.Marshal(c)
+	if err != nil {
+		return err
+	}
+	fmt.Println("broadcasting message to network")
+	return m.network.NetworkBroadcast(data, m.nodeId)
 }
 
 func (m *p2pModule) Send(addr string, msg *anypb.Any, topic string) error {
-	panic("Send not implemented")
+	c := &types.P2PMessage{
+		Topic: topic, // TODO(discuss): Is this the approach we want to go with for P2PMessages?
+		Data:  msg,
+	}
+	data, err := proto.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	// TODO(olshansky): This is a hack while the consensus module is still dependant on `NodeId`.
+	nodeIdInt, err := strconv.Atoi(addr)
+	if err != nil {
+		return err
+	}
+	destNodeId := types.NodeId(nodeIdInt)
+
+	return m.network.NetworkSend(data, destNodeId)
+}
+
+func (m *p2pModule) GetAddrBook() []*types.NetworkPeer {
+	return m.network.GetAddrBook()
 }
