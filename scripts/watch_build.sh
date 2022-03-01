b182e2036fc853bfd5df07ceffe8d689b593e49c
iajrz <axel.rivas@gmail.com>
2022-03-01 16:36:46 -0400

    fix typo and warn if reflex not installed

--- scripts/watch_build.sh
+++ scripts/watch_build.sh
@@ -1,3 +1,8 @@
-#!/bin.bash
+#!/bin/bash
 
-reflex -r '\.go$' -s -- sh -c "go build -v cmd/pocket/main.go"
+if builtin type -P "reflex"
+then
+  reflex -r '\.go$' -s -- sh -c "go build -v cmd/pocket/main.go"
+else
+    echo "reflex not found. Install with `go install github.com/cespare/reflex@latest`"
+fi
