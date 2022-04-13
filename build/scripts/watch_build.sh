6b5741365dabfb8eabdf3a4d878f01bf0caf572c
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-04-13 13:40:49 -0700

    Moved aroudn a couple entrypoints and scripts

--- build/scripts/watch_build.sh
+++ build/scripts/watch_build.sh
@@ -2,7 +2,7 @@
 
 if builtin type -P "reflex"
 then
-  reflex -r '\.go$' -s -- sh -c "go build -v cmd/pocket/main.go"
+  reflex -r '\.go$' -s -- sh -c "go build -v app/pocket/main.go"
 else
     echo "reflex not found. Install with `go install github.com/cespare/reflex@latest`"
 fi
