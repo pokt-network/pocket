3e3965bec9bd383ae96b3b17868a55118418fa1e
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-04-13 17:06:25 -0700

    Namespaced postgres - starting point for local net DB
    - Start a postgres db with `compose_and_watch` if it hasn't already started
    - Connect to the local DB with `make connect_db`
    - Insert a random user ID into the node's specific schema every time it starts up
    - View schemas with `SELECT schema_name FROM information_schema.schemata;`
    - View random user IDs with `SELECT * from node1.users;`

--- shared/node.go
+++ shared/node.go
@@ -1,7 +1,14 @@
 package shared
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v4"
 
 	"github.com/pokt-network/pocket/p2p/pre2p"
 	"github.com/pokt-network/pocket/shared/config"
@@ -24,9 +31,40 @@ type Node struct {
 	Address cryptoPocket.Address
 }
 
+func pocketDbStuff() {
+	ctx := context.TODO()
+
+	url := "postgres://postgres:postgres@pocket-db:5432/postgres"
+	conn, err := pgx.Connect(context.Background(), url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	schema := os.Getenv("POSTGRES_SCHEMA")
+	conn.Exec(ctx, fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS %s", schema))
+	conn.Exec(ctx, fmt.Sprintf("set search_path TO %s", schema))
+
+	_, err = conn.Exec(ctx, `
+		create table IF NOT EXISTS users (id int);`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create users table: %v\n", err)
+		os.Exit(1)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	_, err = conn.Exec(ctx, fmt.Sprintf("INSERT INTO users (id) VALUES (%d)", rand.Int31()))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to insert random int into users table: %v\n", err)
+		os.Exit(1)
+	}
+}
+
 func Create(cfg *config.Config) (n *Node, err error) {
 	// TODO(design): initialize the state singleton until we have a proper solution for this.
 	_ = typesGenesis.GetNodeState(cfg)
+
+	pocketDbStuff()
 
 	// persistenceMod, err := persistence.Create(cfg)
 	prePersistenceMod, err := pre_persistence.Create(cfg)
