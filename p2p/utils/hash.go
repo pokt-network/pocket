5e643fea3780329c045b6f8ed6928eb21dee71b6
derrandz <ouaghad.hamza@gmail.com>
2022-04-24 18:48:59 +0000

    chore: remove old code

--- p2p/utils/hash.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "crypto/rand"
-
-func GenerateRandomHash() []byte {
-	buff := make([]byte, 8) // TODO(derrandz): revisit later. Decide on proper size for message hashes
-	rand.Read(buff)
-	return buff
-}
