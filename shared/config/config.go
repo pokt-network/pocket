5d46cd619c70d25e4d790d55fcbfdcfc7dc07ae1
derrandz <ouaghad.hamza@gmail.com>
2022-04-25 04:15:47 +0000

    chore: fix the json config loader

--- shared/config/config.go
+++ shared/config/config.go
@@ -11,6 +11,19 @@ import (
 	cryptoPocket "github.com/pokt-network/pocket/shared/crypto"
 )
 
+type JsonConfig struct {
+	RootDir string `json:"root_dir"`
+	Genesis string `json:"genesis"`
+
+	PrivateKey string `json:"private_key"`
+
+	Pre2P          Pre2PConfig          `json:"pre2p"` // TODO(derrandz): delete this once P2P is ready.
+	P2P            P2PConfig            `json:"p2p"`
+	Consensus      ConsensusConfig      `json:"consensus"`
+	PrePersistence PrePersistenceConfig `json:"pre_persistence"`
+	Persistence    PersistenceConfig    `json:"persistence"`
+	Utility        UtilityConfig        `json:"utility"`
+}
 type Config struct {
 	RootDir string `json:"root_dir"`
 	Genesis string `json:"genesis"`
@@ -37,19 +50,19 @@ type PrePersistenceConfig struct {
 }
 
 type P2PConfig struct {
-	Protocol         string               `json:"protocol"`
-	Address          cryptoPocket.Address `json:"address"`
-	ExternalIp       string               `json:"external_ip"`
-	Peers            []string             `json:"peers"`
-	BufferSize       uint                 `json:"connection_buffer_size"`
-	WireHeaderLength uint                 `json:"max_wire_header_length"`
-	TimeoutInMs      uint                 `json:"timeout_in_ms"`
+	Protocol         string   `json:"protocol,omitempty"`
+	Address          string   `json:"address,omitempty"`
+	ExternalIp       string   `json:"external_ip,omitempty"`
+	Peers            []string `json:"peers,omitempty"`
+	BufferSize       uint     `json:"connection_buffer_size,omitempty"`
+	WireHeaderLength uint     `json:"max_wire_header_length,omitempty"`
+	TimeoutInMs      uint     `json:"timeout_in_ms,omitempty"`
 }
 
 type PacemakerConfig struct {
-	TimeoutMsec               uint64 `json:"timeout_msec"`
-	Manual                    bool   `json:"manual"`
-	DebugTimeBetweenStepsMsec uint64 `json:"debug_time_between_steps_msec"`
+	TimeoutMsec               uint64 `json:"timeout_msec,omitempty"`
+	Manual                    bool   `json:"manual,omitempty"`
+	DebugTimeBetweenStepsMsec uint64 `json:"debug_time_between_steps_msec,omitempty"`
 }
 
 type ConsensusConfig struct {
@@ -84,9 +97,12 @@ func LoadConfig(file string) (c *Config) {
 	if err != nil {
 		log.Fatalln("Error reading config file: ", err)
 	}
-	if err = json.Unmarshal(bytes, c); err != nil {
+	jsonCfg := &JsonConfig{}
+	if err = json.Unmarshal(bytes, jsonCfg); err != nil {
 		log.Fatalln("Error parsing config file: ", err)
 	}
+
+	c = jsonCfg.toConfig()
 
 	if err := c.ValidateAndHydrate(); err != nil {
 		log.Fatalln("Error validating or completing config: ", err)
@@ -134,6 +150,44 @@ func (c *ConsensusConfig) ValidateAndHydrate() error {
 	}
 
 	return nil
+}
+
+func (jc *JsonConfig) toConfig() *Config {
+	pc := &Config{
+		RootDir: jc.RootDir,
+		Genesis: jc.Genesis,
+
+		PrivateKey: cryptoPocket.Ed25519PrivateKey([]byte(jc.PrivateKey)),
+
+		P2P: &P2PConfig{
+			Protocol:         jc.P2P.Protocol,
+			Address:          jc.P2P.Address,
+			ExternalIp:       jc.P2P.ExternalIp,
+			Peers:            jc.P2P.Peers,
+			BufferSize:       jc.P2P.BufferSize,
+			WireHeaderLength: jc.P2P.WireHeaderLength,
+			TimeoutInMs:      jc.P2P.TimeoutInMs,
+		},
+		Consensus: &ConsensusConfig{
+			MaxMempoolBytes: jc.Consensus.MaxMempoolBytes,
+			MaxBlockBytes:   jc.Consensus.MaxBlockBytes,
+			Pacemaker: &PacemakerConfig{
+				TimeoutMsec:               jc.Consensus.Pacemaker.TimeoutMsec,
+				Manual:                    jc.Consensus.Pacemaker.Manual,
+				DebugTimeBetweenStepsMsec: jc.Consensus.Pacemaker.DebugTimeBetweenStepsMsec,
+			},
+		},
+		PrePersistence: &PrePersistenceConfig{
+			Capacity:        jc.PrePersistence.Capacity,
+			MempoolMaxBytes: jc.PrePersistence.MempoolMaxBytes,
+			MempoolMaxTxs:   jc.PrePersistence.MempoolMaxTxs,
+		},
+		Persistence: &PersistenceConfig{
+			DataDir: jc.Persistence.DataDir,
+		},
+		Utility: &UtilityConfig{},
+	}
+	return pc
 }
 
 func (c *PacemakerConfig) ValidateAndHydrate() error {
