c0c30acda479d6c69ea8f371491ffe77f39c6014
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-05-10 14:35:12 -0700

    Reordered code in account_test.go to make it easier to follow

--- persistence/account.go
+++ persistence/account.go
@@ -12,19 +12,7 @@ import (
 // TODO(Andrew): Convert all queries to use 'height' in the interface for historical lookups
 // TODO(team): Consider converting all address params from bytes to string to avoid unnecessary encoding?
 
-func (p PostgresContext) AddAccountAmount(address []byte, amount string) error {
-	return p.operationAccountAmount(address, amount, func(s *big.Int, s1 *big.Int) error {
-		s.Add(s, s1)
-		return nil
-	})
-}
-
-func (p PostgresContext) SubtractAccountAmount(address []byte, amount string) error {
-	return p.operationAccountAmount(address, amount, func(s *big.Int, s1 *big.Int) error {
-		s.Sub(s, s1)
-		return nil
-	})
-}
+// --- Account Functions ---
 
 func (p PostgresContext) GetAccountAmount(address []byte) (amount string, err error) {
 	ctx, conn, err := p.DB.GetCtxAndConnection()
@@ -37,6 +25,23 @@ func (p PostgresContext) GetAccountAmount(address []byte) (amount string, err er
 	return
 }
 
+func (p PostgresContext) AddAccountAmount(address []byte, amount string) error {
+	return p.operationAccountAmount(address, amount, func(orig *big.Int, delta *big.Int) error {
+		orig.Add(orig, delta)
+		return nil
+	})
+}
+
+func (p PostgresContext) SubtractAccountAmount(address []byte, amount string) error {
+	return p.operationAccountAmount(address, amount, func(orig *big.Int, delta *big.Int) error {
+		orig.Sub(orig, delta)
+		return nil
+	})
+}
+
+// DISCUSS(team): If we are okay with `GetAccountAmount` return 0 as a default, this function
+// can leverage `operationAccountAmount` with `*orig = *delta` and make everything much simpler.
+// Ditto below for the pool.
 func (p PostgresContext) SetAccountAmount(address []byte, amount string) error {
 	ctx, conn, err := p.DB.GetCtxAndConnection()
 	if err != nil {
@@ -53,11 +58,52 @@ func (p PostgresContext) SetAccountAmount(address []byte, amount string) error {
 	if _, err = tx.Exec(ctx, schema.NullifyAccountAmountQuery(hex.EncodeToString(address), height)); err != nil {
 		return err
 	}
+	// DISCUSS(team): Do we want to enforce `amount > 0` here? Ditto below for the pool.
 	if _, err = tx.Exec(ctx, schema.InsertAccountAmountQuery(hex.EncodeToString(address), amount, height)); err != nil {
 		return err
 	}
 	return tx.Commit(ctx)
 }
+
+func (p *PostgresContext) operationAccountAmount(address []byte, deltaAmount string, op func(*big.Int, *big.Int) error) error {
+	ctx, conn, err := p.DB.GetCtxAndConnection()
+	if err != nil {
+		return err
+	}
+	height, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+	accountAmount, err := p.GetAccountAmount(address)
+	if err != nil {
+		return err
+	}
+	accountAmountBig, err := shared.StringToBigInt(accountAmount)
+	if err != nil {
+		return err
+	}
+	deltaAmountBig, err := shared.StringToBigInt(deltaAmount)
+	if err != nil {
+		return err
+	}
+	if err := op(accountAmountBig, deltaAmountBig); err != nil {
+		return err
+	}
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Exec(ctx, schema.NullifyAccountAmountQuery(hex.EncodeToString(address), height)); err != nil {
+		return err
+	}
+	// DISCUSS(team): Do we want to enforce `accountAmountBig > 0` here? Ditto below for the pool.
+	if _, err := tx.Exec(ctx, schema.InsertAccountAmountQuery(hex.EncodeToString(address), shared.BigIntToString(accountAmountBig), height)); err != nil {
+		return err
+	}
+	return tx.Commit(ctx)
+}
+
+// --- Pool Functions ---
 
 func (p PostgresContext) GetPoolAmount(name string) (amount string, err error) {
 	ctx, conn, err := p.DB.GetCtxAndConnection()
@@ -123,43 +169,6 @@ func (p PostgresContext) SetPoolAmount(name string, amount string) error {
 		return err
 	}
 	if _, err = tx.Exec(ctx, schema.InsertPoolAmountQuery(name, amount, height)); err != nil {
-		return err
-	}
-	return tx.Commit(ctx)
-}
-
-func (p *PostgresContext) operationAccountAmount(address []byte, amount string, op func(*big.Int, *big.Int) error) error {
-	ctx, conn, err := p.DB.GetCtxAndConnection()
-	if err != nil {
-		return err
-	}
-	height, err := p.GetHeight()
-	if err != nil {
-		return err
-	}
-	originalAmount, err := p.GetAccountAmount(address)
-	if err != nil {
-		return err
-	}
-	originalAmountBig, err := shared.StringToBigInt(originalAmount)
-	if err != nil {
-		return err
-	}
-	amountBig, err := shared.StringToBigInt(amount)
-	if err != nil {
-		return err
-	}
-	if err := op(originalAmountBig, amountBig); err != nil {
-		return err
-	}
-	tx, err := conn.BeginTx(ctx, pgx.TxOptions{})
-	if err != nil {
-		return err
-	}
-	if _, err := tx.Exec(ctx, schema.NullifyAccountAmountQuery(hex.EncodeToString(address), height)); err != nil {
-		return err
-	}
-	if _, err := tx.Exec(ctx, schema.InsertAccountAmountQuery(hex.EncodeToString(address), shared.BigIntToString(originalAmountBig), height)); err != nil {
 		return err
 	}
 	return tx.Commit(ctx)
