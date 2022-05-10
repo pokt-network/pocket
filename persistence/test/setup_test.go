6d71021bc5b195e09f663e822607715f46b4f299
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-05-10 13:20:37 -0700

    Made TestMain in persistence tests easier to understand

--- persistence/test/setup_test.go
+++ persistence/test/setup_test.go
@@ -12,16 +12,18 @@ import (
 	"github.com/pokt-network/pocket/persistence"
 )
 
-var (
-	user       = "postgres"
-	password   = "secret"
-	db         = "postgres"
-	port       = "5432"
-	dialect    = "postgres"
-	connString = "postgres://%s:%s@localhost:%s/%s?sslmode=disable"
+const (
+	user             = "postgres"
+	password         = "secret"
+	db               = "postgres"
+	schema           = "test_schema"
+	localhost        = "0.0.0.0"
+	port             = "5432"
+	dialect          = "postgres"
+	connStringFormat = "postgres://%s:%s@localhost:%s/%s?sslmode=disable"
 )
 
-// TODO:(TEAM) make these tests thread safe
+// TODO:(team) make these tests thread safe
 var PostgresDB *persistence.PostgresDB
 
 func init() {
@@ -29,10 +31,6 @@ func init() {
 }
 
 func TestMain(m *testing.M) {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
-	}
 	opts := dockertest.RunOptions{
 		Repository: "postgres",
 		Tag:        "12.3",
@@ -41,19 +39,36 @@ func TestMain(m *testing.M) {
 			"POSTGRES_PASSWORD=" + password,
 			"POSTGRES_DB=" + db,
 		},
-		ExposedPorts: []string{"5432"},
+		ExposedPorts: []string{port},
 		PortBindings: map[docker.Port][]docker.PortBinding{
-			"5432": {
-				{HostIP: "0.0.0.0", HostPort: port},
+			port: {
+				{HostIP: localhost, HostPort: port},
 			},
 		},
 	}
+	connString := fmt.Sprintf(connStringFormat, user, password, port, db)
+
+	defer func() {
+		ctx, _ := PostgresDB.GetContext()
+		PostgresDB.Conn.Close(ctx)
+		ctx.Done()
+	}()
+
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
 	resource, err := pool.RunWithOptions(&opts)
 	if err != nil {
-		log.Fatalf("could not start resource: %s\n", err.Error())
+		log.Fatalf("***Make sure your docker daemon is running!!*** Could not start resource: %s\n", err.Error())
 	}
+
+	// DISCUSS(drewsky): Is this some sort of cleanup?
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
+	signal.Notify(c, os.Interrupt, os.Kill) // Why not syscall.SIGTERM?
 	go func() {
 		for sig := range c {
 			log.Printf("exit signal %d received\n", sig)
@@ -62,9 +77,10 @@ func TestMain(m *testing.M) {
 			}
 		}
 	}()
-	connString = fmt.Sprintf(connString, user, password, port, db)
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
 	if err = pool.Retry(func() error {
-		conn, err := persistence.ConnectAndInitializeDatabase(connString, "test_schema")
+		conn, err := persistence.ConnectAndInitializeDatabase(connString, schema)
 		if err != nil {
 			log.Println(err.Error())
 			return err
@@ -74,12 +90,9 @@ func TestMain(m *testing.M) {
 	}); err != nil {
 		log.Fatalf("could not connect to docker: %s", err.Error())
 	}
-	defer func() {
-		ctx, _ := PostgresDB.GetContext()
-		PostgresDB.Conn.Close(ctx)
-		ctx.Done()
-	}()
 	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
 	if err := pool.Purge(resource); err != nil {
 		log.Fatalf("could not purge resource: %s", err)
 	}
