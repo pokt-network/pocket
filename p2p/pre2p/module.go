8e47c4f77341b8c9150455bfb98fab121408efd8
derrandz <ouaghad.hamza@gmail.com>
2022-05-11 02:51:49 +0100

    feat: track pre2p metrics

---
+++ p2p/pre2p/module.go
@@ -0,0 +1,185 @@
+package pre2p
+
+// TODO(team): This is a a temporary parallel to the real `p2p` module.
+// It should be removed once the real `p2p` module is ready but is meant
+// to be a "real" replacement for now.
+
+import (
+	"log"
+
+	"github.com/pokt-network/pocket/p2p/pre2p/raintree"
+	"github.com/pokt-network/pocket/p2p/pre2p/stdnetwork"
+	typesPre2P "github.com/pokt-network/pocket/p2p/pre2p/types"
+
+	"github.com/pokt-network/pocket/shared/config"
+	cryptoPocket "github.com/pokt-network/pocket/shared/crypto"
+	"github.com/pokt-network/pocket/shared/modules"
+	"github.com/pokt-network/pocket/shared/types"
+	typesGenesis "github.com/pokt-network/pocket/shared/types/genesis"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+var _ modules.P2PModule = &p2pModule{}
+
+type p2pModule struct {
+	bus modules.Bus
+
+	listener typesPre2P.TransportLayerConn
+	address  cryptoPocket.Address
+
+	network typesPre2P.Network
+}
+
+func Create(cfg *config.Config) (m modules.P2PModule, err error) {
+	log.Println("Creating network module")
+
+	l, err := CreateListener(cfg.Pre2P)
+	if err != nil {
+		return nil, err
+	}
+
+	testState := typesGenesis.GetNodeState(nil)
+	addrBook, err := ValidatorMapToAddrBook(cfg.Pre2P, testState.ValidatorMap)
+	if err != nil {
+		return nil, err
+	}
+
+	var network typesPre2P.Network
+	if cfg.Pre2P.UseRainTree {
+		selfAddr := cfg.PrivateKey.Address()
+		network = raintree.NewRainTreeNetwork(selfAddr, addrBook, cfg)
+	} else {
+		network = stdnetwork.NewNetwork(addrBook)
+	}
+
+	m = &p2pModule{
+		listener: l,
+		network:  network,
+		address:  cfg.PrivateKey.Address(),
+	}
+
+	return m, nil
+}
+
+func (m *p2pModule) SetBus(bus modules.Bus) {
+	m.bus = bus
+}
+
+func (m *p2pModule) GetBus() modules.Bus {
+	if m.bus == nil {
+		log.Fatalf("PocketBus is not initialized")
+	}
+	return m.bus
+}
+
+func (m *p2pModule) Start() error {
+	log.Println("Starting network module")
+
+	telemetryMod := m.GetBus().GetTelemetryModule()
+
+	telemetryMod.RegisterCounterMetric(
+		"nodes",
+		"the counter to track the number of nodes online",
+	)
+
+	telemetryMod.RegisterCounterMetric(
+		"received_messages",
+		"the counter to track received messages",
+	)
+
+	telemetryMod.RegisterCounterMetric(
+		"opened_connections",
+		"the counter to track how many connections were open",
+	)
+
+	go func() {
+		for {
+			data, err := m.listener.Read()
+			if err != nil {
+				log.Println("Error reading data from connection: ", err)
+				continue
+			}
+
+			m.GetBus().
+				GetTelemetryModule().
+				IncrementCounterMetric("opened_connections")
+
+			go m.handleNetworkMessage(data)
+		}
+	}()
+
+	// TODO(tema):
+	// No way to know if the node has actually properly started as of the current implementation of m.listener
+	// Make sure to do this after the node has started if the implementation allowed for this in the future.
+	m.GetBus().
+		GetTelemetryModule().
+		IncrementCounterMetric("nodes")
+
+	return nil
+}
+
+func (m *p2pModule) Stop() error {
+	log.Println("Stopping network module")
+	if err := m.listener.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *p2pModule) Broadcast(msg *anypb.Any, topic types.PocketTopic) error {
+	c := &types.PocketEvent{
+		Topic: topic,
+		Data:  msg,
+	}
+	data, err := proto.Marshal(c)
+	if err != nil {
+		return err
+	}
+	log.Println("broadcasting message to network")
+	return m.network.NetworkBroadcast(data)
+}
+
+func (m *p2pModule) Send(addr cryptoPocket.Address, msg *anypb.Any, topic types.PocketTopic) error {
+	c := &types.PocketEvent{
+		Topic: topic,
+		Data:  msg,
+	}
+	data, err := proto.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	return m.network.NetworkSend(data, addr)
+}
+
+func (m *p2pModule) handleNetworkMessage(networkMsgData []byte) {
+	m.GetBus().
+		GetTelemetryModule().
+		IncrementCounterMetric("receieved_messages")
+
+	appMsgData, err := m.network.HandleNetworkData(networkMsgData)
+	if err != nil {
+		log.Println("Error handling raw data: ", err)
+		return
+	}
+
+	// There was no error, but we don't need to forward this to the app-specific bus.
+	// For example, the message has already been handled by the application.
+	if appMsgData == nil {
+		return
+	}
+
+	networkMessage := types.PocketEvent{}
+	if err := proto.Unmarshal(appMsgData, &networkMessage); err != nil {
+		log.Println("Error decoding network message: ", err)
+		return
+	}
+
+	event := types.PocketEvent{
+		Topic: networkMessage.Topic,
+		Data:  networkMessage.Data,
+	}
+
+	m.GetBus().PublishEventToBus(&event)
+}
