6fb9647782692edb03ac0f3e38ad69958908cd8b
A. Bender <109155979+phthan0@users.noreply.github.com>
2022-07-26 19:50:14 -0400

    Update bls.go

--- shared/crypto/bls.go
+++ shared/crypto/bls.go
@@ -1,23 +1,48 @@
 package bls
 
 import (
-	"bytes"
-	"testing"
-
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/eywa-protocol/bls-crypto/bls"
-	"github.com/stretchr/testify/require"
+        "github.com/herumi/bls-eth-go-binary/bls"
+        "log"
+        "time"
 )
 
-var (
-	message              = GenRandomBytes(5000)
-	secretKey, publicKey = bls.GenerateRandomKey()
-	signature            = secretKey.Sign(message)
-	pubBytes             = publicKey.Marshal()
-	sigBytes             = signature.Marshal()
-)
 
-func Test_VerifySignature(t *testing.T) {
-	require.True(t, signature.Verify(publicKey, message))
+
+func main() {
+        bls.Init(bls.BLS12_381)
+        bls.SetETHmode(bls.EthModeDraft07)
+
+
+        n := "Pokt"
+        m := []byte("Pokt")
+        var aggSig *bls.Sign
+        var aggPub *bls.PublicKey
+        startTime := time.Now()
+        for i := 0; i < 1000; i++ {
+                var sec bls.SecretKey
+                sec.SetByCSPRNG()
+                if i == 0 {
+                        aggSig = sec.SignByte(m)
+                        aggPub = sec.GetPublicKey()
+                } else {
+                        aggSig.Add(sec.SignByte(m))
+                        aggPub.Add(sec.GetPublicKey())
+                }
+        }
+        endTime := time.Now()
+        log.Printf("Time required to sign 1000 messages and aggregate 1000 pub keys and signatures: %f seconds", endTime.Sub(startTime).Seconds())
+        log.Printf("Aggregate Signature: 0x%x", aggSig.Serialize())
+        log.Printf("Aggregate Public Key: 0x%x", aggPub.Serialize())
+
+        startTime = time.Now()
+        if !aggSig.Verify(aggPub, n) {
+                log.Fatal("Aggregate Signature Does Not Verify")
+        }
+        log.Printf("Aggregate Signature Verifies Correctly!")
+        endTime = time.Now()
+        log.Printf("Time required to verify aggregate sig: %f seconds", endTime.Sub(startTime).Seconds())
 }
+
+
+
 
