d44ca6c1ce8bb6092d509af6d8e437b31d4adbcd
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-08-17 16:10:47 -0400

    Populate genesis state in DEBUG_CLEAR_STATE

--- persistence/debugging.go
+++ persistence/debugging.go
@@ -12,6 +12,7 @@ func (m *persistenceModule) HandleDebugMessage(debugMessage *types.DebugMessage)
 		m.showLatestBlockInStore(debugMessage)
 	case types.DebugMessageAction_DEBUG_CLEAR_STATE:
 		m.clearState(debugMessage)
+		m.populateGenesisState(m.GetBus().GetConfig().GenesisSource.GetState())
 	default:
 		log.Printf("Debug message not handled by persistence module: %s \n", debugMessage.Message)
 	}
