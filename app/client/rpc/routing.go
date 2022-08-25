43bd107fa4a1fa28c4bdc77433a77bd9b837a857
Alessandro De Blasis <alex@deblasis.net>
2022-08-25 13:58:59 +0100

    fix(RPC): fixed HTTP method for Health and Version routes

--- app/client/rpc/routing.go
+++ app/client/rpc/routing.go
@@ -22,8 +22,8 @@ type Routes []Route
 func (s *rpcServer) GetRoutes() Routes {
 	routes := Routes{
 		// System routes
-		Route{Name: "Health", Method: "POST", Path: "/v1/health", HandlerFunc: s.HealthHandler},
-		Route{Name: "Version", Method: "POST", Path: "/v1/version", HandlerFunc: s.VersionHandler},
+		Route{Name: "Health", Method: "GET", Path: "/v1/health", HandlerFunc: s.HealthHandler},
+		Route{Name: "Version", Method: "GET", Path: "/v1/version", HandlerFunc: s.VersionHandler},
 
 		// Commands
 		Route{Name: "BroadcastTxSync", Method: "POST", Path: "/v1/client/broadcast_tx_sync", HandlerFunc: s.BroadcastRawTxSyncHandler},
