4d3331db5ff028d6a7cef9f060940eee32977366
Alessandro De Blasis <alex@deblasis.net>
2022-08-25 16:35:13 +0100

    style(RPC): format

--- app/client/rpc/routing.go
+++ app/client/rpc/routing.go
@@ -27,11 +27,11 @@ func (s *rpcServer) GetRoutes() Routes {
 
 		// Commands
 		Route{Name: "BroadcastTxSync", Method: "POST", Path: "/v1/client/broadcast_tx_sync", HandlerFunc: s.BroadcastRawTxSyncHandler},
-		Route{Name: "BroadcastTxAsync", Method: "POST", Path: "/v1/client/broadcast_tx_async", HandlerFunc: s.TODOHandler},   //TODO (team)
-		Route{Name: "BroadcastTxCommit", Method: "POST", Path: "/v1/client/broadcast_tx_commit", HandlerFunc: s.TODOHandler}, //TODO (team)
+		Route{Name: "BroadcastTxAsync", Method: "POST", Path: "/v1/client/broadcast_tx_async", HandlerFunc: s.TODOHandler},   // TODO (team)
+		Route{Name: "BroadcastTxCommit", Method: "POST", Path: "/v1/client/broadcast_tx_commit", HandlerFunc: s.TODOHandler}, // TODO (team)
 
 		// Queries
-		Route{Name: "QueryTx", Method: "POST", Path: "/v1/query/tx", HandlerFunc: s.TODOHandler}, //TODO (team)
+		Route{Name: "QueryTx", Method: "POST", Path: "/v1/query/tx", HandlerFunc: s.TODOHandler}, // TODO (team)
 	}
 	return routes
 }
