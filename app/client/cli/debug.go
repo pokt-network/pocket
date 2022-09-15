35914a81bd91a989fc7134f8769e0b385a56e386
Alessandro De Blasis <alex@deblasis.net>
2022-09-15 10:08:35 +0100

    fix(Utility): CLI specific fixes

--- app/client/cli/debug.go
+++ app/client/cli/debug.go
@@ -10,10 +10,9 @@ import (
 	"github.com/pokt-network/pocket/p2p"
 	"github.com/pokt-network/pocket/shared"
 	pocketCrypto "github.com/pokt-network/pocket/shared/crypto"
+	"github.com/pokt-network/pocket/shared/debug"
 	"github.com/pokt-network/pocket/shared/modules"
-	"github.com/pokt-network/pocket/shared/telemetry"
-	"github.com/pokt-network/pocket/shared/types"
-	"github.com/pokt-network/pocket/shared/types/genesis/test_artifacts"
+	"github.com/pokt-network/pocket/telemetry"
 	"github.com/spf13/cobra"
 	"google.golang.org/protobuf/types/known/anypb"
 )
@@ -25,8 +24,8 @@ const (
 	PromptTogglePacemakerMode    string = "TogglePacemakerMode"
 	PromptShowLatestBlockInStore string = "ShowLatestBlockInStore"
 
-	configPath  string = "build/config/config1.json"
-	genesisPath string = "build/config/genesis.json"
+	defaultConfigPath  = "build/config/config1.json"
+	defaultGenesisPath = "build/config/genesis.json"
 )
 
 var (
@@ -97,32 +96,32 @@ func promptGetInput() (string, error) {
 func handleSelect(selection string) {
 	switch selection {
 	case PromptResetToGenesis:
-		m := &types.DebugMessage{
-			Action:  types.DebugMessageAction_DEBUG_CONSENSUS_RESET_TO_GENESIS,
+		m := &debug.DebugMessage{
+			Action:  debug.DebugMessageAction_DEBUG_CONSENSUS_RESET_TO_GENESIS,
 			Message: nil,
 		}
 		broadcastDebugMessage(m)
 	case PromptPrintNodeState:
-		m := &types.DebugMessage{
-			Action:  types.DebugMessageAction_DEBUG_CONSENSUS_PRINT_NODE_STATE,
+		m := &debug.DebugMessage{
+			Action:  debug.DebugMessageAction_DEBUG_CONSENSUS_PRINT_NODE_STATE,
 			Message: nil,
 		}
 		broadcastDebugMessage(m)
 	case PromptTriggerNextView:
-		m := &types.DebugMessage{
-			Action:  types.DebugMessageAction_DEBUG_CONSENSUS_TRIGGER_NEXT_VIEW,
+		m := &debug.DebugMessage{
+			Action:  debug.DebugMessageAction_DEBUG_CONSENSUS_TRIGGER_NEXT_VIEW,
 			Message: nil,
 		}
 		broadcastDebugMessage(m)
 	case PromptTogglePacemakerMode:
-		m := &types.DebugMessage{
-			Action:  types.DebugMessageAction_DEBUG_CONSENSUS_TOGGLE_PACE_MAKER_MODE,
+		m := &debug.DebugMessage{
+			Action:  debug.DebugMessageAction_DEBUG_CONSENSUS_TOGGLE_PACE_MAKER_MODE,
 			Message: nil,
 		}
 		broadcastDebugMessage(m)
 	case PromptShowLatestBlockInStore:
-		m := &types.DebugMessage{
-			Action:  types.DebugMessageAction_DEBUG_SHOW_LATEST_BLOCK_IN_STORE,
+		m := &debug.DebugMessage{
+			Action:  debug.DebugMessageAction_DEBUG_SHOW_LATEST_BLOCK_IN_STORE,
 			Message: nil,
 		}
 		sendDebugMessage(m)
@@ -132,7 +131,7 @@ func handleSelect(selection string) {
 }
 
 // Broadcast to the entire validator set
-func broadcastDebugMessage(debugMsg *types.DebugMessage) {
+func broadcastDebugMessage(debugMsg *debug.DebugMessage) {
 	anyProto, err := anypb.New(debugMsg)
 	if err != nil {
 		log.Fatalf("[ERROR] Failed to create Any proto: %v", err)
@@ -141,19 +140,19 @@ func broadcastDebugMessage(debugMsg *types.DebugMessage) {
 	// TODO(olshansky): Once we implement the cleanup layer in RainTree, we'll be able to use
 	// broadcast. The reason it cannot be done right now is because this client is not in the
 	// address book of the actual validator nodes, so `node1.consensus` never receives the message.
-	// p2pMod.Broadcast(anyProto, types.PocketTopic_DEBUG_TOPIC)
+	// p2pMod.Broadcast(anyProto, debug.PocketTopic_DEBUG_TOPIC)
 
 	for _, val := range consensusMod.ValidatorMap() {
-		addr, err := pocketCrypto.NewAddress(val.Address)
+		addr, err := pocketCrypto.NewAddress(val.GetAddress())
 		if err != nil {
 			log.Fatalf("[ERROR] Failed to convert validator address into pocketCrypto.Address: %v", err)
 		}
-		p2pMod.Send(addr, anyProto, types.PocketTopic_DEBUG_TOPIC)
+		p2pMod.Send(addr, anyProto, debug.PocketTopic_DEBUG_TOPIC)
 	}
 }
 
 // Send to just a single (i.e. first) validator in the set
-func sendDebugMessage(debugMsg *types.DebugMessage) {
+func sendDebugMessage(debugMsg *debug.DebugMessage) {
 	anyProto, err := anypb.New(debugMsg)
 	if err != nil {
 		log.Fatalf("[ERROR] Failed to create Any proto: %v", err)
@@ -161,14 +160,14 @@ func sendDebugMessage(debugMsg *types.DebugMessage) {
 
 	var validatorAddress []byte
 	for _, val := range consensusMod.ValidatorMap() {
-		validatorAddress, err = pocketCrypto.NewAddress(val.Address)
+		validatorAddress, err = pocketCrypto.NewAddress(val.GetAddress())
 		if err != nil {
 			log.Fatalf("[ERROR] Failed to convert validator address into pocketCrypto.Address: %v", err)
 		}
 		break
 	}
 
-	p2pMod.Send(validatorAddress, anyProto, types.PocketTopic_DEBUG_TOPIC)
+	p2pMod.Send(validatorAddress, anyProto, debug.PocketTopic_DEBUG_TOPIC)
 }
 
 func initDebug(remoteCLIURL string) {
@@ -180,29 +179,24 @@ func initDebug(remoteCLIURL string) {
 			log.Fatalf(err.Error())
 		}
 
-		config, genesis := test_artifacts.ReadConfigAndGenesisFiles(configPath, genesisPath)
-		config.Base.PrivateKey = clientPrivateKey.String()
-
-		consensusMod, err = consensus.Create(config, genesis)
+		consensusMod, err = consensus.Create(defaultConfigPath, defaultGenesisPath, true) // TECHDEBT: extra param required for injecting private key hack for debug client
 		if err != nil {
 			log.Fatalf("[ERROR] Failed to create consensus module: %v", err.Error())
 		}
-
-		p2pMod, err = p2p.Create(config, genesis)
+		p2pMod, err = p2p.Create(defaultConfigPath, defaultGenesisPath, true) // TECHDEBT: extra param required for injecting private key hack for debug client
 		if err != nil {
 			log.Fatalf("[ERROR] Failed to create p2p module: %v", err.Error())
 		}
-
 		// This telemetry module instance is a NOOP because the 'enable_telemetry' flag in the `cfg` above is set to false.
 		// Since this client mimics partial - networking only - functionality of a full node, some of the telemetry-related
 		// code paths are executed. To avoid those messages interfering with the telemetry data collected, a non-nil telemetry
 		// module that NOOPs (per the configs above) is injected.
-		telemetryMod, err := telemetry.Create(config, genesis)
+		telemetryMod, err := telemetry.Create(defaultConfigPath, defaultGenesisPath)
 		if err != nil {
 			log.Fatalf("[ERROR] Failed to create NOOP telemetry module: " + err.Error())
 		}
 
-		_ = shared.CreateBusWithOptionalModules(nil, p2pMod, nil, consensusMod, telemetryMod, config, genesis)
+		_ = shared.CreateBusWithOptionalModules(nil, p2pMod, nil, consensusMod, telemetryMod)
 
 		p2pMod.Start()
 	})
