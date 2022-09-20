c0afedf1a054a44822393e2bf811e7a3c27d544a
Alessandro De Blasis <alex@deblasis.net>
2022-09-20 13:29:14 +0100

    feat(Config): viper config parsing + environment variables

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,7 +1,39 @@
 package runtime
 
+import (
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+)
+
 func Init(configPath, genesisPath string) (config *Config, genesis *Genesis, err error) {
-	config, err = ParseConfigJSON(configPath)
+	dir, file := path.Split(configPath)
+	filename := strings.TrimSuffix(file, filepath.Ext(file))
+
+	viper.AddConfigPath(".")
+	viper.AddConfigPath(dir)
+	viper.SetConfigName(filename)
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+
+	// The lines below allow for environment variables configuration (12 factor app)
+	// Eg: POCKET_CONSENSUS_PRIVATE_KEY=somekey would override `consensus.private_key` in config
+	viper.SetEnvPrefix("POCKET")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config, func(dc *mapstructure.DecoderConfig) {
+		// This is to leverage the `json` struct tags without having to add `mapstructure` ones.
+		// Until we have complex use cases, this should work just fine.
+		dc.TagName = "json"
+	})
 	if err != nil {
 		return
 	}
