2aa7c294d59765a209423af0cc7bdccec7951ed9
Alessandro De Blasis <alex@deblasis.net>
2022-09-20 11:42:34 +0100

    fix(Config): Base init

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -6,8 +6,12 @@ func Init(configPath, genesisPath string) (config *Config, genesis *Genesis, err
 		return
 	}
 
+	if config.Base == nil {
+		config.Base = &BaseConfig{}
+	}
 	config.Base.ConfigPath = configPath
 	config.Base.GenesisPath = genesisPath
+
 	genesis, err = ParseGenesisJSON(genesisPath)
 	return
 }
