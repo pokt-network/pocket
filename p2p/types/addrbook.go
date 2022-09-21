408b2f22623f635b0f86aae10f0093a45bbb0e23
Alessandro De Blasis <alex@deblasis.net>
2022-09-21 21:36:10 +0100

    refactor(P2P): cleanup unused methods

--- p2p/types/addrbook.go
+++ p2p/types/addrbook.go
@@ -1,29 +1,3 @@
 package types
 
-import (
-	"fmt"
-	"sort"
-)
-
 type AddrBook []*NetworkPeer
-
-func (ab *AddrBook) ToListAndMap(selfAddressString string) (addrList AddrList, addrBookMap AddrBookMap, err error) {
-	addrBook := *ab
-	// OPTIMIZE(olshansky): This is a very naive approach for now that recomputes everything every time that we can optimize later
-	addrBookMap = make(map[string]*NetworkPeer, len(addrBook))
-	addrList = make([]string, len(addrBook))
-	for i, peer := range addrBook {
-		addr := peer.Address.String()
-		addrList[i] = addr
-		addrBookMap[addr] = peer
-	}
-	sort.Strings(addrList)
-	if i, ok := addrList.Find(selfAddressString); ok {
-		// The list is sorted lexicographically above, but is reformatted below so this addr of this node
-		// is always the first in the list. This makes RainTree propagation easier to compute and interpret.
-		addrList = append(addrList[i:], addrList[0:i]...)
-	} else {
-		return nil, nil, fmt.Errorf("self address not found for %s in addrBook so this client can send messages but does not propagate them", selfAddressString)
-	}
-	return
-}
--- p2p/types/addrlist.go
+++ p2p/types/addrlist.go
@@ -1,16 +1,3 @@
 package types
 
 type AddrList []string
-
-func (ab *AddrList) Find(address string) (index int, found bool) {
-	if ab == nil {
-		return 0, false
-	}
-	addressBook := *ab
-	for i, a := range addressBook {
-		if a == address {
-			return i, true
-		}
-	}
-	return 0, false
-}
