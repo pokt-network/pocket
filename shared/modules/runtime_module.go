eb25f351dbd75e92b1850bf884fb382c0c4800f4
Alessandro De Blasis <alex@deblasis.net>
2022-09-26 17:37:29 +0100

    Update shared/modules/runtime_module.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- shared/modules/runtime_module.go
+++ shared/modules/runtime_module.go
@@ -3,5 +3,5 @@ package modules
 type Runtime interface {
 	GetConfig() Config
 	GetGenesis() GenesisState
-	ShouldUseRandomPK() bool
+	ShouldUseRandomPK() bool // INVESTIGATE: look into how we can remove this from the runtime interface
 }
