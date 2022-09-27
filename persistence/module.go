242565cd694b10971961c2a6adfbc6c5c6ff1f9a
Daniel Olshansky <olshansky.daniel@gmail.com>
2022-09-26 20:46:10 -0700

    Undo more change

---
+++ persistence/module.go
@@ -0,0 +1,235 @@
+package persistence
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/pokt-network/pocket/persistence/types"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/pokt-network/pocket/persistence/kvstore"
+	"github.com/pokt-network/pocket/shared/modules"
+	"github.com/pokt-network/pocket/shared/test_artifacts"
+)
+
+var _ modules.PersistenceModule = &PersistenceModule{}
+var _ modules.PersistenceRWContext = &PostgresContext{}
+var _ modules.PersistenceGenesisState = &types.PersistenceGenesisState{}
+var _ modules.PersistenceConfig = &types.PersistenceConfig{}
+
+type PersistenceModule struct {
+	bus modules.Bus
+
+	postgresURL string
+	nodeSchema  string
+	genesisPath string
+	blockStore  kvstore.KVStore // INVESTIGATE: We may need to create a custom `BlockStore` package in the future
+
+	// TECHDEBT: Need to implement context pooling (for writes), timeouts (for read & writes), etc...
+	writeContext *PostgresContext // only one write context is allowed at a time
+}
+
+const (
+	PersistenceModuleName = "persistence"
+)
+
+func Create(configPath, genesisPath string) (modules.PersistenceModule, error) {
+	m := new(PersistenceModule)
+	c, err := m.InitConfig(configPath)
+	if err != nil {
+		return nil, err
+	}
+	cfg := c.(*types.PersistenceConfig)
+	g, err := m.InitGenesis(genesisPath)
+	if err != nil {
+		return nil, err
+	}
+	genesis := g.(*types.PersistenceGenesisState)
+	conn, err := connectToDatabase(cfg.GetPostgresUrl(), cfg.GetNodeSchema())
+	if err != nil {
+		return nil, err
+	}
+	if err := initializeDatabase(conn); err != nil {
+		return nil, err
+	}
+	conn.Close(context.TODO())
+
+	blockStore, err := initializeBlockStore(cfg.GetBlockStorePath())
+	if err != nil {
+		return nil, err
+	}
+
+	persistenceMod := &PersistenceModule{
+		bus:          nil,
+		postgresURL:  cfg.GetPostgresUrl(),
+		nodeSchema:   cfg.GetNodeSchema(),
+		genesisPath:  genesisPath,
+		blockStore:   blockStore,
+		writeContext: nil,
+	}
+
+	// Determine if we should hydrate the genesis db or use the current state of the DB attached
+	if shouldHydrateGenesis, err := persistenceMod.shouldHydrateGenesisDb(); err != nil {
+		return nil, err
+	} else if shouldHydrateGenesis {
+		// TECHDEBT: reconsider if this is the best place to call `populateGenesisState`. Note that
+		// 		     this forces the genesis state to be reloaded on every node startup until state sync is
+		//           implemented.
+		// NOTE: `populateGenesisState` does not return an error but logs a fatal error if there's a problem
+		persistenceMod.populateGenesisState(genesis)
+	} else {
+		log.Println("Loading state from previous state...")
+	}
+
+	return persistenceMod, nil
+}
+
+func (m *PersistenceModule) InitConfig(pathToConfigJSON string) (config modules.IConfig, err error) {
+	data, err := ioutil.ReadFile(pathToConfigJSON)
+	if err != nil {
+		return
+	}
+	// over arching configuration file
+	rawJSON := make(map[string]json.RawMessage)
+	if err = json.Unmarshal(data, &rawJSON); err != nil {
+		log.Fatalf("[ERROR] an error occurred unmarshalling the %s file: %v", pathToConfigJSON, err.Error())
+	}
+	// persistence specific configuration file
+	config = new(types.PersistenceConfig)
+	err = json.Unmarshal(rawJSON[m.GetModuleName()], config)
+	return
+}
+
+func (m *PersistenceModule) InitGenesis(pathToGenesisJSON string) (genesis modules.IGenesis, err error) {
+	data, err := ioutil.ReadFile(pathToGenesisJSON)
+	if err != nil {
+		return
+	}
+	// over arching configuration file
+	rawJSON := make(map[string]json.RawMessage)
+	if err = json.Unmarshal(data, &rawJSON); err != nil {
+		log.Fatalf("[ERROR] an error occurred unmarshalling the %s file: %v", pathToGenesisJSON, err.Error())
+	}
+	// persistence specific configuration file
+	genesis = new(types.PersistenceGenesisState)
+	err = json.Unmarshal(rawJSON[test_artifacts.GetGenesisFileName(m.GetModuleName())], genesis)
+	return
+}
+
+func (m *PersistenceModule) Start() error {
+	log.Println("Starting persistence module...")
+	return nil
+}
+
+func (m *PersistenceModule) Stop() error {
+	m.blockStore.Stop()
+	return nil
+}
+
+func (m *PersistenceModule) GetModuleName() string {
+	return PersistenceModuleName
+}
+
+func (m *PersistenceModule) SetBus(bus modules.Bus) {
+	m.bus = bus
+}
+
+func (m *PersistenceModule) GetBus() modules.Bus {
+	if m.bus == nil {
+		log.Fatalf("PocketBus is not initialized")
+	}
+	return m.bus
+}
+
+func (m *PersistenceModule) NewRWContext(height int64) (modules.PersistenceRWContext, error) {
+	if m.writeContext != nil && !m.writeContext.conn.IsClosed() {
+		return nil, fmt.Errorf("write context already exists")
+	}
+	conn, err := connectToDatabase(m.postgresURL, m.nodeSchema)
+	if err != nil {
+		return nil, err
+	}
+	tx, err := conn.BeginTx(context.TODO(), pgx.TxOptions{
+		IsoLevel:       pgx.ReadUncommitted,
+		AccessMode:     pgx.ReadWrite,
+		DeferrableMode: pgx.Deferrable, // TODO(andrew): Research if this should be `Deferrable`
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	m.writeContext = &PostgresContext{
+		Height:     height,
+		conn:       conn,
+		tx:         tx,
+		blockstore: m.blockStore,
+	}
+
+	return *m.writeContext, nil
+
+}
+
+func (m *PersistenceModule) NewReadContext(height int64) (modules.PersistenceReadContext, error) {
+	conn, err := connectToDatabase(m.postgresURL, m.nodeSchema)
+	if err != nil {
+		return nil, err
+	}
+	tx, err := conn.BeginTx(context.TODO(), pgx.TxOptions{
+		IsoLevel:       pgx.ReadCommitted,
+		AccessMode:     pgx.ReadOnly,
+		DeferrableMode: pgx.NotDeferrable, // TODO(andrew): Research if this should be `Deferrable`
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return PostgresContext{
+		Height:     height,
+		conn:       conn,
+		tx:         tx,
+		blockstore: m.blockStore,
+	}, nil
+}
+
+func (m *PersistenceModule) ResetContext() error {
+	if m.writeContext != nil {
+		if !m.writeContext.GetTx().Conn().IsClosed() {
+			if err := m.writeContext.Release(); err != nil {
+				log.Println("[TODO][ERROR] Error releasing write context...", err)
+			}
+		}
+		m.writeContext = nil
+	}
+	return nil
+}
+
+func (m *PersistenceModule) GetBlockStore() kvstore.KVStore {
+	return m.blockStore
+}
+
+func initializeBlockStore(blockStorePath string) (kvstore.KVStore, error) {
+	if blockStorePath == "" {
+		return kvstore.NewMemKVStore(), nil
+	}
+	return kvstore.NewKVStore(blockStorePath)
+}
+
+// TODO(drewsky): Simplify and externalize the logic for whether genesis should be populated and
+// move the if logic out of this file.
+func (m *PersistenceModule) shouldHydrateGenesisDb() (bool, error) {
+	checkContext, err := m.NewReadContext(-1)
+	if err != nil {
+		return false, err
+	}
+	defer checkContext.Close()
+
+	maxHeight, err := checkContext.GetLatestBlockHeight()
+	if err == nil || maxHeight == 0 {
+		return true, nil
+	}
+
+	return m.blockStore.Exists(heightToBytes(int64(maxHeight)))
+}
---
+++ persistence/shared_sql.go
@@ -0,0 +1,289 @@
+package persistence
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/pokt-network/pocket/persistence/types"
+	"github.com/pokt-network/pocket/shared/modules"
+)
+
+// IMPROVE(team): Move this into a proto enum. We are not using `iota` for the time being
+// for the purpose of being explicit: https://github.com/pokt-network/pocket/pull/140#discussion_r939731342
+// TODO Cleanup with #149
+const (
+	UndefinedStakingStatus = 0
+	UnstakingStatus        = 1
+	StakedStatus           = 2
+	UnstakedStatus         = 3
+)
+
+func UnstakingHeightToStatus(unstakingHeight int64) int32 {
+	switch unstakingHeight {
+	case -1:
+		return StakedStatus
+	case 0:
+		return UnstakedStatus
+	default:
+		return UnstakingStatus
+	}
+}
+
+func (p *PostgresContext) GetExists(actorSchema types.ProtocolActorSchema, address []byte, height int64) (exists bool, err error) {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return
+	}
+
+	if err = tx.QueryRow(ctx, actorSchema.GetExistsQuery(hex.EncodeToString(address), height)).Scan(&exists); err != nil {
+		return
+	}
+
+	return
+}
+
+func (p *PostgresContext) GetActor(actorSchema types.ProtocolActorSchema, address []byte, height int64) (actor types.BaseActor, err error) {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return
+	}
+	actor, height, err = p.GetActorFromRow(tx.QueryRow(ctx, actorSchema.GetQuery(hex.EncodeToString(address), height)))
+	if err != nil {
+		return
+	}
+
+	return p.GetChainsForActor(ctx, tx, actorSchema, actor, height)
+}
+
+func (p *PostgresContext) GetActorFromRow(row pgx.Row) (actor types.BaseActor, height int64, err error) {
+	err = row.Scan(
+		&actor.Address, &actor.PublicKey, &actor.StakedTokens, &actor.ActorSpecificParam,
+		&actor.OutputAddress, &actor.PausedHeight, &actor.UnstakingHeight,
+		&height)
+	return
+}
+
+func (p *PostgresContext) GetChainsForActor(
+	ctx context.Context,
+	tx pgx.Tx,
+	actorSchema types.ProtocolActorSchema,
+	actor types.BaseActor,
+	height int64) (a types.BaseActor, err error) {
+	if actorSchema.GetChainsTableName() == "" {
+		return actor, nil
+	}
+	rows, err := tx.Query(ctx, actorSchema.GetChainsQuery(actor.Address, height))
+	if err != nil {
+		return actor, err
+	}
+	defer rows.Close()
+
+	var chainAddr string
+	var chainID string
+	var chainEndHeight int64 // unused
+	for rows.Next() {
+		err = rows.Scan(&chainAddr, &chainID, &chainEndHeight)
+		if err != nil {
+			return
+		}
+		if chainAddr != actor.Address {
+			return actor, fmt.Errorf("unexpected address %s, expected %s when reading chains", chainAddr, actor.Address)
+		}
+		actor.Chains = append(actor.Chains, chainID)
+	}
+	return actor, nil
+}
+
+func (p *PostgresContext) InsertActor(actorSchema types.ProtocolActorSchema, actor types.BaseActor) error {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return err
+	}
+
+	height, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(ctx, actorSchema.InsertQuery(
+		actor.Address, actor.PublicKey, actor.StakedTokens, actor.ActorSpecificParam,
+		actor.OutputAddress, actor.PausedHeight, actor.UnstakingHeight, actor.Chains,
+		height))
+	return err
+}
+
+func (p *PostgresContext) UpdateActor(actorSchema types.ProtocolActorSchema, actor types.BaseActor) error {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return err
+	}
+
+	height, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+
+	if _, err = tx.Exec(ctx, actorSchema.UpdateQuery(actor.Address, actor.StakedTokens, actor.ActorSpecificParam, height)); err != nil {
+		return err
+	}
+
+	chainsTableName := actorSchema.GetChainsTableName()
+	if chainsTableName != "" && actor.Chains != nil {
+		if _, err = tx.Exec(ctx, types.NullifyChains(actor.Address, height, chainsTableName)); err != nil {
+			return err
+		}
+		if _, err = tx.Exec(ctx, actorSchema.UpdateChainsQuery(actor.Address, actor.Chains, height)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *PostgresContext) GetActorsReadyToUnstake(actorSchema types.ProtocolActorSchema, height int64) (actors []modules.IUnstakingActor, err error) {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := tx.Query(ctx, actorSchema.GetReadyToUnstakeQuery(height))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		unstakingActor := &types.UnstakingActor{}
+		var addr, output, stakeAmount string
+		if err = rows.Scan(&addr, &stakeAmount, &output); err != nil {
+			return
+		}
+		unstakingActor.SetAddress(addr)
+		unstakingActor.SetStakeAmount(stakeAmount)
+		unstakingActor.SetOutputAddress(output)
+		actors = append(actors, unstakingActor)
+	}
+	return
+}
+
+func (p *PostgresContext) GetActorStatus(actorSchema types.ProtocolActorSchema, address []byte, height int64) (int, error) {
+	var unstakingHeight int64
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return UndefinedStakingStatus, err
+	}
+
+	if err := tx.QueryRow(ctx, actorSchema.GetUnstakingHeightQuery(hex.EncodeToString(address), height)).Scan(&unstakingHeight); err != nil {
+		return UndefinedStakingStatus, err
+	}
+
+	switch {
+	case unstakingHeight == -1:
+		return StakedStatus, nil
+	case unstakingHeight > height:
+		return UnstakingStatus, nil
+	default:
+		return UnstakedStatus, nil
+	}
+}
+
+func (p *PostgresContext) SetActorUnstakingHeightAndStatus(actorSchema types.ProtocolActorSchema, address []byte, unstakingHeight int64) error {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return err
+	}
+
+	height, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(ctx, actorSchema.UpdateUnstakingHeightQuery(hex.EncodeToString(address), unstakingHeight, height))
+	return err
+}
+
+func (p *PostgresContext) GetActorPauseHeightIfExists(actorSchema types.ProtocolActorSchema, address []byte, height int64) (pausedHeight int64, err error) {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return types.DefaultBigInt, err
+	}
+
+	if err := tx.QueryRow(ctx, actorSchema.GetPausedHeightQuery(hex.EncodeToString(address), height)).Scan(&pausedHeight); err != nil {
+		return types.DefaultBigInt, err
+	}
+
+	return pausedHeight, nil
+}
+
+func (p PostgresContext) SetActorStatusAndUnstakingHeightIfPausedBefore(actorSchema types.ProtocolActorSchema, pausedBeforeHeight, unstakingHeight int64) error {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return err
+	}
+
+	currentHeight, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(ctx, actorSchema.UpdateUnstakedHeightIfPausedBeforeQuery(pausedBeforeHeight, unstakingHeight, currentHeight))
+	return err
+}
+
+func (p PostgresContext) SetActorPauseHeight(actorSchema types.ProtocolActorSchema, address []byte, pauseHeight int64) error {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return err
+	}
+
+	currentHeight, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(ctx, actorSchema.UpdatePausedHeightQuery(hex.EncodeToString(address), pauseHeight, currentHeight))
+	return err
+}
+
+func (p PostgresContext) SetActorStakeAmount(actorSchema types.ProtocolActorSchema, address []byte, stakeAmount string) error {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return err
+	}
+
+	currentHeight, err := p.GetHeight()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(ctx, actorSchema.SetStakeAmountQuery(hex.EncodeToString(address), stakeAmount, currentHeight))
+	return err
+}
+
+func (p PostgresContext) GetActorOutputAddress(actorSchema types.ProtocolActorSchema, operatorAddr []byte, height int64) ([]byte, error) {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return nil, err
+	}
+
+	var outputAddr string
+	if err := tx.QueryRow(ctx, actorSchema.GetOutputAddressQuery(hex.EncodeToString(operatorAddr), height)).Scan(&outputAddr); err != nil {
+		return nil, err
+	}
+
+	return hex.DecodeString(outputAddr)
+}
+
+func (p PostgresContext) GetActorStakeAmount(actorSchema types.ProtocolActorSchema, address []byte, height int64) (string, error) {
+	ctx, tx, err := p.GetCtxAndTx()
+	if err != nil {
+		return "", err
+	}
+
+	var stakeAmount string
+	if err := tx.QueryRow(ctx, actorSchema.GetStakeAmountQuery(hex.EncodeToString(address), height)).Scan(&stakeAmount); err != nil {
+		return "", err
+	}
+	return stakeAmount, nil
+}
