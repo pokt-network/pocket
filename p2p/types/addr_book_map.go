b5f317afc58300508e352b6dbda8c77162f5435f
Alessandro De Blasis <alex@deblasis.net>
2022-10-04 18:11:46 +0100

    refactor(P2P): addrBook and addrBookMap are back together

--- p2p/types/addr_book_map.go
+++
@@ -1,6 +0,0 @@
-package types
-
-// AddrBookMap maps p2p addresses to their respective NetworkPeer.
-//
-// Since maps cannot be sorted arbitrarily in Go, to achieve sorting, we need to rely on `addrList` which is a slice of addresses/strings and therefore we can sort it the way we want.
-type AddrBookMap map[string]*NetworkPeer
