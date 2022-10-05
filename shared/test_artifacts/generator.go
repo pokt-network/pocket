df71b6c3f40e30327c2a7a7977c393ee14d1dda3
Andrew Nguyen <amnguyen1@mail.usf.edu>
2022-10-05 09:01:04 -0400

    Update shared/test_artifacts/generator.go
    Co-authored-by: Daniel Olshansky <olshansky@pokt.network>

--- shared/test_artifacts/generator.go
+++ shared/test_artifacts/generator.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// INVESTIGATE It seems improperly scoped that the modules have to have shared 'testing' code
+// INVESTIGATE: It seems improperly scoped that the modules have to have shared 'testing' code
 //  It might be an inevitability to have shared testing code, but would like more eyes on it.
 //  Look for opportunities to make testing completely modular
 
