f7b8d2377541eacbb8549a271218d8367e9bb431
Alessandro De Blasis <alex@deblasis.net>
2022-10-05 15:21:36 +0100

    Update p2p/raintree/target.go
    Co-authored-by: Daniel Olshansky <olshansky.daniel@gmail.com>

--- p2p/raintree/target.go
+++ p2p/raintree/target.go
@@ -11,7 +11,7 @@ type target struct {
 	address    cryptoPocket.Address
 	serviceUrl string
 
-	level                  uint32  // the level of the node in the RainTree tree
+	level                  uint32  // the level of the node in the RainTree tree (inverse of height in traditional computer science)
 	percentage             float64 // the target percentage within the peer list used to select this as a target
 	addrBookLengthAtHeight int     // the length of the addr book at the specified block height and tree level
 	index                  int     // the index of this target peer within the addr book at the specific height and level
