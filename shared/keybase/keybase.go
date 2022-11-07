2598fe65e3a224e557f38ad725ed059cd0c7f720
Jason You <Jasonyou1995@users.noreply.github.com>
2022-11-07 17:33:02 -0500

    Adding the keybase package

---
+++ shared/keybase/keybase.go
@@ -0,0 +1,10 @@
+package keybase
+
+// TODO: Mini-keybase implementation
+// 1. Creat and Store Keys
+// 2. Delete Keys
+// 3. Document the Key architecture
+//
+// [Note]
+// Using `/shared/crypto/ed25519` as the Public Key library
+// Using Badger.db or Level.db
