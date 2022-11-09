c51e6e004b85884fb9a0d5786bd05bf58de595e4
Jason You <Jasonyou1995@users.noreply.github.com>
2022-11-09 12:28:37 -0500

    Update delete command comments

--- shared/keys/cmd/delete.go
+++ shared/keys/cmd/delete.go
@@ -29,14 +29,16 @@ import (
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "delete <key> ...",
+	Short: "Delete the given key from the keystore",
+	Long: `Delete the public key from the backend keystore
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Note:
+		This CLI does not delete private key
+
+'
+`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("delete called")
 	},
