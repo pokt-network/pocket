2042e8c4ea8ff7d0b84315bcd658659e96044ad9
Jason You <Jasonyou1995@users.noreply.github.com>
2022-11-11 13:25:49 -0500

    Updating reviewed changes

--- shared/keys/cmd/root.go
+++ shared/keys/cmd/root.go
@@ -42,13 +42,8 @@ Currently we have the following functions:
 	create			Creating a key and string the key locally
 	delete			Delete a stored key
 `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//Run: func(cmd *cobra.Command, args []string) {},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -59,14 +54,10 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// Persistent Flags
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.keys.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	// Local flags
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
