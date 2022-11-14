7d300b99b00c5c169030c7114b4301c8fed71b3a
Alessandro De Blasis <alex@deblasis.net>
2022-11-14 18:16:15 +0000

    docs(Messaging): godocs

--- shared/messaging/utils.go
+++ shared/messaging/utils.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
+// PackMessage returns a *PocketEnvelope after having packed the message supplied as an argument
 func PackMessage(message proto.Message) (*PocketEnvelope, error) {
 	anyMsg, err := anypb.New(message)
 	if err != nil {
@@ -16,6 +17,7 @@ func PackMessage(message proto.Message) (*PocketEnvelope, error) {
 	return &PocketEnvelope{Content: anyMsg}, nil
 }
 
+// MustPackMessage returns a *PocketEnvelope after having packed the message supplied as an argument and fails fatally in case of errors
 func MustPackMessage(message proto.Message) *PocketEnvelope {
 	anyMsg, err := anypb.New(message)
 	if err != nil {
@@ -24,6 +26,7 @@ func MustPackMessage(message proto.Message) *PocketEnvelope {
 	return &PocketEnvelope{Content: anyMsg}
 }
 
+// UnpackMessage extracts the message inside the PocketEnvelope decorating it with typing information
 func UnpackMessage(envelope *PocketEnvelope) (protoreflect.ProtoMessage, error) {
 	anyMsg := envelope.Content
 	msg, err := anypb.UnmarshalNew(anyMsg, proto.UnmarshalOptions{})
