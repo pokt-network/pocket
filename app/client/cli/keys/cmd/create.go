471a369e378e804d4d2f2e33d340e8e5631a80d6
Jason You <Jasonyou1995@users.noreply.github.com>
2022-11-14 22:58:09 -0500

    Implementing core security functions
    encrypt/decrypt with AES
    generateKeyFromPassPhrase
    generateRandomKey

--- app/client/cli/keys/cmd/create.go
+++ app/client/cli/keys/cmd/create.go
@@ -4,12 +4,17 @@ package cmd
 
 import (
 	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"github.com/cosmos/cosmos-sdk/client/input"
 	"github.com/cosmos/go-bip39"
 	"github.com/spf13/cobra"
 	"github.com/syndtr/goleveldb/leveldb"
+	"io"
 	cryptoPocket "keys/crypto"
 	"log"
 )
@@ -153,4 +158,130 @@ func printKey(keystore key) error {
 	log.Printf("%s\n", output)
 
 	return nil
+}
+
+// Encryption function that takes a 32 bytes hex string key
+func encrypt(stringToEncrypt string, keyString string) (string, error) {
+	var err error
+
+	// convert key and plaintext to bytes
+	var key, plaintext []byte
+	if key, err = hex.DecodeString(keyString); err != nil {
+		return "", err
+	}
+	plaintext = []byte(stringToEncrypt)
+
+	if len(key) != 32 {
+		return "", errors.New("key size much be 32 bytes for AES-256 security level")
+	}
+
+	// create a new cipher Block from the key
+	var block cipher.Block
+	if block, err = aes.NewCipher(key); err != nil {
+		return "", err
+	}
+
+	// create a new GCM (Galois Counter Mode)
+	// https://en.wikipedia.org/wiki/Galois/Counter_Mode
+	// https://golang.org/pkg/crypto/cipher/#NewGCM
+	var aesGCM cipher.AEAD
+	if aesGCM, err = cipher.NewGCM(block); err != nil {
+		return "", err
+	}
+
+	// create nonce from GCM (nonce size = 12)
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	// encrypt the data using the AEA GCM Seal function
+	enc := aesGCM.Seal(nonce, nonce, plaintext, nil)
+
+	return hex.EncodeToString(enc), nil
+}
+
+// Decryption function that takes a 32 bytes hex string key
+func decrypt(encryptedString string, keyString string) (string, error) {
+	var err error
+
+	// convert key and encrypted data to bytes
+	var key, enc []byte
+	if key, err = hex.DecodeString(keyString); err != nil {
+		return "", err
+	}
+	if enc, err = hex.DecodeString(encryptedString); err != nil {
+		return "", err
+	}
+
+	if len(key) != 32 {
+		return "", errors.New("key size much be 32 bytes for AES-256 security level")
+	}
+
+	// create a new cipher Block from the key
+	var block cipher.Block
+	if block, err = aes.NewCipher(key); err != nil {
+		return "", err
+	}
+
+	// create a new GCM (Galois Counter Mode)
+	var aesGCM cipher.AEAD
+	if aesGCM, err = cipher.NewGCM(block); err != nil {
+		return "", err
+	}
+
+	// get nonce size and extract nonce from the prefix of the ciphertext
+	nonceSize := aesGCM.NonceSize()
+	nonce, ciphertext := enc[:nonceSize], enc[nonceSize:]
+
+	// decrypt the data
+	var plaintext []byte
+	if plaintext, err = aesGCM.Open(nil, nonce, ciphertext, nil); err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+/* Generating strong random passphrase for user
+In most use cases `generateKeyFromPassPhrase` should be sufficient
+
+Warning: this passphrase is not saved by default.
+Warning: recommend to store this pass phase in safe vault for users in case they lost it.
+
+*/
+func generateRandomKey() (string, error) {
+	// generate a random 32 byte key for AES-256
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	// Keep the key as a secrete! (User only need to remember their passphrase, could be "")
+	passphrase := hex.EncodeToString(bytes)
+	log.Printf("Generated user passphrase: %s\n", passphrase)
+	log.Println("Warning: write this passphrase down!")
+	log.Println("Warning: this passphrase will disappear!")
+	return passphrase, nil
+}
+
+/* Generate a 32 byte hash digest as key based on the user provided pass phrase.
+   SHA3-256 only has 128-bit collision resistance, because its output length is 32 bytes.
+
+- Input
+	- stringToHash: string of any length
+
+*/
+func generateKeyFromPassPhrase(passphrase string) (string, error) {
+	// generate a strong random secret key that is at least 32 bytes long
+	buf := []byte(passphrase)
+	h := cryptoPocket.SHA3Hash(buf)
+
+	// check key length
+	if len(h) != 32 {
+		return "", errors.New("key generation error: hash digest must be 32 bytes long")
+	}
+
+	// return the 32 bytes key hex string
+	return hex.EncodeToString(h), nil
 }
