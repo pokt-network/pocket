3dc5ffd3e203a1af0585eab46ce45b67605a5e2d
Alessandro De Blasis <alex@deblasis.net>
2022-12-04 21:12:06 +0000

    feat(P2P): HandleEvent

--- shared/modules/p2p_module.go
+++ shared/modules/p2p_module.go
@@ -11,6 +11,8 @@ type P2PModule interface {
 	Module
 	ConfigurableModule
 
+	HandleEvent(*anypb.Any) error
+
 	Broadcast(msg *anypb.Any) error
 	Send(addr cryptoPocket.Address, msg *anypb.Any) error
 	GetAddress() (cryptoPocket.Address, error)
