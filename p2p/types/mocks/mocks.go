a7b1d96c7c01e974eeb6ad054def04b0fee2c73b
Alessandro De Blasis <alex@deblasis.net>
2022-12-08 21:37:53 +0000

    chore(mocks): added explainer comment

--- p2p/types/mocks/mocks.go
+++ p2p/types/mocks/mocks.go
@@ -1,1 +1,3 @@
 package mock_types
+
+// This file is in place to declare the package for dynamically generated mocks
--- shared/modules/mocks/mocks.go
+++ shared/modules/mocks/mocks.go
@@ -1,1 +1,3 @@
 package mock_modules
+
+// This file is in place to declare the package for dynamically generated mocks
