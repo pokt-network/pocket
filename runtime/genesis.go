d4d7be329909fa3589188535546b6d2bf24cd08c
Alessandro De Blasis <alex@deblasis.net>
2022-12-19 23:54:30 +0000

    chore(runtime): 🧹

--- runtime/genesis.go
+++ runtime/genesis.go
@@ -7,18 +7,6 @@ import (
 	"github.com/pokt-network/pocket/runtime/genesis"
 )
 
-// type runtimeGenesis struct {
-// 	GenesisState genesis.GenesisState
-// }
-
-// func NewGenesis(
-// 	genesisState genesis.GenesisState,
-// ) *runtimeGenesis {
-// 	return &runtimeGenesis{
-// 		GenesisState: genesisState,
-// 	}
-// }
-
 func parseGenesisJSON(genesisPath string) (g *genesis.GenesisState, err error) {
 	data, err := os.ReadFile(genesisPath)
 	if err != nil {
