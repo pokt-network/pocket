f94d52933b88acdf15eb5f5a65bedd0df3bac5e7
Alessandro De Blasis <alex@deblasis.net>
2022-12-21 18:04:45 +0000

    chore(P2P): nits

--- p2p/types/addr_book.go
+++ p2p/types/addr_book.go
@@ -1,8 +1,8 @@
 package types
 
-import coreTypes "github.com/pokt-network/pocket/shared/core/types"
-
 //go:generate mockgen -source=$GOFILE -destination=./mocks/addrbook_provider_mock.go github.com/pokt-network/pocket/p2p/types AddrBookProvider
+
+import coreTypes "github.com/pokt-network/pocket/shared/core/types"
 
 // AddrBook is a way of representing NetworkPeer sets
 type AddrBook []*NetworkPeer
