77586e4b94a7d32ce5e300cc9c8d25a9933885da
Alessandro De Blasis <alex@deblasis.net>
2022-12-22 23:15:08 +0000

    feat(P2P): isClientOnly flag

--- shared/modules/types.go
+++ shared/modules/types.go
@@ -56,6 +56,7 @@ type P2PConfig interface {
 	GetUseRainTree() bool
 	GetIsEmptyConnectionType() bool // TODO : make enum
 	GetMaxMempoolCount() uint64
+	GetIsClientOnly() bool
 }
 
 type TelemetryConfig interface {
