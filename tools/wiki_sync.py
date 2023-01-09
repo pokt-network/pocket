9dbf718acd4b1ce7d01cef1b632eedf865cd120e
profishional <profishcrypto@gmail.com>
2023-01-09 15:41:24 +1100

    better vars, comments, new vars

--- tools/wiki_sync.py
+++ tools/wiki_sync.py
@@ -7,6 +7,7 @@ from typing import Dict, List
 
 WIKI_DIR = "tools/wiki"
 TEMP_WIKI = "tools/temp_wiki"
+WIKI_TAG = "GITHUB_WIKI"  # text included in the Github Wiki formatting comment
 
 @dataclass
 class DocumentationFile:
@@ -30,25 +31,29 @@ def get_file_to_wiki_comment(file_path: Path) -> Path:
     # gets the last line of of the file and parses it for the format
     with open(file_path) as file:
         wiki_comment = file.readlines()[-1]
-        if "GITHUB_WIKI" in wiki_comment:
-            mapping_format = wiki_comment.split(" ")[2].strip()
+        if WIKI_TAG not in wiki_comment:
+            return None
+        mapping_format = wiki_comment.split(" ")[2].strip()
         return Path(mapping_format)
 
 
 def categorize_paths() -> Dict[str, List[DocumentationFile]]:
+    # Uses helper functions to find .md files and paths, to parse the Github Wiki formatting spec
+    # Creates a map of the formatting spec to document information
+    # Map is used to create a sidebar file, and a copy paste the files from the Pocket Repository to Github Wiki
+
     sidebar = defaultdict(list)
-    paths = list()
     for path in get_all_markdown_file_paths():
         wiki_path_format = get_file_to_wiki_comment(path)
 
-        # doc info parsing
-        dirname = os.path.dirname(wiki_path_format)
-        file = os.path.basename(wiki_path_format)
-        categories = dirname.split("/")
-        display_name = " ".join([s.title() for s in file.split("_")])
-        filename = f"{categories[-1].title()} {display_name.title()}"
+        if wiki_path_format:
+            dirname = os.path.dirname(wiki_path_format)
+            file = os.path.basename(wiki_path_format)
+            categories = dirname.split("/")
+            display_name = " ".join([s.title() for s in file.split("_")])
+            filename = f"{categories[-1].title()} {display_name.title()}"
 
-        sidebar[dirname].append(DocumentationFile(filename, display_name, path))
+            sidebar[dirname].append(DocumentationFile(filename, display_name, path))
 
     return sidebar
 
@@ -57,20 +62,21 @@ def write_sidebar_file(sidebar: Dict[str, List[DocumentationFile]]) -> None:
     sidebar_format = "'''Contents'''\n"
     sidebar_format += "*'''[[ Home | Home ]]'''\n"
 
-    level = 1
+    DEPTH = 1  # helps track the level of nesting in the wiki sidebar
     for category, doc_files in sidebar.items():
         if category == "home":
             continue
 
         subcategories = category.split("/")
         for subcategory in subcategories:
-            sidebar_format += ("*" * level) + f"'''{subcategory}'''\n"
-            level += 1
+            sidebar_format += ("*" * DEPTH) + f"'''{subcategory.title()}'''\n"
+            DEPTH += 1
 
         for doc in doc_files:
-            sidebar_format += ("*" * level) + f"[[ {doc.file_name} | {doc.display_name} ]]\n"
+            sidebar_format += ("*" * DEPTH) + f"[[ {doc.file_name} | {doc.display_name} ]]\n"
 
-        level = 1
+        # reset depth for the next category
+        DEPTH = 1
 
     with open(f"{WIKI_DIR}/_Sidebar.mediawiki", "w") as f:
         f.write(sidebar_format)
@@ -98,20 +104,21 @@ def run_wiki_migration():
     repo_name = os.environ['REPOSITORY_NAME']
 
     # init, pull, delete
-    subprocess.call(["git", "init"], cwd=f"{TEMP_WIKI}")
-    subprocess.call(["git", "config", "user.name",  f"{user_name}"], cwd=f"{TEMP_WIKI}")
-    subprocess.call(["git", "config", "user.email", f"{user_email}"], cwd=f"{TEMP_WIKI}")
-    subprocess.call(["git", "pull", f"https://{secret}@github.com/{owner}/{repo_name}.wiki.git"], cwd=f"{TEMP_WIKI}")
+    subprocess.call(["git", "init"], cwd=TEMP_WIKI)
+    subprocess.call(["git", "config", "user.name",  f"{user_name}"], cwd=TEMP_WIKI)
+    subprocess.call(["git", "config", "user.email", f"{user_email}"], cwd=TEMP_WIKI)
+    subprocess.call(["git", "pull", f"https://{secret}@github.com/{owner}/{repo_name}.wiki.git"], cwd=TEMP_WIKI)
 
     # sync the new and old wiki files
     subprocess.call(["rsync", "-av",  "--delete", "tools/wiki/", "tools/temp_wiki", "--exclude", ".git"])
 
-    # add, commit and push
-    subprocess.call(["git", "add", "."], cwd=f"{TEMP_WIKI}")
-    subprocess.call(["git", "commit", "-m", 'update wiki content'], cwd=f"{TEMP_WIKI}")
+    # add, config, commit and push
+    subprocess.call(["git", "add", "."], cwd=TEMP_WIKI)
+    sha_hash = subprocess.check_output(["git", "rev-list", "--max-count=1", "HEAD"], cwd=TEMP_WIKI).decode().strip()
+    subprocess.call(["git", "commit", "-m", f"update wiki content - sha: {sha_hash}"], cwd=TEMP_WIKI)
     subprocess.call(["git", "remote", "add", "master", \
-        f"https://{secret}@github.com/{owner}/{repo_name}.wiki.git"], cwd=f"{TEMP_WIKI}")
-    subprocess.call(["git", "push", "--set-upstream", "master", "master"], cwd=f"{TEMP_WIKI}")
+        f"https://{secret}@github.com/{owner}/{repo_name}.wiki.git"], cwd=TEMP_WIKI)
+    subprocess.call(["git", "push", "--set-upstream", "master", "master"], cwd=TEMP_WIKI)
 
 
 
