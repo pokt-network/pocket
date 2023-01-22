f169b688cd48d77fdbc8d2731e69924063c9b2bf
Harry Law <hrryslw@pm.me>
2023-01-22 22:44:57 +0000

    Add signature tests

--- app/client/keybase/keybase_test.go
+++ app/client/keybase/keybase_test.go
@@ -9,8 +9,10 @@ import (
 )
 
 const (
-	testPassphrase    = "testingtesting123"
+	testPrivString    = "5ff9e3522eecfd3ccd42e187537bca9f2b9ac7f35d7074573e789ed7ec49870d4479e7524be67bb1f435836afbef4592bfc75afbf52e51495f9ac6d141ddbc02"
+	testPassphrase    = "Testing@Testing123"
 	testNewPassphrase = "321gnitsetgnitset"
+	testTx            = "79fca587bbcfd5da86d73e1d849769017b1c91cc8177dec0fc0e3e0d345f2b35"
 )
 
 var (
@@ -34,30 +36,6 @@ func TestKeybase_CreateNewKey(t *testing.T) {
 	kp := keypairs[0]
 	require.Equal(t, len(addr), crypto.AddressLen)
 	require.Equal(t, addr, kp.GetAddressBytes())
-}
-
-func TestKeybase_CreateNewKeyFromBytes(t *testing.T) {
-	db, err := initDB()
-	defer db.Stop()
-	require.NoError(t, err)
-
-	err = db.CreateFromBytes(testKey.Bytes(), testPassphrase)
-	require.NoError(t, err)
-
-	addresses, keypairs, err := db.GetAll()
-	require.NoError(t, err)
-	require.Equal(t, len(addresses), 1)
-	require.Equal(t, len(keypairs), 1)
-
-	addr := addresses[0]
-	kp := keypairs[0]
-	require.Equal(t, len(addr), crypto.AddressLen)
-	require.Equal(t, addr, kp.GetAddressBytes())
-	require.Equal(t, kp.GetAddressString(), testKey.Address().String())
-
-	privKey, err := kp.Unarmour(testPassphrase)
-	require.NoError(t, err)
-	require.Equal(t, privKey.String(), testKey.String())
 }
 
 func TestKeybase_CreateNewKeyFromString(t *testing.T) {
@@ -287,6 +265,53 @@ func TestKeybase_DeleteKeyWrongPassphrase(t *testing.T) {
 	require.ErrorIs(t, err, ErrorWrongPassphrase)
 }
 
+func TestKeybase_SignMessage(t *testing.T) {
+	db, err := initDB()
+	defer db.Stop()
+	require.NoError(t, err)
+
+	pk, err := createTestKeyFromString(testPrivString)
+	require.NoError(t, err)
+
+	err = db.CreateFromString(testPrivString, testPassphrase)
+	require.NoError(t, err)
+
+	privKey, err := db.GetPrivKey(pk.Address().String(), testPassphrase)
+	require.NoError(t, err)
+
+	txBz, err := hex.DecodeString(testTx)
+	require.NoError(t, err)
+
+	signedMsg, err := db.Sign(privKey.Address().String(), testPassphrase, txBz)
+	require.NoError(t, err)
+
+	verified, err := db.Verify(privKey.Address().String(), txBz, signedMsg)
+	require.NoError(t, err)
+	require.Equal(t, verified, true)
+}
+
+func TestKeybase_SignMessageWrongPassphrase(t *testing.T) {
+	db, err := initDB()
+	defer db.Stop()
+	require.NoError(t, err)
+
+	pk, err := createTestKeyFromString(testPrivString)
+	require.NoError(t, err)
+
+	err = db.CreateFromString(testPrivString, testPassphrase)
+	require.NoError(t, err)
+
+	privKey, err := db.GetPrivKey(pk.Address().String(), testPassphrase)
+	require.NoError(t, err)
+
+	txBz, err := hex.DecodeString(testTx)
+	require.NoError(t, err)
+
+	signedMsg, err := db.Sign(privKey.Address().String(), testNewPassphrase, txBz)
+	require.ErrorIs(t, err, ErrorWrongPassphrase)
+	require.Nil(t, signedMsg)
+}
+
 func initDB() (Keybase, error) {
 	db, err := NewKeybaseInMemory("")
 	if err != nil {
@@ -297,4 +322,12 @@ func initDB() (Keybase, error) {
 
 func createTestKey() (crypto.PrivateKey, error) {
 	return crypto.GeneratePrivateKey()
+}
+
+func createTestKeyFromString(str string) (crypto.PrivateKey, error) {
+	bz, err := hex.DecodeString(str)
+	if err != nil {
+		return nil, err
+	}
+	return crypto.NewPrivateKeyFromBytes(bz)
 }
