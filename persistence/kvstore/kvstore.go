8d1512cfc28c09adf6efd8ea679a47123625141a
gokutheengineer <engineerzubizaretta@gmail.com>
2023-01-26 17:37:35 +0300

    clean kvstore, mocks in persistence

--- persistence/kvstore/kvstore.go
+++ persistence/kvstore/kvstore.go
@@ -1,7 +1,5 @@
 package kvstore
 
-//go:generate mockgen -source=$GOFILE -destination=../types/mocks/block_store_mock.go github.com/pokt-network/pocket/persistence/types KVStore
-
 import (
 	"errors"
 	"log"
