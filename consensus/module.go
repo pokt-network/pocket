2dc53067efe8f57359b9b9319046d952d33284e2
goku <118421317+gokutheengineer@users.noreply.github.com>
2023-01-31 14:55:47 +0300

    Update consensus/module.go
    Co-authored-by: Daniel Olshansky <olshansky@pokt.network>

--- consensus/module.go
+++ consensus/module.go
@@ -125,7 +125,7 @@ func (m *consensusModule) GetNodeIdFromNodeAddress(peerId string) (uint64, error
 
 	validators, err := m.getValidatorsAtHeight(m.CurrentHeight())
 	if err != nil {
-		//REFACTOR: As per issue #434, once new id is sorted out, this return statement must be changed
+		// REFACTOR(#434): As per issue #434, once the new id is sorted out, this return statement must be changed
 		return 0, err
 	}
 
