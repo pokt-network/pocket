960121018934d6e2ab292dafbabaed54750de821
Alessandro De Blasis <alex@deblasis.net>
2023-02-01 16:26:01 +0000

    refactor(shared): generic_fifo_list refactored with equality, +tests

--- shared/mempool/list/generic_fifo_list.go
+++ shared/mempool/list/generic_fifo_list.go
@@ -2,47 +2,32 @@ package list
 
 import (
 	"container/list"
-	"encoding/json"
 	"fmt"
-	"log"
 	"sync"
 )
 
-type GenericFIFOList[TData any] struct {
-	set      map[string]struct{}
+type GenericFIFOList[TData comparable] struct {
 	queue    *list.List
 	capacity int
 	m        sync.Mutex
 
-	indexerFn     func(any) string
+	isEqual       func(TData, TData) bool
 	isOverflowing func(*GenericFIFOList[TData]) bool
 
-	onAdd       func(TData, *GenericFIFOList[TData])
-	onRemove    func(TData, *GenericFIFOList[TData])
-	onCollision func(TData, *GenericFIFOList[TData])
+	onAdd    func(TData, *GenericFIFOList[TData])
+	onRemove func(TData, *GenericFIFOList[TData])
 }
 
-func NewGenericFIFOList[TData any](capacity int, options ...func(*GenericFIFOList[TData])) *GenericFIFOList[TData] {
+func NewGenericFIFOList[TData comparable](capacity int, options ...func(*GenericFIFOList[TData])) *GenericFIFOList[TData] {
 	gfs := &GenericFIFOList[TData]{
-		set:      make(map[string]struct{}, capacity),
-		queue:    list.New(),
-		capacity: capacity,
-		indexerFn: func(item any) string {
-			// INVESTIGATE: is this the best way to do this? we could use our codec package but we cannot assume that item is a protoMessage
-			bytes, err := json.Marshal(item)
-			if err != nil {
-				log.Fatalf("failed to marshal item: %v", err)
-			}
-			return string(bytes)
-		},
+		queue:         list.New(),
+		capacity:      capacity,
 		isOverflowing: defaultIsOverflowing[TData],
+		isEqual:       defaultIsEqual[TData],
 		onAdd: func(item TData, g *GenericFIFOList[TData]) {
 			// noop
 		},
 		onRemove: func(item TData, g *GenericFIFOList[TData]) {
-			// noop
-		},
-		onCollision: func(item TData, g *GenericFIFOList[TData]) {
 			// noop
 		},
 	}
@@ -58,20 +43,11 @@ func (g *GenericFIFOList[TData]) Push(item TData) error {
 	g.m.Lock()
 	defer g.m.Unlock()
 
-	index := g.indexerFn(item)
-	if _, ok := g.set[index]; ok {
-		if g.onCollision != nil {
-			g.onCollision(item, g)
-		}
-	}
-
-	g.set[index] = struct{}{}
 	g.queue.PushBack(item)
 	g.onAdd(item, g)
 
 	if g.isOverflowing != nil && g.isOverflowing(g) {
 		front := g.queue.Front()
-		delete(g.set, g.indexerFn(front.Value.(TData)))
 		g.queue.Remove(front)
 		g.onRemove(item, g)
 	}
@@ -88,7 +64,6 @@ func (g *GenericFIFOList[TData]) Pop() (v TData, err error) {
 
 	front := g.queue.Front()
 	item := front.Value.(TData)
-	delete(g.set, g.indexerFn(item))
 	g.queue.Remove(front)
 	g.onRemove(item, g)
 	return item, nil
@@ -98,15 +73,11 @@ func (g *GenericFIFOList[TData]) Remove(item TData) {
 	g.m.Lock()
 	defer g.m.Unlock()
 
-	itemIndex := g.indexerFn(item)
-	if _, ok := g.set[itemIndex]; ok {
-		delete(g.set, itemIndex)
-		for e := g.queue.Front(); e != nil; e = e.Next() {
-			if g.indexerFn(e.Value.(TData)) == itemIndex {
-				g.queue.Remove(e)
-				g.onRemove(item, g)
-				break
-			}
+	for e := g.queue.Front(); e != nil; e = e.Next() {
+		if g.isEqual(e.Value.(TData), item) {
+			g.queue.Remove(e)
+			g.onRemove(item, g)
+			break
 		}
 	}
 }
@@ -122,7 +93,6 @@ func (g *GenericFIFOList[TData]) Clear() {
 	g.m.Lock()
 	defer g.m.Unlock()
 
-	g.set = make(map[string]struct{}, g.capacity)
 	g.queue.Init()
 }
 
@@ -137,16 +107,13 @@ func (g *GenericFIFOList[TData]) Contains(item TData) bool {
 	g.m.Lock()
 	defer g.m.Unlock()
 
-	_, ok := g.set[g.indexerFn(item)]
-	return ok
-}
+	for e := g.queue.Front(); e != nil; e = e.Next() {
+		if g.isEqual(e.Value.(TData), item) {
+			return true
+		}
+	}
 
-func (g *GenericFIFOList[TData]) ContainsIndex(index string) bool {
-	g.m.Lock()
-	defer g.m.Unlock()
-
-	_, ok := g.set[index]
-	return ok
+	return false
 }
 
 func (g *GenericFIFOList[TData]) GetAll() []TData {
@@ -162,38 +129,36 @@ func (g *GenericFIFOList[TData]) GetAll() []TData {
 
 // Options
 
-func WithIndexerFn[TData any](fn func(any) string) func(*GenericFIFOList[TData]) {
-	return func(g *GenericFIFOList[TData]) {
-		g.indexerFn = fn
-	}
-}
-
-func WithCustomIsOverflowingFn[TData any](fn func(*GenericFIFOList[TData]) bool) func(*GenericFIFOList[TData]) {
+func WithCustomIsOverflowingFn[TData comparable](fn func(*GenericFIFOList[TData]) bool) func(*GenericFIFOList[TData]) {
 	return func(g *GenericFIFOList[TData]) {
 		g.isOverflowing = fn
 	}
 }
 
-func WithOnAdd[TData any](fn func(TData, *GenericFIFOList[TData])) func(*GenericFIFOList[TData]) {
+func WithOnAdd[TData comparable](fn func(TData, *GenericFIFOList[TData])) func(*GenericFIFOList[TData]) {
 	return func(g *GenericFIFOList[TData]) {
 		g.onAdd = fn
 	}
 }
 
-func WithOnRemove[TData any](fn func(TData, *GenericFIFOList[TData])) func(*GenericFIFOList[TData]) {
+func WithOnRemove[TData comparable](fn func(TData, *GenericFIFOList[TData])) func(*GenericFIFOList[TData]) {
 	return func(g *GenericFIFOList[TData]) {
 		g.onRemove = fn
 	}
 }
 
-func WithOnCollision[TData any](fn func(TData, *GenericFIFOList[TData])) func(*GenericFIFOList[TData]) {
+func WithIsEqual[TData comparable](fn func(TData, TData) bool) func(*GenericFIFOList[TData]) {
 	return func(g *GenericFIFOList[TData]) {
-		g.onCollision = fn
+		g.isEqual = fn
 	}
 }
 
 // private methods
 
-func defaultIsOverflowing[TData any](g *GenericFIFOList[TData]) bool {
+func defaultIsOverflowing[TData comparable](g *GenericFIFOList[TData]) bool {
 	return g.queue.Len() > g.capacity
+}
+
+func defaultIsEqual[TData comparable](a, b TData) bool {
+	return a == b
 }
