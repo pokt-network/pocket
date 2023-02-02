23b2d9b9cf51de0ae9c4a8a405f8e1873b24da45
Alessandro De Blasis <alex@deblasis.net>
2023-02-02 19:24:29 +0000

    feat(localnet): cluster-manager validator keys from secret - WIP

--- build/localnet/cluster-manager/main.go
+++ build/localnet/cluster-manager/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 
+	"github.com/pokt-network/pocket/shared/crypto"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+)
+
+var (
+	validatorKeysMap = make(map[string]crypto.Ed25519PrivateKey)
 )
 
 func main() {
@@ -22,6 +29,8 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	fetchValidatorPrivateKeys(clientset)
 
 	watcher, err := clientset.CoreV1().Services("default").Watch(context.TODO(), metav1.ListOptions{})
 	if err != nil {
@@ -41,14 +50,53 @@ func main() {
 		switch event.Type {
 		case watch.Added:
 			fmt.Printf("Validator %s added\n", service.Name)
-		case watch.Modified:
-			fmt.Printf("Validator %s modified\n", service.Name)
+			fmt.Printf("Staking Validator with Address: %s\n", validatorKeysMap[extractValidatorId(service.Name)].Address())
 		case watch.Deleted:
 			fmt.Printf("Validator %s deleted\n", service.Name)
+			fmt.Printf("Unstaking Validator with Address: %s\n", validatorKeysMap[extractValidatorId(service.Name)].Address())
 		}
+	}
+}
+
+func fetchValidatorPrivateKeys(clientset *kubernetes.Clientset) {
+	private_keys_secret, err := clientset.CoreV1().Secrets("default").Get(context.TODO(), "v1-localnet-validators-private-keys", metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for key, value := range private_keys_secret.Data {
+		pk, err := parsePrivateKey(bytes.NewReader(value))
+		if err != nil {
+			panic(err)
+		}
+		validatorKeysMap[key] = pk
 	}
 }
 
 func isValidator(service *v1.Service) bool {
 	return service.Labels["v1-purpose"] == "validator"
+}
+
+func extractValidatorId(validatorName string) string {
+	if len(validatorName) >= 3 {
+		return validatorName[len(validatorName)-3:]
+	}
+	return validatorName
+}
+
+func parsePrivateKey(reader io.Reader) (pk crypto.Ed25519PrivateKey, err error) {
+	if reader == nil {
+		return nil, fmt.Errorf("cannot read from reader %v", reader)
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+
+	priv := &crypto.Ed25519PrivateKey{}
+	err = priv.UnmarshalText(buf.Bytes())
+	if err != nil {
+		return
+	}
+	pk = priv.Bytes()
+	return
 }
