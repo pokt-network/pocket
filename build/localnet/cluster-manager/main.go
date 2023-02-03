f0eaf434b0d32cf1f2549929a9fc3b16a1f70b25
Alessandro De Blasis <alex@deblasis.net>
2023-02-03 14:35:41 +0000

    feat(localnet): cluster-manager dogfoods the CLI to stake/unstake

--- build/localnet/cluster-manager/main.go
+++ build/localnet/cluster-manager/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/pokt-network/pocket/shared/crypto"
 	v1 "k8s.io/api/core/v1"
@@ -16,21 +18,26 @@ import (
 
 var (
 	validatorKeysMap = make(map[string]crypto.Ed25519PrivateKey)
+	rpcHost          = "http://v1-validator001:8081"
 )
 
+func init() {
+	if os.Getenv("RPC_HOST") != "" {
+		rpcHost = os.Getenv("RPC_HOST")
+	}
+}
+
 func main() {
-	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	fetchValidatorPrivateKeys(clientset)
+	fetchValidatorPrivateKeys(clientset, validatorKeysMap)
 
 	watcher, err := clientset.CoreV1().Services("default").Watch(context.TODO(), metav1.ListOptions{})
 	if err != nil {
@@ -47,56 +54,37 @@ func main() {
 			continue
 		}
 
+		validatorId := extractValidatorId(service.Name)
 		switch event.Type {
 		case watch.Added:
-			fmt.Printf("Validator %s added\n", service.Name)
-			fmt.Printf("Staking Validator with Address: %s\n", validatorKeysMap[extractValidatorId(service.Name)].Address())
+			fmt.Printf("Validator %s added to the cluster\n", service.Name)
+			stakeValidator(validatorKeysMap[validatorId], "150000000001", []string{"0001"}, fmt.Sprintf("v1-validator%s:8080", validatorId))
 		case watch.Deleted:
-			fmt.Printf("Validator %s deleted\n", service.Name)
-			fmt.Printf("Unstaking Validator with Address: %s\n", validatorKeysMap[extractValidatorId(service.Name)].Address())
+			fmt.Printf("Validator %s deleted from the cluster\n", service.Name)
+			unstakeValidator(validatorKeysMap[validatorId])
 		}
 	}
 }
 
-func fetchValidatorPrivateKeys(clientset *kubernetes.Clientset) {
-	private_keys_secret, err := clientset.CoreV1().Secrets("default").Get(context.TODO(), "v1-localnet-validators-private-keys", metav1.GetOptions{})
+func stakeValidator(pk crypto.Ed25519PrivateKey, amount string, chains []string, serviceURL string) error {
+	fmt.Printf("Staking Validator with Address: %s\n", pk.Address())
+	os.WriteFile("./pk.json", []byte("\""+pk.String()+"\""), 0644)
+
+	out, err := exec.Command("/usr/local/bin/client", "--not_interactive=true", "--remote_cli_url=http://v1-validator001:50832", "Validator", "Stake", pk.Address().String(), amount, strings.Join(chains, ","), serviceURL).CombinedOutput()
+	fmt.Println(string(out))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
-	for key, value := range private_keys_secret.Data {
-		pk, err := parsePrivateKey(bytes.NewReader(value))
-		if err != nil {
-			panic(err)
-		}
-		validatorKeysMap[key] = pk
-	}
+	return nil
 }
+func unstakeValidator(pk crypto.Ed25519PrivateKey) error {
+	fmt.Printf("Unstaking Validator with Address: %s\n", pk.Address())
+	os.WriteFile("./pk.json", []byte("\""+pk.String()+"\""), 0644)
 
-func isValidator(service *v1.Service) bool {
-	return service.Labels["v1-purpose"] == "validator"
-}
-
-func extractValidatorId(validatorName string) string {
-	if len(validatorName) >= 3 {
-		return validatorName[len(validatorName)-3:]
-	}
-	return validatorName
-}
-
-func parsePrivateKey(reader io.Reader) (pk crypto.Ed25519PrivateKey, err error) {
-	if reader == nil {
-		return nil, fmt.Errorf("cannot read from reader %v", reader)
-	}
-
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(reader)
-
-	priv := &crypto.Ed25519PrivateKey{}
-	err = priv.UnmarshalText(buf.Bytes())
+	out, err := exec.Command("/usr/local/bin/client", "--not_interactive=true", "--remote_cli_url=http://v1-validator001:50832", "Validator", "Unstake", pk.Address().String()).CombinedOutput()
+	fmt.Println(string(out))
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-	pk = priv.Bytes()
-	return
+	return nil
 }
---
+++ build/localnet/cluster-manager/utils.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/pokt-network/pocket/shared/crypto"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+func fetchValidatorPrivateKeys(clientset *kubernetes.Clientset, validatorKeysMap map[string]crypto.Ed25519PrivateKey) {
+	private_keys_secret, err := clientset.CoreV1().Secrets("default").Get(context.TODO(), "v1-localnet-validators-private-keys", metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for key, value := range private_keys_secret.Data {
+		pk, err := parsePrivateKey(bytes.NewReader(value))
+		if err != nil {
+			panic(err)
+		}
+		validatorKeysMap[key] = pk
+	}
+}
+
+func isValidator(service *v1.Service) bool {
+	return service.Labels["v1-purpose"] == "validator"
+}
+
+func extractValidatorId(validatorName string) string {
+	if len(validatorName) >= 3 {
+		return validatorName[len(validatorName)-3:]
+	}
+	return validatorName
+}
+
+func parsePrivateKey(reader io.Reader) (pk crypto.Ed25519PrivateKey, err error) {
+	if reader == nil {
+		return nil, fmt.Errorf("cannot read from reader %v", reader)
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(reader)
+
+	priv := &crypto.Ed25519PrivateKey{}
+	err = priv.UnmarshalText(buf.Bytes())
+	if err != nil {
+		return
+	}
+	pk = priv.Bytes()
+	return
+}
