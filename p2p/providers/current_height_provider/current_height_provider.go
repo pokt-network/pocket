352c06839886da55f9786a713d0c2780d8a7afa7
Alessandro De Blasis <alex@deblasis.net>
2023-02-07 17:59:51 +0000

    refactor(p2p): deprecated debugCurrentHeightProvider

--- p2p/providers/current_height_provider/current_height_provider.go
+++ p2p/providers/current_height_provider/current_height_provider.go
@@ -1,8 +1,8 @@
 package current_height_provider
 
-import "github.com/pokt-network/pocket/shared/modules"
-
 //go:generate mockgen -source=$GOFILE -destination=../../types/mocks/current_height_provider_mock.go -package=mock_types github.com/pokt-network/pocket/p2p/types CurrentHeightProvider
+
+import "github.com/pokt-network/pocket/shared/modules"
 
 const ModuleName = "current_height_provider"
 
