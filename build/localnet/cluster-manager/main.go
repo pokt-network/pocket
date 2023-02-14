d3c18f9281cfb2ea5b12d3877e9d1a480fb60fa8
Alessandro De Blasis <alex@deblasis.net>
2023-02-14 22:44:32 +0000

    feat(cluster-manager): improved logging

--- build/localnet/cluster-manager/main.go
+++ build/localnet/cluster-manager/main.go
@@ -58,45 +58,45 @@ func main() {
 		validatorId := extractValidatorId(service.Name)
 		switch event.Type {
 		case watch.Added:
-			log.Printf("Validator %s added to the cluster\n", service.Name)
+			log.Info().Str("validator", service.Name).Msg("Validator added to the cluster")
 			if err := stakeValidator(validatorKeysMap[validatorId], "150000000001", []string{"0001"}, fmt.Sprintf("v1-validator%s:8080", validatorId)); err != nil {
-				log.Printf("Error staking validator: %s", err.Error())
+				log.Err(err).Msg("Error staking validator")
 			}
 		case watch.Deleted:
-			log.Printf("Validator %s deleted from the cluster\n", service.Name)
+			log.Info().Str("validator", service.Name).Msg("Validator deleted from the cluster")
 			if err := unstakeValidator(validatorKeysMap[validatorId]); err != nil {
-				log.Printf("Error unstaking validator: %s", err.Error())
+				log.Err(err).Msg("Error unstaking validator")
 			}
 		}
 	}
 }
 
 func stakeValidator(pk crypto.PrivateKey, amount string, chains []string, serviceURL string) error {
-	log.Printf("Staking Validator with Address: %s\n", pk.Address())
+	log.Info().Str("address", pk.Address().String()).Msg("Staking Validator")
 	if err := os.WriteFile("./pk.json", []byte("\""+pk.String()+"\""), 0o600); err != nil {
 		return err
 	}
 
 	//nolint:gosec // G204 Dogfooding CLI
 	out, err := exec.Command("/usr/local/bin/client", "--not_interactive=true", "--remote_cli_url="+rpcUrl, "Validator", "Stake", pk.Address().String(), amount, strings.Join(chains, ","), serviceURL).CombinedOutput()
+	log.Info().Str("output", string(out)).Msg("CLI")
 	if err != nil {
 		return err
 	}
-	log.Printf(string(out))
 	return nil
 }
 
 func unstakeValidator(pk crypto.PrivateKey) error {
-	log.Printf("Unstaking Validator with Address: %s\n", pk.Address())
+	log.Info().Str("address", pk.Address().String()).Msg("Unstaking Validator")
 	if err := os.WriteFile("./pk.json", []byte("\""+pk.String()+"\""), 0o600); err != nil {
 		return err
 	}
 
 	//nolint:gosec // G204 Dogfooding CLI
 	out, err := exec.Command("/usr/local/bin/client", "--not_interactive=true", "--remote_cli_url="+rpcUrl, "Validator", "Unstake", pk.Address().String()).CombinedOutput()
+	log.Info().Str("output", string(out)).Msg("CLI")
 	if err != nil {
 		return err
 	}
-	log.Printf(string(out))
 	return nil
 }
