45682966a8d251fa2e15ce0e1ffc11d89fc42af2
harry <hrryslw@pm.me>
2023-02-14 21:44:36 +0000

    [Tooling] Integrate Keybase w/ CLI (Issue #484 ) (#501)
    ## Description
    
    This PR addresses the `#150` comments throughout the `app/client/cli`
    directory and integrates the keybase to be used in place of reading keys
    from files locally.
    
    The new method has been added to the `debug/keystore.go` file
    `InitialiseDebugKeybase()` which reads the 999 validator private keys
    from the `build/localnet/manifests/private-keys.yaml` file and stores
    them in the keybase. This is only run when there are less than 999 keys
    in the keybase. To speed this up `BadgerDB`'s `WriteBatch()` function is
    being used - however due to the encryption/JSON encoding and then Gob
    encoding steps of adding private keys to the keybase this takes a few
    seconds when ran for the first time.
    
    This PR does not add any endpoints to interact with the keybase from the
    CLI this is instead tracked in the issue #489
    
    ## Issue
    
    Fixes #484 
    
    ## Type of change
    
    Please mark the relevant option(s):
    
    - [x] New feature, functionality or library
    - [ ] Bug fix
    - [x] Code health or cleanup
    - [ ] Major breaking change
    - [ ] Documentation
    - [ ] Other <!-- add details here if it a different type of change -->
    
    ## List of changes
    
    - Add logic to initialise keybase with 999 validator keys
    - Update `app/client/cli` to use the keybase
    
    
    ## Testing
    
    - [x] `make develop_test`
    - [x]
    [LocalNet](https://github.com/pokt-network/pocket/blob/main/docs/development/README.md)
    w/ all of the steps outlined in the `README`
    
    
    [localnet_with_integration.webm](https://user-images.githubusercontent.com/53987565/217543043-21c35c3e-8389-44a5-968a-d3afb6cb8dd9.webm)
    
    ## Required Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have tested my changes using the available tooling
    - [x] I have updated the corresponding CHANGELOG
    
    ### If Applicable Checklist
    
    - [ ] I have updated the corresponding README(s); local and/or global
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added, or updated,
    [mermaid.js](https://mermaid-js.github.io) diagrams in the corresponding
    README(s)
    - [ ] I have added, or updated, documentation and
    [mermaid.js](https://mermaid-js.github.io) diagrams in `shared/docs/*`
    if I updated `shared/*`README(s)

---
+++ app/client/keybase/debug/keystore.go
@@ -0,0 +1,132 @@
+package debug
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pokt-network/pocket/app/client/keybase"
+	"github.com/pokt-network/pocket/shared/crypto"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	// NOTE: This is the number of validators in the private-keys.yaml manifest file
+	numValidators       = 999
+	debugKeybaseSuffix  = "/.pocket/keys"
+	privateKeysYamlFile = "../../../../../build/localnet/manifests/private-keys.yaml"
+)
+
+var (
+	// TODO: Allow users to override this value via `datadir` flag
+	debugKeybasePath string
+)
+
+// Initialise the debug keybase with the 999 validator keys from the private-keys manifest file
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("[ERROR] Cannot find user home directory: %s", err.Error())
+	}
+	debugKeybasePath = homeDir + debugKeybaseSuffix
+
+	if err := InitialiseDebugKeybase(); err != nil { // Initialise the debug keybase with the 999 validators
+		log.Fatalf("[ERROR] Cannot initialise the keybase with the validator keys: %s", err.Error())
+	}
+}
+
+// Struct to process the yaml file of pre-generated private-keys
+type yamlConfig struct {
+	ApiVersion string            `yaml:"apiVersion"`
+	Kind       string            `yaml:"kind"`
+	MetaData   map[string]string `yaml:"metadata"`
+	Type       string            `yaml:"type"`
+	StringData map[string]string `yaml:"stringData"`
+}
+
+// Creates/Opens the DB and initialises the keys from the pre-generated YAML file of private keys
+func InitialiseDebugKeybase() error {
+	// Get private keys from manifest file
+	_, current, _, _ := runtime.Caller(0)
+	//nolint:gocritic // Use path to find private-keys yaml file from being called in any location in the repo
+	yamlFile := filepath.Join(current, privateKeysYamlFile)
+
+	if exists, err := fileExists(yamlFile); !exists || err != nil {
+		return fmt.Errorf("Unable to find YAML file: %s", yamlFile)
+	}
+
+	// Parse the YAML file and load into the yamlConfig struct
+	yamlData, err := os.ReadFile(yamlFile)
+	if err != nil {
+		return err
+	}
+
+	var config yamlConfig
+	if err := yaml.Unmarshal(yamlData, &config); err != nil {
+		return err
+	}
+
+	// Create/Open the keybase at `$HOME/.pocket/keys`
+	kb, err := keybase.NewKeybase(debugKeybasePath)
+	if err != nil {
+		return err
+	}
+	db := kb.GetBadgerDB()
+
+	// Add the keys if the keybase contains less than 999
+	curAddr, _, err := kb.GetAll()
+	if err != nil {
+		return err
+	}
+
+	// Add validator addresses if not present
+	if len(curAddr) < numValidators {
+		// Use writebatch to speed up bulk insert
+		wb := db.NewWriteBatch()
+		for _, privHexString := range config.StringData {
+			// Import the keys into the keybase with no passphrase or hint as these are for debug purposes
+			keyPair, err := crypto.CreateNewKeyFromString(privHexString, "", "")
+			if err != nil {
+				return err
+			}
+
+			// Use key address as key in DB
+			addrKey := keyPair.GetAddressBytes()
+
+			// Encode KeyPair into []byte for value
+			keypairBz, err := keyPair.Marshal()
+			if err != nil {
+				return err
+			}
+			if err := wb.Set(addrKey, keypairBz); err != nil {
+				return err
+			}
+		}
+		if err := wb.Flush(); err != nil {
+			return err
+		}
+	}
+
+	// Close DB connection
+	if err := kb.Stop(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Check file at the given path exists
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
