08622b7b53415851ac3412c59ccb096b47143530
Alessandro De Blasis <alex@deblasis.net>
2023-02-14 01:19:38 +0000

    Update shared/modules/modules_registry_module.go
    Co-authored-by: Daniel Olshansky <olshansky@pokt.network>

--- shared/modules/modules_registry_module.go
+++ shared/modules/modules_registry_module.go
@@ -5,6 +5,6 @@ package modules
 type ModulesRegistry interface {
 	// RegisterModule registers a Module with the ModuleRegistry
 	RegisterModule(module Module)
-	// GetModuleByName returns a Module by name or nil if not found in the ModuleRegistry
+	// GetModule returns a Module by name or nil if not found in the ModuleRegistry
 	GetModule(moduleName string) (Module, error)
 }
