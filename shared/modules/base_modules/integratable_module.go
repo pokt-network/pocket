28069335be09404cd1a43350fafe37e5ef49da85
Alessandro De Blasis <alex@deblasis.net>
2023-02-14 13:17:48 +0000

    docs(shared): base IntegratableModule godoc

--- shared/modules/base_modules/integratable_module.go
+++ shared/modules/base_modules/integratable_module.go
@@ -4,6 +4,10 @@ import "github.com/pokt-network/pocket/shared/modules"
 
 var _ modules.IntegratableModule = &IntegratableModule{}
 
+// IntegratableModule is a base struct that is meant to be embedded in module structs that implement the interface `modules.IntegratableModule`.
+//
+// It provides the basic logic for the `SetBus` and `GetBus` methods and allows the implementer to reduce boilerplate code keeping the code
+// DRY (Don't Repeat Yourself) while preserving the ability to override the methods if needed.
 type IntegratableModule struct {
 	bus modules.Bus
 }
