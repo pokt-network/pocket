469912fe1615ef5ce93a7ed5022f26e863442283
Alessandro De Blasis <alex@deblasis.net>
2023-02-15 21:37:33 +0000

    fix(keybase): doing the heavylifting only when needed

--- shared/k8s/debug.go
+++ shared/k8s/debug.go
@@ -3,13 +3,12 @@ package k8s
 import (
 	"context"
 
-	cryptoPocket "github.com/pokt-network/pocket/shared/crypto"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-func FetchValidatorPrivateKeys(clientset *kubernetes.Clientset) (map[string]cryptoPocket.KeyPair, error) {
-	var validatorKeysMap = make(map[string]cryptoPocket.KeyPair)
+func FetchValidatorPrivateKeys(clientset *kubernetes.Clientset) (map[string]string, error) {
+	var validatorKeysMap = make(map[string]string)
 
 	private_keys_secret, err := clientset.CoreV1().Secrets("default").Get(context.TODO(), "v1-localnet-validators-private-keys", metav1.GetOptions{})
 	if err != nil {
@@ -17,12 +16,7 @@ func FetchValidatorPrivateKeys(clientset *kubernetes.Clientset) (map[string]cryp
 	}
 
 	for id, privHexString := range private_keys_secret.Data {
-		// Import the keys into the keybase with no passphrase or hint as these are for debug purposes
-		keyPair, err := cryptoPocket.CreateNewKeyFromString(string(privHexString), "", "")
-		if err != nil {
-			return nil, err
-		}
-		validatorKeysMap[id] = keyPair
+		validatorKeysMap[id] = string(privHexString)
 	}
 	return validatorKeysMap, nil
 }
