pokt-network/pocket

commit 45682966a8d251fa2e15ce0e1ffc11d89fc42af2

[Tooling] Integrate Keybase w/ CLI (Issue #484 ) (#501)
## Description

This PR addresses the `#150` comments throughout the `app/client/cli`
directory and integrates the keybase to be used in place of reading keys
from files locally.

The new method has been added to the `debug/keystore.go` file
`InitialiseDebugKeybase()` which reads the 999 validator private keys
from the `build/localnet/manifests/private-keys.yaml` file and stores
them in the keybase. This is only run when there are less than 999 keys
in the keybase. To speed this up `BadgerDB`'s `WriteBatch()` function is
being used - however due to the encryption/JSON encoding and then Gob
encoding steps of adding private keys to the keybase this takes a few
seconds when ran for the first time.

This PR does not add any endpoints to interact with the keybase from the
CLI this is instead tracked in the issue #489

## Issue

Fixes #484 

## Type of change

Please mark the relevant option(s):

- [x] New feature, functionality or library
- [ ] Bug fix
- [x] Code health or cleanup
- [ ] Major breaking change
- [ ] Documentation
- [ ] Other <!-- add details here if it a different type of change -->

## List of changes

- Add logic to initialise keybase with 999 validator keys
- Update `app/client/cli` to use the keybase


## Testing

- [x] `make develop_test`
- [x]
[LocalNet](https://github.com/pokt-network/pocket/blob/main/docs/development/README.md)
w/ all of the steps outlined in the `README`


[localnet_with_integration.webm](https://user-images.githubusercontent.com/53987565/217543043-21c35c3e-8389-44a5-968a-d3afb6cb8dd9.webm)

## Required Checklist

- [x] I have performed a self-review of my own code
- [x] I have commented my code, particularly in hard-to-understand areas
- [x] I have tested my changes using the available tooling
- [x] I have updated the corresponding CHANGELOG

### If Applicable Checklist

app/client/cli/actor.go
@@ -3,10 +3,14 @@ package cli
 import (
 	"fmt"
 	"math/big"
+	"path/filepath"
 	"regexp"
 	"strings"
 
+	"github.com/pokt-network/pocket/app/client/keybase"
+
 	coreTypes "github.com/pokt-network/pocket/shared/core/types"
+	"github.com/pokt-network/pocket/shared/crypto"
 	typesUtil "github.com/pokt-network/pocket/utility/types"
 	"github.com/spf13/cobra"
 )
@@ -69,7 +73,7 @@ func newActorCommands(cmdDef actorCmdDef) []*cobra.Command {
 		newUnstakeCmd(cmdDef),
 		newUnpauseCmd(cmdDef),
 	}
-	applySubcommandOptions(cmds, cmdDef)
+	applySubcommandOptions(cmds, cmdDef.Options)
 	return cmds
 }
 
@@ -86,18 +90,34 @@ A node can update relayChainIDs, serviceURI, and raise the stake amount with thi
 If the node is currently staked at X and you submit an update with new stake Y. Only Y-X will be subtracted from an account.
 
 If no changes are desired for the parameter, just enter the current param value just as before.`,
-		Args: cobra.ExactArgs(4), // REFACTOR(#150): <fromAddr> not being used at the moment. Update once a keybase is implemented.
+		Args: cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO(#150): update when we have keybase
-			pk, err := readEd25519PrivateKeyFromFile(privateKeyFilePath)
+			// Unpack CLI arguments
+			fromAddrHex := args[0]
+			fromAddr := crypto.AddressFromString(args[0])
+			amount := args[1]
+
+			// Open the keybase at the specified directory
+			pocketDir := strings.TrimSuffix(dataDir, "/")
+			keybasePath, err := filepath.Abs(pocketDir + keybaseSuffix)
 			if err != nil {
 				return err
 			}
-			// NOTE: since we don't have a keybase yet (tracked in #150), we are currently inferring the `fromAddr` from the PrivateKey supplied via the flag `--path_to_private_key_file`
-			// the following line is commented out to show that once we have a keybase, `fromAddr` should come from the command arguments and not the PrivateKey (pk) anymore.
-			//
-			// fromAddr := crypto.AddressFromString(args[0])
-			amount := args[1]
+			kb, err := keybase.NewKeybase(keybasePath)
+			if err != nil {
+				return err
+			}
+
+			pwd = readPassphrase(pwd)
+
+			pk, err := kb.GetPrivKey(fromAddrHex, pwd)
+			if err != nil {
+				return err
+			}
+			if err := kb.Stop(); err != nil {
+				return err
+			}
+
 			err = validateStakeAmount(amount)
 			if err != nil {
 				return err
@@ -108,11 +128,8 @@ If no changes are desired for the parameter, just enter the current param value 
 			chains := strings.Split(rawChains, ",")
 			serviceURI := args[3]
 
-			// TODO (team): passphrase is currently not used since there's no keybase yet, the prompt is here to mimick the real world UX
-			pwd = readPassphrase(pwd)
-
 			msg := &typesUtil.MessageStake{
-				PublicKey:     pk.PublicKey().Bytes(),
+				PublicKey:     fromAddr,
 				Chains:        chains,
 				Amount:        amount,
 				ServiceUrl:    serviceURI,
@@ -146,15 +163,34 @@ func newEditStakeCmd(cmdDef actorCmdDef) *cobra.Command {
 		Use:   "EditStake <fromAddr> <amount> <relayChainIDs> <serviceURI>",
 		Short: "EditStake <fromAddr> <amount> <relayChainIDs> <serviceURI>",
 		Long:  fmt.Sprintf(`Stakes a new <amount> for the %s actor with address <fromAddr> for the specified <relayChainIDs> and <serviceURI>.`, cmdDef.Name),
-		Args:  cobra.ExactArgs(4), // REFACTOR(#150): <fromAddr> not being used at the moment. Update once a keybase is implemented.
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO(#150): update when we have keybase
-			pk, err := readEd25519PrivateKeyFromFile(privateKeyFilePath)
+			// Unpack CLI arguments
+			fromAddrHex := args[0]
+			fromAddr := crypto.AddressFromString(args[0])
+			amount := args[1]
+
+			// Open the keybase at the specified directory
+			pocketDir := strings.TrimSuffix(dataDir, "/")
+			keybasePath, err := filepath.Abs(pocketDir + keybaseSuffix)
+			if err != nil {
+				return err
+			}
+			kb, err := keybase.NewKeybase(keybasePath)
 			if err != nil {
 				return err
 			}
 
-			amount := args[1]
+			pwd = readPassphrase(pwd)
+
+			pk, err := kb.GetPrivKey(fromAddrHex, pwd)
+			if err != nil {
+				return err
+			}
+			if err := kb.Stop(); err != nil {
+				return err
+			}
+
 			err = validateStakeAmount(amount)
 			if err != nil {
 				return err
@@ -164,11 +200,8 @@ func newEditStakeCmd(cmdDef actorCmdDef) *cobra.Command {
 			chains := strings.Split(rawChains, ",")
 			serviceURI := args[3]
 
-			// TODO (team): passphrase is currently not used since there's no keybase yet, the prompt is here to mimick the real world UX
-			pwd = readPassphrase(pwd)
-
 			msg := &typesUtil.MessageEditStake{
-				Address:    pk.Address(),
+				Address:    fromAddr,
 				Chains:     chains,
 				Amount:     amount,
 				ServiceUrl: serviceURI,
@@ -200,16 +233,31 @@ func newUnstakeCmd(cmdDef actorCmdDef) *cobra.Command {
 		Use:   "Unstake <fromAddr>",
 		Short: "Unstake <fromAddr>",
 		Long:  fmt.Sprintf(`Unstakes the prevously staked tokens for the %s actor with address <fromAddr>`, cmdDef.Name),
-		Args:  cobra.ExactArgs(1), // REFACTOR(#150): <fromAddr> not being used at the moment. Update once a keybase is implemented.
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO(#150): update when we have keybase
-			pk, err := readEd25519PrivateKeyFromFile(privateKeyFilePath)
+			// Unpack CLI arguments
+			fromAddrHex := args[0]
+
+			// Open the keybase at the specified directory
+			pocketDir := strings.TrimSuffix(dataDir, "/")
+			keybasePath, err := filepath.Abs(pocketDir + keybaseSuffix)
+			if err != nil {
+				return err
+			}
+			kb, err := keybase.NewKeybase(keybasePath)
 			if err != nil {
 				return err
 			}
 
-			// TODO (team): passphrase is currently not used since there's no keybase yet, the prompt is here to mimick the real world UX
 			pwd = readPassphrase(pwd)
+
+			pk, err := kb.GetPrivKey(fromAddrHex, pwd)
+			if err != nil {
+				return err
+			}
+			if err := kb.Stop(); err != nil {
+				return err
+			}
 
 			msg := &typesUtil.MessageUnstake{
 				Address:   pk.Address(),
@@ -241,16 +289,31 @@ func newUnpauseCmd(cmdDef actorCmdDef) *cobra.Command {
 		Use:   "Unpause <fromAddr>",
 		Short: "Unpause <fromAddr>",
 		Long:  fmt.Sprintf(`Unpauses the %s actor with address <fromAddr>`, cmdDef.Name),
-		Args:  cobra.ExactArgs(1), // REFACTOR(#150): Not being used at the moment. Update once a keybase is implemented.
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO(#150): update when we have keybase
-			pk, err := readEd25519PrivateKeyFromFile(privateKeyFilePath)
+			// Unpack CLI arguments
+			fromAddrHex := args[0]
+
+			// Open the keybase at the specified directory
+			pocketDir := strings.TrimSuffix(dataDir, "/")
+			keybasePath, err := filepath.Abs(pocketDir + keybaseSuffix)
+			if err != nil {
+				return err
+			}
+			kb, err := keybase.NewKeybase(keybasePath)
 			if err != nil {
 				return err
 			}
 
-			// TODO (team): passphrase is currently not used since there's no keybase yet, the prompt is here to mimick the real world UX
 			pwd = readPassphrase(pwd)
+
+			pk, err := kb.GetPrivKey(fromAddrHex, pwd)
+			if err != nil {
+				return err
+			}
+			if err := kb.Stop(); err != nil {
+				return err
+			}
 
 			msg := &typesUtil.MessageUnpause{
 				Address:   pk.Address(),

app/client/cli/account.go
@@ -2,16 +2,17 @@ package cli
 
 import (
 	"fmt"
+	"path/filepath"
+	"strings"
 
+	"github.com/pokt-network/pocket/app/client/keybase"
 	"github.com/pokt-network/pocket/shared/crypto"
 	"github.com/pokt-network/pocket/utility/types"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	accountCmd := NewAccountCommand()
-	accountCmd.Flags().StringVar(&pwd, "pwd", "", "passphrase used by the cmd, non empty usage bypass interactive prompt")
-	rootCmd.AddCommand(accountCmd)
+	rootCmd.AddCommand(NewAccountCommand())
 }
 
 func NewAccountCommand() *cobra.Command {
@@ -22,7 +23,9 @@ func NewAccountCommand() *cobra.Command {
 		Args:    cobra.ExactArgs(0),
 	}
 
-	cmd.AddCommand(accountCommands()...)
+	cmds := accountCommands()
+	applySubcommandOptions(cmds, attachPwdFlagToSubcommands())
+	cmd.AddCommand(cmds...)
 
 	return cmd
 }
@@ -34,22 +37,37 @@ func accountCommands() []*cobra.Command {
 			Short:   "Send <fromAddr> <to> <amount>",
 			Long:    "Sends <amount> to address <to> from address <fromAddr>",
 			Aliases: []string{"send"},
-			Args:    cobra.ExactArgs(3), // REFACTOR(#150): <fromAddr> not being used at the moment. Update once a keybase is implemented.
+			Args:    cobra.ExactArgs(3),
 			RunE: func(cmd *cobra.Command, args []string) error {
-				// TODO(#150): update when we have keybase
-				pk, err := readEd25519PrivateKeyFromFile(privateKeyFilePath)
-				if err != nil {
-					return err
-				}
-				// NOTE: since we don't have a keybase yet (tracked in #150), we are currently inferring the `fromAddr` from the PrivateKey supplied via the flag `--path_to_private_key_file`
-				// the following line is commented out to show that once we have a keybase, `fromAddr` should come from the command arguments and not the PrivateKey (pk) anymore.
-				//
-				// fromAddr := crypto.AddressFromString(args[0])
+				// Unpack CLI arguments
+				fromAddrHex := args[0]
+				fromAddr := crypto.AddressFromString(args[0])
 				toAddr := crypto.AddressFromString(args[1])
 				amount := args[2]
 
+				// Open the debug keybase at the specified path
+				pocketDir := strings.TrimSuffix(dataDir, "/")
+				keybasePath, err := filepath.Abs(pocketDir + keybaseSuffix)
+				if err != nil {
+					return err
+				}
+				kb, err := keybase.NewKeybase(keybasePath)
+				if err != nil {
+					return err
+				}
+
+				pwd = readPassphrase(pwd)
+
+				pk, err := kb.GetPrivKey(fromAddrHex, pwd)
+				if err != nil {
+					return err
+				}
+				if err := kb.Stop(); err != nil {
+					return err
+				}
+
 				msg := &types.MessageSend{
-					FromAddress: pk.Address(),
+					FromAddress: fromAddr,
 					ToAddress:   toAddr,
 					Amount:      amount,
 				}

app/client/cli/cmd.go
@@ -2,21 +2,32 @@ package cli
 
 import (
 	"context"
+	"log"
+	"os"
 
+	// NOTE: Imported for debug purposes in order to populate the keybase with the pre-generated keys
+	_ "github.com/pokt-network/pocket/app/client/keybase/debug"
 	"github.com/pokt-network/pocket/runtime/defaults"
 	"github.com/spf13/cobra"
 )
 
-const cliExecutableName = "client"
+const (
+	cliExecutableName = "client"
+	keybaseSuffix     = "/keys"
+)
 
 var (
-	remoteCLIURL       string
-	privateKeyFilePath string
+	remoteCLIURL string
+	dataDir      string
 )
 
 func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("[ERROR] Cannot find user home directory: %s", err.Error())
+	}
 	rootCmd.PersistentFlags().StringVar(&remoteCLIURL, "remote_cli_url", defaults.DefaultRemoteCLIURL, "takes a remote endpoint in the form of <protocol>://<host> (uses RPC Port)")
-	rootCmd.PersistentFlags().StringVar(&privateKeyFilePath, "path_to_private_key_file", "./pk.json", "Path to private key to use when signing")
+	rootCmd.PersistentFlags().StringVar(&dataDir, "data_dir", homeDir+"/.pocket", "Path to store pocket related data (keybase etc.)")
 }
 
 var rootCmd = &cobra.Command{

app/client/cli/gov.go
@@ -2,7 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"path/filepath"
+	"strings"
 
+	"github.com/pokt-network/pocket/app/client/keybase"
 	"github.com/pokt-network/pocket/utility/types"
 	"github.com/spf13/cobra"
 	"google.golang.org/protobuf/types/known/anypb"
@@ -21,7 +24,10 @@ func NewGovernanceCommand() *cobra.Command {
 		Args:    cobra.ExactArgs(0),
 	}
 
-	cmd.AddCommand(govCommands()...)
+	cmds := govCommands()
+	applySubcommandOptions(cmds, attachPwdFlagToSubcommands())
+
+	cmd.AddCommand(cmds...)
 
 	return cmd
 }
@@ -35,17 +41,34 @@ func govCommands() []*cobra.Command {
 			Aliases: []string{},
 			Args:    cobra.ExactArgs(3),
 			RunE: func(cmd *cobra.Command, args []string) error {
+				// Unpack CLI arguments
+				fromAddrHex := args[0]
+				key := args[1]
+				value := args[2]
+
 				// TODO(deblasis): implement RPC client, route and handler
 				fmt.Printf("changing parameter %s owned by %s to %s\n", args[1], args[0], args[2])
 
-				// TODO(#150): update when we have keybase
-				pk, err := readEd25519PrivateKeyFromFile(privateKeyFilePath)
+				// Open the debug keybase at the specified path
+				pocketDir := strings.TrimSuffix(dataDir, "/")
+				keybasePath, err := filepath.Abs(pocketDir + keybaseSuffix)
+				if err != nil {
+					return err
+				}
+				kb, err := keybase.NewKeybase(keybasePath)
 				if err != nil {
 					return err
 				}
 
-				key := args[1]
-				value := args[2]
+				pwd = readPassphrase(pwd)
+
+				pk, err := kb.GetPrivKey(fromAddrHex, pwd)
+				if err != nil {
+					return err
+				}
+				if err := kb.Stop(); err != nil {
+					return err
+				}
 
 				pbValue, err := anypb.New(wrapperspb.String(value))
 				if err != nil {

app/client/cli/utils.go
@@ -21,18 +21,6 @@ import (
 	"golang.org/x/term"
 )
 
-// readEd25519PrivateKeyFromFile returns an Ed25519PrivateKey from a file where the file simply encodes it in a string (for now)
-// HACK(#150): this is a temporary hack since we don't have yet a keybase, the next step would be to read from an "ArmoredJson" like in V0
-func readEd25519PrivateKeyFromFile(pkPath string) (pk crypto.Ed25519PrivateKey, err error) {
-	pkFile, err := os.Open(pkPath)
-	if err != nil {
-		return
-	}
-	defer pkFile.Close()
-	pk, err = parseEd25519PrivateKeyFromReader(pkFile)
-	return
-}
-
 func parseEd25519PrivateKeyFromReader(reader io.Reader) (pk crypto.Ed25519PrivateKey, err error) {
 	if reader == nil {
 		return nil, fmt.Errorf("cannot read from reader %v", reader)
@@ -94,7 +82,7 @@ func confirmation(pwd string) bool {
 // prepareTxBytes wraps a Message into a Transaction and signs it with the provided pk
 //
 // returns the raw protobuf bytes of the signed transaction
-func prepareTxBytes(msg typesUtil.Message, pk crypto.Ed25519PrivateKey) ([]byte, error) {
+func prepareTxBytes(msg typesUtil.Message, pk crypto.PrivateKey) ([]byte, error) {
 	var err error
 	anyMsg, err := codec.GetCodec().ToAny(msg)
 	if err != nil {
@@ -129,7 +117,7 @@ func prepareTxBytes(msg typesUtil.Message, pk crypto.Ed25519PrivateKey) ([]byte,
 }
 
 // postRawTx posts a signed transaction
-func postRawTx(ctx context.Context, pk crypto.Ed25519PrivateKey, j []byte) (*rpc.PostV1ClientBroadcastTxSyncResponse, error) {
+func postRawTx(ctx context.Context, pk crypto.PrivateKey, j []byte) (*rpc.PostV1ClientBroadcastTxSyncResponse, error) {
 	client, err := rpc.NewClientWithResponses(remoteCLIURL)
 	if err != nil {
 		return nil, err
@@ -172,9 +160,9 @@ func validateStakeAmount(amount string) error {
 	return nil
 }
 
-func applySubcommandOptions(cmds []*cobra.Command, cmdDef actorCmdDef) {
+func applySubcommandOptions(cmds []*cobra.Command, cmdOptions []cmdOption) {
 	for _, cmd := range cmds {
-		for _, opt := range cmdDef.Options {
+		for _, opt := range cmdOptions {
 			opt(cmd)
 		}
 	}

app/client/keybase/debug/keystore.go
@@ -1,0 +1,132 @@
+package debug
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pokt-network/pocket/app/client/keybase"
+	"github.com/pokt-network/pocket/shared/crypto"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	// NOTE: This is the number of validators in the private-keys.yaml manifest file
+	numValidators       = 999
+	debugKeybaseSuffix  = "/.pocket/keys"
+	privateKeysYamlFile = "../../../../../build/localnet/manifests/private-keys.yaml"
+)
+
+var (
+	// TODO: Allow users to override this value via `datadir` flag
+	debugKeybasePath string
+)
+
+// Initialise the debug keybase with the 999 validator keys from the private-keys manifest file
+func init() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("[ERROR] Cannot find user home directory: %s", err.Error())
+	}
+	debugKeybasePath = homeDir + debugKeybaseSuffix
+
+	if err := InitialiseDebugKeybase(); err != nil { // Initialise the debug keybase with the 999 validators
+		log.Fatalf("[ERROR] Cannot initialise the keybase with the validator keys: %s", err.Error())
+	}
+}
+
+// Struct to process the yaml file of pre-generated private-keys
+type yamlConfig struct {
+	ApiVersion string            `yaml:"apiVersion"`
+	Kind       string            `yaml:"kind"`
+	MetaData   map[string]string `yaml:"metadata"`
+	Type       string            `yaml:"type"`
+	StringData map[string]string `yaml:"stringData"`
+}
+
+// Creates/Opens the DB and initialises the keys from the pre-generated YAML file of private keys
+func InitialiseDebugKeybase() error {
+	// Get private keys from manifest file
+	_, current, _, _ := runtime.Caller(0)
+	//nolint:gocritic // Use path to find private-keys yaml file from being called in any location in the repo
+	yamlFile := filepath.Join(current, privateKeysYamlFile)
+
+	if exists, err := fileExists(yamlFile); !exists || err != nil {
+		return fmt.Errorf("Unable to find YAML file: %s", yamlFile)
+	}
+
+	// Parse the YAML file and load into the yamlConfig struct
+	yamlData, err := os.ReadFile(yamlFile)
+	if err != nil {
+		return err
+	}
+
+	var config yamlConfig
+	if err := yaml.Unmarshal(yamlData, &config); err != nil {
+		return err
+	}
+
+	// Create/Open the keybase at `$HOME/.pocket/keys`
+	kb, err := keybase.NewKeybase(debugKeybasePath)
+	if err != nil {
+		return err
+	}
+	db := kb.GetBadgerDB()
+
+	// Add the keys if the keybase contains less than 999
+	curAddr, _, err := kb.GetAll()
+	if err != nil {
+		return err
+	}
+
+	// Add validator addresses if not present
+	if len(curAddr) < numValidators {
+		// Use writebatch to speed up bulk insert
+		wb := db.NewWriteBatch()
+		for _, privHexString := range config.StringData {
+			// Import the keys into the keybase with no passphrase or hint as these are for debug purposes
+			keyPair, err := crypto.CreateNewKeyFromString(privHexString, "", "")
+			if err != nil {
+				return err
+			}
+
+			// Use key address as key in DB
+			addrKey := keyPair.GetAddressBytes()
+
+			// Encode KeyPair into []byte for value
+			keypairBz, err := keyPair.Marshal()
+			if err != nil {
+				return err
+			}
+			if err := wb.Set(addrKey, keypairBz); err != nil {
+				return err
+			}
+		}
+		if err := wb.Flush(); err != nil {
+			return err
+		}
+	}
+
+	// Close DB connection
+	if err := kb.Stop(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Check file at the given path exists
+func fileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}

app/client/keybase/keybase.go
@@ -1,9 +1,15 @@
 package keybase
 
-import "github.com/pokt-network/pocket/shared/crypto"
+import (
+	"github.com/dgraph-io/badger/v3"
+	"github.com/pokt-network/pocket/shared/crypto"
+)
 
 // Keybase interface implements the CRUD operations for the keybase
 type Keybase interface {
+	// Debug
+	GetBadgerDB() *badger.DB
+
 	// Close the DB connection
 	Stop() error
 

app/client/keybase/keystore.go
@@ -3,12 +3,18 @@ package keybase
 import (
 	"bytes"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
 
 	"github.com/dgraph-io/badger/v3"
 	"github.com/pokt-network/pocket/shared/crypto"
+)
+
+const (
+	prefetchSize = 15
 )
 
 // Errors
@@ -45,6 +51,12 @@ func NewKeybaseInMemory() (Keybase, error) {
 		return nil, err
 	}
 	return &badgerKeybase{db: db}, nil
+}
+
+// Return DB instance
+// FOR DEBUG PURPOSES ONLY
+func (keybase *badgerKeybase) GetBadgerDB() *badger.DB {
+	return keybase.db
 }
 
 // Close the DB
@@ -190,7 +202,7 @@ func (keybase *badgerKeybase) GetAll() (addresses []string, keyPairs []crypto.Ke
 	// View executes the function provided managing a read only transaction
 	err = keybase.db.View(func(tx *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
-		opts.PrefetchSize = 5
+		opts.PrefetchSize = prefetchSize
 		it := tx.NewIterator(opts)
 		defer it.Close()
 		for it.Rewind(); it.Valid(); it.Next() {
@@ -340,7 +352,7 @@ func dirExists(path string) (bool, error) {
 		}
 		return true, nil
 	}
-	if os.IsNotExist(err) {
+	if errors.Is(err, fs.ErrNotExist) {
 		// Create directories in path recursively
 		if err := os.MkdirAll(path, os.ModePerm); err != nil {
 			return false, fmt.Errorf("Error creating directory at path: %s, (%v)", path, err.Error())

commit 77c08ce04e6231223605da8bdbc494a22f4ddc54

[CLI] Stake command bugfix (#518)
## Description

This PR fixes a bug introduced in #501 

## Issue

Not created because I am too tired

## Type of change

Please mark the relevant option(s):

- [ ] New feature, functionality or library
- [x] Bug fix
- [ ] Code health or cleanup
- [ ] Major breaking change
- [ ] Documentation
- [ ] Other <!-- add details here if it a different type of change -->

## List of changes

- Sender `Address` was erroneously sent instead of the `PublicKey`.

## Testing

- [x] `make develop_test`
- [x]
[LocalNet](https://github.com/pokt-network/pocket/blob/main/docs/development/README.md)
w/ all of the steps outlined in the `README`

## Required Checklist

- [x] I have performed a self-review of my own code
- [x] I have commented my code, particularly in hard-to-understand areas
- [x] I have tested my changes using the available tooling
- [x] I have updated the corresponding CHANGELOG

### If Applicable Checklist

- [ ] I have updated the corresponding README(s); local and/or global
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [ ] I have added, or updated,
[mermaid.js](https://mermaid-js.github.io) diagrams in the corresponding
README(s)
- [ ] I have added, or updated, documentation and
[mermaid.js](https://mermaid-js.github.io) diagrams in `shared/docs/*`
if I updated `shared/*`README(s)

---------

Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

app/client/cli/actor.go
@@ -94,7 +94,6 @@ If no changes are desired for the parameter, just enter the current param value 
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// Unpack CLI arguments
 			fromAddrHex := args[0]
-			fromAddr := crypto.AddressFromString(args[0])
 			amount := args[1]
 
 			// Open the keybase at the specified directory
@@ -129,7 +128,7 @@ If no changes are desired for the parameter, just enter the current param value 
 			serviceURI := args[3]
 
 			msg := &typesUtil.MessageStake{
-				PublicKey:     fromAddr,
+				PublicKey:     pk.PublicKey().Bytes(),
 				Chains:        chains,
 				Amount:        amount,
 				ServiceUrl:    serviceURI,