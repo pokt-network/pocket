6f27c9d08c97657baabc4b63e52d2ecb9d4eba81
Harry Law <hrryslw@pm.me>
2023-02-20 18:09:19 +0000

    Add todo

--- shared/converters/file_utils.go
+++ shared/converters/file_utils.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+// TODO: Consolidate with converters
+
 // Check directory exists and creates path if it doesn't exist
 func DirExists(path string) (bool, error) {
 	stat, err := os.Stat(path)
