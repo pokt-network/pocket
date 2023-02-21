c9d0bfcb473cf3d418b0d01b05191a3cc55e28b4
Bryan White <bryanchriswhite@gmail.com>
2023-02-21 10:29:58 +0100

    chore: var consolidation and commments

--- libp2p/protocol/protocol.go
+++ libp2p/protocol/protocol.go
@@ -2,8 +2,11 @@ package protocol
 
 import "github.com/libp2p/go-libp2p/core/protocol"
 
-// PoktProtocolID is the libp2p protocol ID matching current version of the pokt protocol.
-var PoktProtocolID = protocol.ID("pokt/v1.0.0")
-
-// DefaultTopicStr is a "default" pubsub topic string for use when subscribing.
-var DefaultTopicStr = "pokt/default"
+var (
+	// PoktProtocolID is the libp2p protocol ID used when opening a new stream
+	// to a remote peer and setting the stream handler for the local peer.
+	// Libp2p APIs use this to distinguish which multiplexed protocols/streams to consider.
+	PoktProtocolID = protocol.ID("pokt/v1.0.0")
+	// DefaultTopicStr is a "default" pubsub topic string for use when subscribing.
+	DefaultTopicStr = "pokt/default"
+)
