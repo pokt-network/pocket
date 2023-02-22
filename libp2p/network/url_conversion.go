eef55e3a42ca5a632a9e1ae1f1028705e2a8d897
Bryan White <bryanchriswhite@gmail.com>
2023-02-22 11:56:13 +0100

    feat: add url <--> multiaddr conversion helpers

---
+++ libp2p/network/url_conversion.go
@@ -0,0 +1,107 @@
+package network
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"strings"
+
+	"github.com/multiformats/go-multiaddr"
+)
+
+const (
+	// anyScheme is a string which fills the "scheme" degree of freedom for
+	// a URL. Used to pad URLs without schemes such that they can be parsed
+	// via stdlib URL parser.
+	anyScheme = "scheme://"
+)
+
+// Libp2pMultiaddrFromServiceUrl transforms a URL into its libp2p multiaddr equivalent.
+// (see: https://github.com/libp2p/specs/blob/master/addressing/README.md#multiaddr-basics)
+func Libp2pMultiaddrFromServiceUrl(serviceUrl string) (multiaddr.Multiaddr, error) {
+	peerUrl, err := url.Parse(anyScheme + serviceUrl)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"parsing peer service URL: %s: %w",
+			serviceUrl,
+			err,
+		)
+	}
+
+	var (
+		// TECHDEBT: assuming TCP; remote peer's transport type must be knowable!
+		// (ubiquitously switching to multiaddr, instead of a URL, would resolve this)
+		peerTransportStr = "tcp"
+		peerIPVersionStr = "ip4"
+	)
+
+	// TECHDEBT: there's probably a more conventional way to do this.
+	// Check if we're dealing with IPv4 or IPv6
+	if strings.Count(peerUrl.Hostname(), ":") > 0 {
+		peerIPVersionStr = "ip6"
+	}
+
+	/* CONSIDER: using a `/dns<4 or 6>/<hostname>` multiaddr instead of resolving here.
+	 * I attempted using `/dns4/.../tcp/...` and go this error:
+	 * > failed to listen on any addresses: [can only dial TCP over IPv4 or IPv6]
+	 *
+	 * TECHDEBT: is there a way for us to effectively prefer IPv6 responses when resolving DNS?
+	 * TECHDEBT: resolving DNS this way has limitations:
+	 * > The address parameter can use a host name, but this is not recommended,
+	 * > because it will return at most one of the host name's IP addresses.
+	 * (see: https://pkg.go.dev/net#ResolveIPAddr)
+	 */
+	peerIP, err := net.ResolveIPAddr(peerIPVersionStr, peerUrl.Hostname())
+	if err != nil {
+		return nil, fmt.Errorf(
+			"resolving peer IP for hostname: %s: %w",
+			peerUrl.Hostname(),
+			err,
+		)
+	}
+
+	peerMultiAddrStr := fmt.Sprintf(
+		"/%s/%s/%s/%s",
+		peerIPVersionStr,
+		peerIP,
+		peerTransportStr,
+		peerUrl.Port(),
+	)
+	return multiaddr.NewMultiaddr(peerMultiAddrStr)
+}
+
+// ServiceUrlFromLibp2pMultiaddr converts a multiaddr into a URL string.
+func ServiceUrlFromLibp2pMultiaddr(addr multiaddr.Multiaddr) (string, error) {
+	protos := addr.Protocols()
+	if len(protos) < 2 {
+		return "", fmt.Errorf(
+			"unsupported multiaddr: %s; expected at least 2 protocols, got: %d",
+			addr, len(protos),
+		)
+	}
+
+	networkProto := protos[0]
+	// e.g. IP address: "/ip4/10.0.0.1" --> "10.0.0.1"
+	networkValue, err := addr.ValueForProtocol(networkProto.Code)
+	if err != nil {
+		return "", err
+	}
+
+	transportProto := protos[1]
+	// e.g. Port: "/tcp/42069" --> "42069"
+	transportValue, err := addr.ValueForProtocol(transportProto.Code)
+	if err != nil {
+		return "", err
+	}
+
+	// Top level proto must be a network protocol (e.g. ip4, ip6).
+	switch networkProto.Code {
+	case multiaddr.P_IP4, multiaddr.P_IP6:
+		return fmt.Sprintf("%s:%s", networkValue, transportValue), nil
+	}
+
+	return "", fmt.Errorf(
+		"unsupported network protocol, %s",
+		networkProto.Name,
+	)
+}
---
+++ libp2p/network/url_conversion_test.go
@@ -0,0 +1,159 @@
+package network
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/multiformats/go-multiaddr"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPeerMultiAddrFromServiceURL_success(t *testing.T) {
+	testCases := []struct {
+		name                  string
+		serviceUrl            string
+		expetedMultiaddrRegex string
+	}{
+		{
+			"FQDN",
+			"www.google.com:8080",
+			`/ip4/(\d{1,3}\.){3}\d{1,3}/tcp/8080`,
+		},
+		{
+			"IPv4",
+			"142.250.181.196:8080",
+			"/ip4/142.250.181.196/tcp/8080",
+		},
+		{
+			"IPv6",
+			"2a00:1450:4005:802::2004:8080",
+			"/ip6/2a00:1450:4005:802::2004/tcp/8080",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			actualMultiaddr, err := Libp2pMultiaddrFromServiceUrl(testCase.serviceUrl)
+			require.NoError(t, err)
+			require.NotNil(t, actualMultiaddr)
+			require.Regexp(t, regexp.MustCompile(testCase.expetedMultiaddrRegex), actualMultiaddr.String())
+		})
+	}
+}
+
+const (
+	FQDN = "fqdn"
+	IP4  = "ip4"
+	IP6  = "ip6"
+)
+
+func TestPeerMultiAddrFromServiceURL_error(t *testing.T) {
+	hostnames := map[string]string{
+		FQDN: "www.google.com",
+		IP4:  "142.250.181.196",
+		IP6:  "2a00:1450:4005:802::2004",
+	}
+
+	testCases := []struct {
+		name             string
+		serviceUrlFormat string
+		// TECHDEBT: assert specific errors.
+		expectedErrContains string
+	}{
+		// Usage of scheme is invalid.
+		{
+			"FQDN with scheme",
+			"tcp://%s:8080",
+			"no such host",
+		},
+
+		// Port **number** is required
+		{
+			"invalid port number",
+			"%s:abc",
+			"invalid port",
+		},
+		{
+			"missing port number",
+			"%s:",
+			"unexpected end of multiaddr",
+		},
+		// TODO: this case is tricky to detect as IPv6 addresses
+		// can omit hextet delimiters and still be valid.
+		// (see: Address_representation)
+		// {
+		// 	"missing port number and delimiter",
+		// 	"%s",
+		// },
+	}
+
+	for _, testCase := range testCases {
+		for hostType, hostname := range hostnames {
+			testName := fmt.Sprintf("%s/%s", testCase.name, hostType)
+			t.Run(testName, func(t *testing.T) {
+				serviceURL := fmt.Sprintf(testCase.serviceUrlFormat, hostname)
+				actualMultiaddr, err := Libp2pMultiaddrFromServiceUrl(serviceURL)
+				// TECHDEBT: assert specific errors
+				// Print resulting multiaddr to understand why no error.
+				require.ErrorContainsf(t, err, testCase.expectedErrContains, fmt.Sprintf("actualMultiaddr: %s", actualMultiaddr))
+			})
+		}
+	}
+}
+
+func TestServiceUrlFromLibp2pMultiaddr_success(t *testing.T) {
+	testCases := []struct {
+		name         string
+		multiaddrStr string
+		expectedUrl  string
+	}{
+		{
+			"IPv4",
+			"/ip4/142.250.181.196/tcp/8080",
+			"142.250.181.196:8080",
+		},
+		{
+			"IPv6",
+			"/ip6/2a00:1450:4005:802::2004/tcp/8080",
+			"2a00:1450:4005:802::2004:8080",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			addr, err := multiaddr.NewMultiaddr(testCase.multiaddrStr)
+			require.NoError(t, err)
+
+			actualUrl, err := ServiceUrlFromLibp2pMultiaddr(addr)
+			require.NoError(t, err)
+			require.NotNil(t, actualUrl)
+			require.Equal(t, testCase.expectedUrl, actualUrl)
+		})
+	}
+}
+
+func TestServiceUrlFromLibp2pMultiaddr_error(t *testing.T) {
+	testCases := []struct {
+		name                string
+		multiaddrStr        string
+		expectedErrContains string
+	}{
+		{
+			"FQDN",
+			"/dns/www.google.com/tcp/8080",
+			"unsupported network protocol",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			addr, err := multiaddr.NewMultiaddr(testCase.multiaddrStr)
+			require.NoError(t, err)
+
+			_, err = ServiceUrlFromLibp2pMultiaddr(addr)
+			// DISCUSS: asserting specific errors instead.
+			require.ErrorContains(t, err, testCase.expectedErrContains)
+		})
+	}
+}
