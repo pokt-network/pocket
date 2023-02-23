7bca9a324ab2faa28040e9ff4083802bf136fea2
Bryan White <bryanchriswhite@gmail.com>
2023-02-23 11:25:08 +0100

    patch: consensus signature in test

--- consensus/e2e_tests/pacemaker_test.go
+++ consensus/e2e_tests/pacemaker_test.go
@@ -182,7 +182,8 @@ func TestPacemakerCatchupSameStepDifferentRounds(t *testing.T) {
 		consensusModImpl := GetConsensusModImpl(pocketNode)
 		consensusModImpl.MethodByName("SetHeight").Call([]reflect.Value{reflect.ValueOf(testHeight)})
 		consensusModImpl.MethodByName("SetStep").Call([]reflect.Value{reflect.ValueOf(testStep)})
-		consensusModImpl.MethodByName("SetLeaderId").Call([]reflect.Value{reflect.Zero(reflect.TypeOf(&leaderId))})
+		uintLeaderId := uint64(leaderId)
+		consensusModImpl.MethodByName("SetLeaderId").Call([]reflect.Value{reflect.ValueOf(uintLeaderId)})
 
 		// utilityContext is only set on new rounds, which is skipped in this test
 		utilityContext, err := pocketNode.GetBus().GetUtilityModule().NewContext(int64(testHeight))
