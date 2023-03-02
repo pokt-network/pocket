2cf0e034ae9cb71f5d4dc2524bab3bb160744415
Bryan White <bryanchriswhite@gmail.com>
2023-03-02 10:54:49 +0100

    chore: `NetworkPeer` impelements `Peer` interface

--- p2p/types/network_peer.go
+++ p2p/types/network_peer.go
@@ -1,17 +1,38 @@
 package types
 
 import (
+	"io"
+
 	"github.com/multiformats/go-multiaddr"
 
-	cryptoPocket "github.com/pokt-network/pocket/shared/crypto"
+	"github.com/pokt-network/pocket/shared/crypto"
+	sharedP2P "github.com/pokt-network/pocket/shared/p2p"
 )
+
+var _ sharedP2P.Peer = &NetworkPeer{}
 
 type NetworkPeer struct {
 	Dialer    Transport
-	PublicKey cryptoPocket.PublicKey
-	Address   cryptoPocket.Address
+	PublicKey crypto.PublicKey
+	Address   crypto.Address
 	Multiaddr multiaddr.Multiaddr
 
 	// This is only included because it's a more human-friendly differentiator between peers
 	ServiceURL string
+}
+
+func (peer *NetworkPeer) GetAddress() crypto.Address {
+	return peer.Address
+}
+
+func (peer *NetworkPeer) GetStream() io.ReadWriteCloser {
+	return peer.Dialer
+}
+
+func (peer *NetworkPeer) GetPublicKey() crypto.PublicKey {
+	return peer.PublicKey
+}
+
+func (peer *NetworkPeer) GetServiceURL() string {
+	return peer.ServiceURL
 }
