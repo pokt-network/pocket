68e23b46b2edec5e5e9084f412d90c9db260d05f
shakezula <lott.dylan@gmail.com>
2023-03-06 16:26:34 -0700

    WIP: starts the client container
    - starts a client container assuming that a compose_and_watch local
      network is running
    - assigns that role to commander in the init steps
    - current problem is that container is not started when we try to run
      commands against it, so I think we need to ensure proper waiting and
      proper cleanup of the container.

--- e2e/tests/steps_init_test.go
+++ e2e/tests/steps_init_test.go
@@ -1,33 +1,103 @@
 package e2e
 
 import (
-	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"testing"
 
 	"github.com/cucumber/godog"
-	"github.com/pokt-network/pocket/e2e/tests/runner"
-	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"golang.org/x/net/context"
 
-	tc "github.com/testcontainers/testcontainers-go/modules/compose"
+	"github.com/pokt-network/pocket/e2e/tests/runner"
 )
 
 var (
-	// TODO: how could we not use global state here?
-	// we could consider something like badger?
-	realm *Realm
+	// commander is a reference to the container these tests start and issue commands to.
+	commander runner.PocketClient
 )
 
 func thePocketClientShouldHaveExitedWithoutError() error {
 	return godog.ErrPending
 }
+
+// debugContainer makes the debug conatiner implement the PocketClient command interface.
+type debugContainer struct {
+	runner.PocketClient
+
+	c testcontainers.Container
+}
+
+func (d *debugContainer) RunCommand(args ...string) (*runner.CommandResult, error) {
+	_, reader, err := d.c.Exec(context.Background(), []string{"echo 'hello world'"})
+	if err != nil {
+		return nil, err
+	}
+	result, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("result: %+v", result)
+	return &runner.CommandResult{
+		Stdout: string(result),
+		Stderr: err.Error(),
+		Err:    nil,
+	}, nil
+}
+
+func newDebugContainer() (*debugContainer, error) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image: "pocket/client",
+		// WaitingFor: wait.ForAll(wait.ForLog("rainTreeNetwork")), // TODO: do we need to wait at all?
+	}
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := container.Start(ctx); err != nil {
+		return nil, err
+	}
+
+	host, err := container.Host(ctx)
+	if err != nil {
+		return nil, err
+	}
+	ip, err := container.ContainerIP(ctx)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("started debug container with %s %s", host, ip)
+
+	return &debugContainer{c: container}, nil
+}
+
 func theUserHasAPocketClient() error {
-	return godog.ErrPending
+	c, err := newDebugContainer()
+	if err != nil {
+		return fmt.Errorf("failed to get debug container: %+v", err)
+	}
+	commander = c
+	return nil
 }
 
 func theUserRunsTheCommand(arg1 string) error {
-	return godog.ErrPending
+	result, err := commander.RunCommand(arg1)
+	if err != nil {
+		return err
+	}
+	if result.Err != nil {
+		return result.Err
+	}
+	if result.Stdout != "" {
+		log.Println(result.Stdout)
+	}
+	return nil
 }
 
 func theUserShouldBeAbleToSeeStandardOutputContaining(arg1 string) error {
@@ -42,19 +112,8 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^the user should be able to see standard output containing "([^"]*)"$`, theUserShouldBeAbleToSeeStandardOutputContaining)
 }
 
-// Realm is a collection of docker container pocket nodes
-type Realm struct {
-	// nodes holds the list of booted containers in this realm. They fulfill
-	// the PocketClient interface and the Realm may only interact with them
-	// through that interface.
-	nodes []runner.PocketClient
-	// suite is the test suite for this realm
-	suite godog.TestSuite
-}
-
-type networkConfig struct{} // a placeholder for networkConfig info
-
 // TestFeatures runs the e2e tests specifiedin any .features files in this directory.
+// * This test suite assumes that you have a local network running.
 // * loops over networkConfigs and runs the entire cucumebr suite against that network instance.
 // * allows support for multiple seed network configurations in the future.
 func TestFeatures(t *testing.T) {
@@ -66,44 +125,6 @@ func TestFeatures(t *testing.T) {
 			TestingT: t,
 		},
 	}
-
-	realm = &Realm{
-		nodes: make([]runner.PocketClient, 0),
-		suite: suite,
-	}
-
-	// TODO: initialize containers for testing here
-	// - docker-compose
-	// - individual containers manually wired together
-	// TODO: wrap testcontainers.Container with dockerClient to fulfill RunCommand / PocketClinet interface
-
-	compose, err := tc.NewDockerCompose("../../build/deployments/docker-compose.yaml")
-	assert.NoError(t, err, "NewDockerComposeAPI()")
-
-	t.Cleanup(func() {
-		err := compose.Down(context.Background(), tc.RemoveOrphans(true), tc.RemoveImagesLocal)
-		if err != nil {
-			t.Errorf("failed to tear down compose instance: %+v", err)
-		}
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-	t.Cleanup(cancel)
-
-	// Okay, so we get to here - progress.
-	assert.NoError(t, compose.Up(ctx, tc.Wait(true)), "compose.Up()")
-
-	// do some testing here
-
-	for _, n := range realm.nodes {
-		log.Printf("node: %+v", n)
-		result, err := n.RunCommand("echo 'hello world'")
-		if err != nil {
-			log.Fatalf("RunCommand() failed: %+v", err)
-		}
-		fmt.Printf("result: %v\n", result)
-	}
-
 	if suite.Run() != 0 {
 		t.Fatal("non-zero status returned, failed to run feature tests")
 	}
