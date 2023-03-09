69a8fe1ff457740e7e8e1ef77f738ac95b104359
Alessandro De Blasis <alex@deblasis.net>
2023-03-09 21:32:32 +0000

    feat(utility): leader utilityUnitOfWork

---
+++ utility/unit_of_work/leader.go
@@ -0,0 +1,42 @@
+package unit_of_work
+
+import (
+	"github.com/pokt-network/pocket/logger"
+	"github.com/pokt-network/pocket/shared/modules"
+)
+
+var (
+	_ modules.UtilityUnitOfWork       = &leaderUtilityUnitOfWork{}
+	_ modules.LeaderUtilityUnitOfWork = &leaderUtilityUnitOfWork{}
+)
+
+type leaderUtilityUnitOfWork struct {
+	baseUtilityUnitOfWork
+}
+
+func NewForLeader(height int64, readContext modules.PersistenceReadContext, rwPersistenceContext modules.PersistenceRWContext) *leaderUtilityUnitOfWork {
+	return &leaderUtilityUnitOfWork{
+		baseUtilityUnitOfWork: baseUtilityUnitOfWork{
+			persistenceReadContext: readContext,
+			persistenceRWContext:   rwPersistenceContext,
+			logger:                 logger.Global.CreateLoggerForModule(leaderUtilityUOWModuleName),
+		},
+	}
+}
+
+func (uow *leaderUtilityUnitOfWork) CreateProposalBlock(proposer []byte, maxTxBytes int, beforeApplyBlock, afterApplyBlock func(modules.UtilityUnitOfWork) error) (stateHash string, txs [][]byte, err error) {
+	if beforeApplyBlock != nil {
+		uow.logger.Debug().Msg("running beforeApplyBlock...")
+		if err := beforeApplyBlock(uow); err != nil {
+			return "", nil, err
+		}
+	}
+
+	if afterApplyBlock != nil {
+		uow.logger.Debug().Msg("running afterApplyBlock...")
+		if err := afterApplyBlock(uow); err != nil {
+			return "", nil, err
+		}
+	}
+	panic("unimplemented")
+}
