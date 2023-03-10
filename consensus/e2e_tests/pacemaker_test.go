5990ded3a7fb886786ecdfc841bc4c88f66e3b63
Alessandro De Blasis <alex@deblasis.net>
2023-03-10 00:13:40 +0000

    refactor(consensus): pacemaker_test uow

--- consensus/e2e_tests/pacemaker_test.go
+++ consensus/e2e_tests/pacemaker_test.go
@@ -182,11 +182,10 @@ func TestPacemakerCatchupSameStepDifferentRounds(t *testing.T) {
 		consensusModImpl.MethodByName("SetHeight").Call([]reflect.Value{reflect.ValueOf(testHeight)})
 		consensusModImpl.MethodByName("SetStep").Call([]reflect.Value{reflect.ValueOf(testStep)})
 
-		// TODO: @deblasis - refactor using uow
-		// utilityContext is only set on new rounds, which is skipped in this test
-		utilityContext, err := pocketNode.GetBus().GetUtilityModule().NewContext(int64(testHeight))
+		// utilityUnitOfWork is only set on new rounds, which is skipped in this test
+		utilityUnitOfWork, err := pocketNode.GetBus().GetUtilityModule().NewUnitOfWork(int64(testHeight))
 		require.NoError(t, err)
-		consensusModImpl.MethodByName("SetUtilityContext").Call([]reflect.Value{reflect.ValueOf(utilityContext)})
+		consensusModImpl.MethodByName("SetUtilityUnitOfWork").Call([]reflect.Value{reflect.ValueOf(utilityUnitOfWork)})
 	}
 
 	// Set the leader to be in the highest round.
