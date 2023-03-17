78a6bec76dded4da6722e986b89152bb37482530
goku <118421317+gokutheengineer@users.noreply.github.com>
2023-03-17 09:49:41 +0300

    Update shared/messaging/events.go
    Co-authored-by: Daniel Olshansky <olshansky@pokt.network>

--- shared/messaging/events.go
+++ shared/messaging/events.go
@@ -12,7 +12,6 @@ const (
 
 // Helper logger for state sync tranition events
 func TransitionEventToMap(stateSyncMsg *StateMachineTransitionEvent) map[string]any {
-
 	return map[string]any{
 		"state_machine_event": stateSyncMsg.Event,
 		"previous_state":      stateSyncMsg.PreviousState,
