998bc353382e3a827f4362719655789c32a2be63
Bryan White <bryanchriswhite@gmail.com>
2023-03-21 14:00:08 +0100

    test: ensure transport encryption is required

---
+++ p2p/transport_encryption_test.go
@@ -0,0 +1,103 @@
+package p2p
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/libp2p/go-libp2p"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/stretchr/testify/require"
+
+	"github.com/pokt-network/pocket/p2p/protocol"
+	typesP2P "github.com/pokt-network/pocket/p2p/types"
+	"github.com/pokt-network/pocket/p2p/utils"
+	"github.com/pokt-network/pocket/runtime/configs"
+	"github.com/pokt-network/pocket/runtime/configs/types"
+	"github.com/pokt-network/pocket/runtime/defaults"
+	cryptoPocket "github.com/pokt-network/pocket/shared/crypto"
+	"github.com/pokt-network/pocket/shared/modules"
+	mockModules "github.com/pokt-network/pocket/shared/modules/mocks"
+)
+
+func TestP2pModule_Insecure_Error(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	hostname := "127.0.0.1"
+
+	privKey := cryptoPocket.GetPrivKeySeed(1)
+
+	runtimeMgrMock := mockModules.NewMockRuntimeMgr(ctrl)
+	busMock := createMockBus(t, runtimeMgrMock)
+
+	telemetryMock := mockModules.NewMockTelemetryModule(ctrl)
+	timeSeriesAgentMock := prepareNoopTimeSeriesAgentMock(t)
+	eventMetricsAgentMock := mockModules.NewMockEventMetricsAgent(ctrl)
+	eventMetricsAgentMock.EXPECT().EmitEvent(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+
+	telemetryMock.EXPECT().GetTimeSeriesAgent().Return(timeSeriesAgentMock).AnyTimes()
+	telemetryMock.EXPECT().GetEventMetricsAgent().Return(eventMetricsAgentMock).AnyTimes()
+
+	telemetryMock.EXPECT().GetModuleName().Return(modules.TelemetryModuleName).AnyTimes()
+	telemetryMock.EXPECT().GetBus().Return(busMock).AnyTimes()
+	telemetryMock.EXPECT().SetBus(busMock).AnyTimes()
+	busMock.EXPECT().GetTelemetryModule().Return(telemetryMock).AnyTimes()
+
+	genesisStateMock := createMockGenesisState(keys[:1])
+	persistenceMock := preparePersistenceMock(t, busMock, genesisStateMock)
+	busMock.EXPECT().GetPersistenceModule().Return(persistenceMock).AnyTimes()
+
+	mockConsensusModule := mockModules.NewMockConsensusModule(ctrl)
+	mockConsensusModule.EXPECT().CurrentHeight().Return(uint64(1)).AnyTimes()
+	busMock.EXPECT().GetConsensusModule().Return(mockConsensusModule).AnyTimes()
+	runtimeMgrMock.EXPECT().GetConfig().Return(&configs.Config{
+		PrivateKey: privKey.String(),
+		P2P: &configs.P2PConfig{
+			PrivateKey:     privKey.String(),
+			Hostname:       hostname,
+			Port:           defaults.DefaultP2PPort,
+			ConnectionType: types.ConnectionType_TCPConnection,
+		},
+	}).AnyTimes()
+	busMock.EXPECT().GetRuntimeMgr().Return(runtimeMgrMock).AnyTimes()
+
+	p2pMod, err := Create(busMock)
+	require.NoError(t, err)
+
+	err = p2pMod.Start()
+	require.NoError(t, err)
+	defer func() {
+		_ = p2pMod.Stop()
+	}()
+
+	// Setup cleartext transport node
+	clearNodeMultiAddrStr := fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", defaults.DefaultP2PPort+1)
+	clearNodeAddr, err := multiaddr.NewMultiaddr(clearNodeMultiAddrStr)
+	require.NoError(t, err)
+
+	clearNode, err := libp2p.New(libp2p.NoSecurity, libp2p.ListenAddrs(clearNodeAddr))
+	require.NoError(t, err)
+	defer func() {
+		_ = clearNode.Close()
+	}()
+
+	p2pModPeer := &typesP2P.NetworkPeer{
+		PublicKey:  privKey.PublicKey(),
+		Address:    privKey.Address(),
+		ServiceURL: fmt.Sprintf("%s:%d", hostname, defaults.DefaultP2PPort),
+	}
+
+	libp2pPeerInfo, err := utils.Libp2pAddrInfoFromPeer(p2pModPeer)
+	require.NoError(t, err)
+
+	libp2pPubKey, err := utils.Libp2pPublicKeyFromPeer(p2pModPeer)
+	require.NoError(t, err)
+
+	clearNode.Peerstore().AddAddrs(libp2pPeerInfo.ID, libp2pPeerInfo.Addrs, utils.DefaultPeerTTL)
+	err = clearNode.Peerstore().AddPubKey(libp2pPeerInfo.ID, libp2pPubKey)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+	_, err = clearNode.NewStream(ctx, libp2pPeerInfo.ID, protocol.PoktProtocolID)
+	require.ErrorContains(t, err, "failed to negotiate security protocol: protocols not supported:")
+}
