258f1a45ed91c8fae2f8c7f7e4436aa44958ca68
BigBoss <bigboss@metalrodeo.xyz>
2023-03-25 00:41:16 -0400

    clean up vault_test.go

--- app/client/keybase/hashicorp/vault_test.go
+++ app/client/keybase/hashicorp/vault_test.go
@@ -30,13 +30,13 @@ func TestMain(m *testing.M) {
 	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
 	pool, err := dockertest.NewPool("")
 	if err != nil {
-		log.Fatalf("Could not construct pool: %s", err)
+		log.Fatalf("Could not construct pool")
 	}
 
 	// uses pool to try to connect to Docker
 	err = pool.Client.Ping()
 	if err != nil {
-		log.Fatalf("Could not connect to Docker: %s", err)
+		log.Fatalf("Could not connect to Docker")
 	}
 
 	// pulls an image, creates a container based on it and runs it
@@ -57,12 +57,12 @@ func TestMain(m *testing.M) {
 	})
 
 	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
+		log.Fatalf("Could not start resource")
 	}
 
 	err = resource.Expire(120) // Tell docker to hard kill the container in 2 minutes
 	if err != nil {
-		log.Fatalf("Could not set expiration on resource: %s", err)
+		log.Fatalf("Could not set expiration on resource")
 	}
 
 	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
@@ -82,94 +82,89 @@ func TestMain(m *testing.M) {
 
 		return err
 	}); err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
+		log.Fatalf("Could not connect to docker")
 	}
 
 	code := m.Run()
 
 	// You can't defer this because os.Exit doesn't care for defer
 	if err := pool.Purge(resource); err != nil {
-		log.Fatalf("Could not purge resource: %s", err)
+		log.Fatalf("Could not purge resource")
 	}
 
 	os.Exit(code)
 }
 
 func TestVaultKeybase(t *testing.T) {
-
-	if vk == nil {
-		require.NoErrorf(t, fmt.Errorf("vault keybase is nil"), "vault keybase is nil")
-	}
+	require.NotNil(t, vk, "vault keybase is nil")
 
 	// Test Create
 	_, err := vk.Create("passphrase", "hint")
-	require.NoErrorf(t, err, "error creating keypair: %s", err)
+	require.NoError(t, err, "error creating keypair")
 
 	// Generate a new key pair
 	privKey, err := crypto.GeneratePrivateKey()
-	require.NoErrorf(t, err, "error generating key pair: %s", err)
+	require.NoError(t, err, "error generating key pair")
 
 	// Test ImportFromString
 	_, err = vk.ImportFromString(privKey.String(), "passphrase", "hint")
-	require.NoErrorf(t, err, "error importing keypair: %s")
+	require.NoError(t, err, "error importing keypair")
 
 	// Test GetPrivKey
 	origPrivKey := privKey.String()
 	privKey, err = vk.GetPrivKey(privKey.Address().String(), "passphrase")
-	require.NoErrorf(t, err, "error getting private key: %s")
+	require.NoError(t, err, "error getting private key")
 	assert.Equal(t, origPrivKey, privKey.String())
 
 	// Test ImportFromJSON
 	keyPair, err := crypto.CreateNewKey("passphrase2", "hint")
-	require.NoErrorf(t, err, "error creating key pair: %s")
+	require.NoError(t, err, "error creating key pair")
 	keyPairBytes, err := keyPair.ExportJSON("passphrase2")
-	require.NoErrorf(t, err, "error marshaling key pair: %s")
+	require.NoError(t, err, "error marshaling key pair")
 	importedKp, err := vk.ImportFromJSON(string(keyPairBytes), "passphrase2")
-	require.NoErrorf(t, err, "error importing keypair: %s")
+	require.NoError(t, err, "error importing keypair")
 	assert.Equal(t, keyPair, importedKp)
 
 	// Test Get
 	keyPair2, err := vk.Get(keyPair.GetAddressString())
-	require.NoErrorf(t, err, "error getting keypair: %s")
+	require.NoError(t, err, "error getting keypair")
 	assert.Equal(t, keyPair, keyPair2)
 
 	// Test GetPubKey
 	pubKey, err := vk.GetPubKey(keyPair.GetAddressString())
-	require.NoErrorf(t, err, "error getting public key: %s")
+	require.NoError(t, err, "error getting public key")
 	assert.Equal(t, keyPair.GetPublicKey(), pubKey)
 
 	// Test GetAll
 	addresses, keyPairs, err := vk.GetAll()
-	require.NoErrorf(t, err, "error getting all keypairs: %s")
+	require.NoError(t, err, "error getting all keypairs")
 	assert.Equal(t, 3, len(addresses))
 	assert.Equal(t, 3, len(keyPairs))
 
 	// Test ExportPrivString
 	privKeyStr, err := vk.ExportPrivString(privKey.Address().String(), "passphrase")
-	require.NoErrorf(t, err, "error exporting private key: %s")
+	require.NoError(t, err, "error exporting private key")
 	assert.Equal(t, privKey.String(), privKeyStr)
 
 	// Test ExportPrivJSON
 	_, err = vk.ExportPrivJSON(keyPair.GetAddressString(), "passphrase2")
-	require.NoErrorf(t, err, "error exporting private key: %s")
+	require.NoError(t, err, "error exporting private key")
 
 	// Test UpdatePassphrase
 	err = vk.UpdatePassphrase(keyPair.GetAddressString(), "passphrase2", "new-passphrase", "hint")
-	require.NoErrorf(t, err, "error updating passphrase: %s")
+	require.NoError(t, err, "error updating passphrase")
 
-	// // Test Sign
+	// Test Sign
 	msg := []byte("hello world")
 	sig, err := vk.Sign(keyPair.GetAddressString(), "new-passphrase", msg)
-	require.NoErrorf(t, err, "error signing message: %s")
+	require.NoError(t, err, "error signing message")
 
 	// Test Verify
 	verified, err := vk.Verify(keyPair.GetAddressString(), msg, sig)
-	require.NoErrorf(t, err, "error verifying signature: %s")
+	require.NoError(t, err, "error verifying signature")
 
-	if !verified {
-		require.NoErrorf(t, fmt.Errorf("signature not verified"), "signature not verified")
-	}
+	require.True(t, verified, "signature not verified")
 
 	err = vk.Delete(keyPair.GetAddressString(), "new-passphrase")
-	require.NoErrorf(t, err, "error deleting keypair: %s")
+	require.NoError(t, err, "error deleting keypair")
 }
