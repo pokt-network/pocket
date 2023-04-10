ed1fa4ca7863c6db2c827b91de90032500546154
Bryan White <bryanchriswhite@gmail.com>
2023-04-10 11:23:22 +0200

    chore: improve factory type names

--- shared/modules/factory.go
+++ shared/modules/factory.go
@@ -4,9 +4,9 @@ package modules
 // a variadic `ModuleOption` argument(s) and returns a `Module`and an error.
 type ModuleFactoryWithOptions FactoryWithOptions[Module, ModuleOption]
 
-// FactoryWithRequired implements a `#Create()` factory method which takes a
+// FactoryWithConfig implements a `#Create()` factory method which takes a
 // required "config" argument of type K and returns a value of type T and an error.
-type FactoryWithRequired[T interface{}, K interface{}] interface {
+type FactoryWithConfig[T interface{}, K interface{}] interface {
 	Create(bus Bus, cfg K) (T, error)
 }
 
@@ -17,9 +17,9 @@ type FactoryWithOptions[T interface{}, O interface{}] interface {
 	Create(bus Bus, opts ...O) (T, error)
 }
 
-// FactoryWithRequiredAndOptions implements a `#Create()` factory method which
+// FactoryWithConfigAndOptions implements a `#Create()` factory method which
 // takes both a required "config" argument of type K and a variadic "optional"
 // argument(s) of type O and returns a value of type T and an error.
-type FactoryWithRequiredAndOptions[T interface{}, K interface{}, O interface{}] interface {
+type FactoryWithConfigAndOptions[T interface{}, K interface{}, O interface{}] interface {
 	Create(bus Bus, cfg K, opts ...O) (T, error)
 }
