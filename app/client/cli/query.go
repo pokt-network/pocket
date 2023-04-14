5bc244d23ed5bc2b761568ec74f4a7cf55188da2
Harry Law <hrryslw@pm.me>
2023-04-14 21:12:31 +0100

    Add CLI endpoints

--- app/client/cli/query.go
+++ app/client/cli/query.go
@@ -32,23 +32,28 @@ func NewQueryCommand() *cobra.Command {
 
 	heightCmds := queryHeightCommands()
 	heightPaginatedCmds := queryHeightPaginatedCommands()
-	addressPaginatedCmds := queryAddressPaginatedCommands()
+	heightPaginatedSortedCmds := queryHeightPaginatedSortedCommands()
+	paginatedSortedCmds := queryPaginatedSortedCommands()
 	getCmds := queryCommands()
 
 	// attach --height flag
 	applySubcommandOptions(heightCmds, attachHeightFlagToSubcommands())
 	applySubcommandOptions(heightPaginatedCmds, attachHeightFlagToSubcommands())
+	applySubcommandOptions(heightPaginatedSortedCmds, attachHeightFlagToSubcommands())
 
 	// attach --page, --per_page flags
 	applySubcommandOptions(heightPaginatedCmds, attachPaginationFlagsToSubcommands())
-	applySubcommandOptions(addressPaginatedCmds, attachPaginationFlagsToSubcommands())
+	applySubcommandOptions(paginatedSortedCmds, attachPaginationFlagsToSubcommands())
+	applySubcommandOptions(heightPaginatedSortedCmds, attachPaginationFlagsToSubcommands())
 
 	// attach --sort flag
-	applySubcommandOptions(addressPaginatedCmds, attachSortFlagToSubcommands())
+	applySubcommandOptions(paginatedSortedCmds, attachSortFlagToSubcommands())
+	applySubcommandOptions(heightPaginatedSortedCmds, attachSortFlagToSubcommands())
 
 	cmd.AddCommand(heightCmds...)
 	cmd.AddCommand(heightPaginatedCmds...)
-	cmd.AddCommand(addressPaginatedCmds...)
+	cmd.AddCommand(heightPaginatedSortedCmds...)
+	cmd.AddCommand(paginatedSortedCmds...)
 	cmd.AddCommand(getCmds...)
 
 	return cmd
@@ -74,6 +79,41 @@ func queryHeightCommands() []*cobra.Command {
 				}
 
 				response, err := client.PostV1QueryAccount(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
+		{
+			Use:     "App <address> [--height]",
+			Short:   "Get the app data of an address at a specified height",
+			Long:    "Queries the node RPC to obtain the app data of the speicifed address at the given height",
+			Args:    cobra.ExactArgs(1),
+			Aliases: []string{"app"},
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryAddressHeight{
+					Address: args[0],
+					Height:  height,
+				}
+
+				response, err := client.PostV1QueryApp(cmd.Context(), body)
 				if err != nil {
 					return unableToConnectToRpc(err)
 				}
@@ -160,6 +200,75 @@ func queryHeightCommands() []*cobra.Command {
 				return rpcResponseCodeUnhealthy(statusCode, resp)
 			},
 		},
+		{
+			Use:     "Param <parameter_name> [--height]",
+			Short:   "Get the value of the parameter at the specified height",
+			Long:    "Queries the node RPC to obtain the value of the specified parameter",
+			Aliases: []string{"param"},
+			Args:    cobra.ExactArgs(1),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryParameter{
+					ParamName: args[0],
+					Height:    height,
+				}
+
+				response, err := client.PostV1QueryParam(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
+		{
+			Use:     "Upgrade [--height]",
+			Short:   "Get the upgrade version at the specified height",
+			Long:    "Queries the node RPC to obtain the upgrade version for the specified height",
+			Aliases: []string{"param"},
+			Args:    cobra.ExactArgs(0),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryHeight{
+					Height: height,
+				}
+
+				response, err := client.PostV1QueryUpgrade(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
 	}
 
 	return cmds
@@ -203,12 +312,92 @@ func queryHeightPaginatedCommands() []*cobra.Command {
 				return rpcResponseCodeUnhealthy(statusCode, resp)
 			},
 		},
+		{
+			Use:     "Apps [--height] [--page] [--per_page]",
+			Short:   "Get all the data of all apps the specified height",
+			Long:    "Queries the node RPC to obtain the paginated data for all apps at the given height",
+			Args:    cobra.ExactArgs(0),
+			Aliases: []string{"apps"},
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryHeightPaginated{
+					Height:  height,
+					Page:    page,
+					PerPage: per_page,
+				}
+
+				response, err := client.PostV1QueryApps(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
 	}
 
 	return cmds
 }
 
-func queryAddressPaginatedCommands() []*cobra.Command {
+func queryHeightPaginatedSortedCommands() []*cobra.Command {
+	cmds := []*cobra.Command{
+		{
+			Use:     "BlockTxs [--height] [--page] [--per_page] [--sort]",
+			Short:   "Get all the transactions in the block with the specified height",
+			Long:    "Queries the node RPC to obtain the paginated transactions in the block at the given height",
+			Args:    cobra.ExactArgs(0),
+			Aliases: []string{"blocktxs"},
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryHeightPaginated{
+					Height:  height,
+					Page:    page,
+					PerPage: per_page,
+					Sort:    &sort,
+				}
+
+				response, err := client.PostV1QueryBlocktxs(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
+	}
+
+	return cmds
+}
+
+func queryPaginatedSortedCommands() []*cobra.Command {
 	cmds := []*cobra.Command{
 		{
 			Use:     "AccountTxs <address> [--page] [--per_page] [--sort]",
@@ -230,6 +419,42 @@ func queryAddressPaginatedCommands() []*cobra.Command {
 				}
 
 				response, err := client.PostV1QueryAccounttxs(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
+		{
+			Use:     "UnconfirmedTxs [--page] [--per_page] [--sort]",
+			Short:   "Get all the unconfirmed transaction data from the mempool",
+			Long:    "Queries the node RPC to obtain the paginated data for all unconfirmed transactions from the mempool",
+			Args:    cobra.ExactArgs(1),
+			Aliases: []string{"unconfirmedtxs"},
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryPaginated{
+					Page:    page,
+					PerPage: per_page,
+					Sort:    &sort,
+				}
+
+				response, err := client.PostV1QueryUnconfirmedtxs(cmd.Context(), body)
 				if err != nil {
 					return unableToConnectToRpc(err)
 				}
@@ -279,6 +504,101 @@ func queryCommands() []*cobra.Command {
 					return nil
 				}
 				return rpcResponseCodeUnhealthy(statusCode, body)
+			},
+		},
+		{
+			Use:     "Height",
+			Short:   "Get current block height",
+			Long:    "Queries the node RPC to obtain the current block height",
+			Aliases: []string{"height"},
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+				response, err := client.GetV1QueryHeight(cmd.Context())
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				body, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(body))
+					return nil
+				}
+				return rpcResponseCodeUnhealthy(statusCode, body)
+			},
+		},
+		{
+			Use:     "Transaction <hash>",
+			Short:   "Get the transaction data the hash provided",
+			Long:    "Queries the node RPC to obtain the transaction data for the specified hash",
+			Aliases: []string{"tx"},
+			Args:    cobra.ExactArgs(1),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryHash{
+					Hash: args[0],
+				}
+
+				response, err := client.PostV1QueryTx(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
+			},
+		},
+		{
+			Use:     "UnconfirmedTransaction <hash>",
+			Short:   "Get the unconfirmed transaction data the hash provided",
+			Long:    "Queries the node RPC to obtain the unconfirmed transaction data for the specified hash, from the mempool",
+			Aliases: []string{"unconfirmedtx"},
+			Args:    cobra.ExactArgs(1),
+			RunE: func(cmd *cobra.Command, args []string) error {
+				client, err := rpc.NewClientWithResponses(remoteCLIURL)
+				if err != nil {
+					return err
+				}
+
+				body := rpc.QueryHash{
+					Hash: args[0],
+				}
+
+				response, err := client.PostV1QueryUnconfirmedtx(cmd.Context(), body)
+				if err != nil {
+					return unableToConnectToRpc(err)
+				}
+				statusCode := response.StatusCode
+				resp, err := io.ReadAll(response.Body)
+				if err != nil {
+					logger.Global.Error().Err(err).Msg("Error reading response body")
+					return err
+				}
+				if statusCode == http.StatusOK {
+					fmt.Println(string(resp))
+					return nil
+				}
+
+				return rpcResponseCodeUnhealthy(statusCode, resp)
 			},
 		},
 	}
