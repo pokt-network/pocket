f50df0df635a476428e23e900d3370e4f7d5961a
shakezula <lott.dylan@gmail.com>
2023-04-24 12:45:16 -0600

    [WIP] adding the GetAll method to PG store

--- persistence/kvstore/pg_store.go
+++ persistence/kvstore/pg_store.go
@@ -2,6 +2,8 @@ package kvstore
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
@@ -23,6 +25,14 @@ func (p *PostgresKV) Get(key []byte) ([]byte, error) {
 	defer conn.Conn().Close(ctx)
 
 	row := conn.QueryRow(ctx, "SELECT FROM transactions WHERE key=$1", key)
+	var val []byte
+	if err := row.Scan(&val); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrKVStoreNotExists
+		}
+		return nil, err
+	}
+	return val, nil
 }
 
 // Set ...
@@ -49,7 +59,7 @@ func (p *PostgresKV) Set(key []byte, value []byte) error {
 	}
 
 	fmt.Printf("inserted %d", res.RowsAffected())
-	fmt.Printf("res#String: %v", res.String)
+	fmt.Printf("res#String: %s", res.String())
 
 	return nil
 }
@@ -73,6 +83,8 @@ func (p *PostgresKV) Delete(key []byte) error {
 		return err
 	}
 
+	fmt.Printf("res.Delete(): %v\n", res.Delete())
+
 	if err := tx.Commit(ctx); err != nil {
 		return err
 	}
@@ -84,14 +96,50 @@ func (p *PostgresKV) Delete(key []byte) error {
 
 // Stop ...
 func (p *PostgresKV) Stop() error {
-	panic("not implemented") // TODO: Implement
+	return nil
 }
 
 // Accessors
 
-// GetAll ...
+// GetAll gets all of the keys that fit the prefix
 func (p *PostgresKV) GetAll(prefixKey []byte, descending bool) (keys [][]byte, values [][]byte, err error) {
-	panic("not implemented") // TODO: Implement
+	ctx := context.TODO()
+	conn, err := p.Pool.Acquire(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer conn.Conn().Close(ctx)
+
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+	defer tx.Rollback(ctx)
+
+	// TODO IN THIS COMMIT Modify query to ORDER BY to support descending bool in funciton args
+	prefixQuery := `WITH prefix_match(prefix) AS (
+		VALUES ('your_prefix_here')
+	  )
+	  SELECT *
+	  FROM your_table
+	  WHERE key LIKE (SELECT prefix || '%' FROM prefix_match);
+	`
+	rows, err := tx.Query(ctx, prefixQuery, prefixKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for rows.Next() {
+		var k, v []byte
+		if err := rows.Scan(&k, &v); err != nil {
+			return nil, nil, err
+		}
+		keys = append(keys, k)
+		values = append(values, v)
+		fmt.Printf("key: %+v - value: %+v", k, v)
+	}
+
+	return keys, values, nil
 }
 
 // Exists ...
