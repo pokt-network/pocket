818adf51fb080313eee1c6b6d944997f9731a478
Dmitry K <okdas@pm.me>
2023-04-24 12:44:24 -0700

    namespaced cluster-manager

--- shared/k8s/debug.go
+++ shared/k8s/debug.go
@@ -2,13 +2,29 @@ package k8s
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
 
+	"github.com/pokt-network/pocket/logger"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
 //nolint:gosec // G101 Not a credential
 const privateKeysSecretResourceName = "v1-localnet-validators-private-keys"
+
+var CurrentNamespace = ""
+
+func init() {
+	var err error
+	CurrentNamespace, err = getNamespace()
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Global.Info().Str("namespace", CurrentNamespace).Msg("using namespace")
+}
 
 func FetchValidatorPrivateKeys(clientset *kubernetes.Clientset) (map[string]string, error) {
 	validatorKeysMap := make(map[string]string)
@@ -23,4 +39,18 @@ func FetchValidatorPrivateKeys(clientset *kubernetes.Clientset) (map[string]stri
 		validatorKeysMap[id] = string(privHexString)
 	}
 	return validatorKeysMap, nil
+}
+
+func getNamespace() (string, error) {
+	nsFile := "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+
+	if _, err := os.Stat(nsFile); err == nil {
+		nsBytes, err := ioutil.ReadFile(nsFile)
+		if err != nil {
+			return "", fmt.Errorf("could not read namespace file: %v", err)
+		}
+		return string(nsBytes), nil
+	}
+
+	return "default", nil
 }
