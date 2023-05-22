8ed5a7d6debf2a43d2613f51f3942db055fd58b1
Bryan White <bryanchriswhite@gmail.com>
2023-05-22 11:35:51 +0200

    wip: background router testutils

--- p2p/background/testutil.go
+++ p2p/background/testutil.go
@@ -1,1 +1,6 @@
+//go:build test
+
 package background
+
+// BackgroundRouter exports `backgroundRouter` for testing purposes.
+type BackgroundRouter = backgroundRouter
--- p2p/testutil.go
+++ p2p/testutil.go
@@ -2,5 +2,25 @@
 
 package p2p
 
+import (
+	libp2pHost "github.com/libp2p/go-libp2p/core/host"
+
+	"github.com/pokt-network/pocket/p2p/raintree"
+)
+
 // P2PModule exports the `p2pModule` type for use in tests
 type P2PModule = p2pModule
+
+func (m *p2pModule) GetHost() libp2pHost.Host {
+	return m.host
+}
+
+// GetRainTreeRouter returns the `RainTreeRouter` for use in integration tests
+func (m *p2pModule) GetRainTreeRouter() *raintree.RainTreeRouter {
+	return m.router.(*raintree.RainTreeRouter)
+}
+
+// GetBackgroundRouter returns the `BackgroundRouter` for use in integration tests
+//func (m *p2pModule) GetBackgroundRouter() background.BackgroundRouter {
+//	return m.backgroundRouter
+//}
