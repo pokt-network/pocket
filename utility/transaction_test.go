a6d04a967f85e8b7bfeeffaf7eb56ae99a46c4a1
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-05-22 20:09:52 -0700

    WIP

--- utility/transaction_test.go
+++ utility/transaction_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	mockPersistence "github.com/pokt-network/pocket/persistence/types/mocks"
 	"github.com/pokt-network/pocket/shared/core/types"
+	coreTypes "github.com/pokt-network/pocket/shared/core/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
@@ -16,43 +16,58 @@ func TestGetIndexedTransaction(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	// Test data
-	tx := types.Transaction{}
-	txProtoBytes, err := proto.Marshal(&tx)
+	// Test data - Prepare Transaction
+	emptyTx := types.Transaction{}
+	txProtoBytes, err := proto.Marshal(&emptyTx)
 	require.NoError(t, err)
-	// txString := string(txProtoBytes)
+
+	// Test data - Prepare IndexedTransaction
+	idxTx := &coreTypes.IndexedTransaction{
+		Tx:            txProtoBytes,
+		Height:        0,
+		Index:         0,
+		ResultCode:    0,
+		Error:         "TODO",
+		SignerAddr:    "TODO",
+		RecipientAddr: "TODO",
+		MessageType:   "TODO",
+	}
+
+	// Prepare the environment
+	_, utilityMod, persistenceMod := prepareEnvironment(t, 0, 0, 0, 0)
+	txIndexer := persistenceMod.GetTxIndexer()
+	err = txIndexer.Index(idxTx)
+	require.NoError(t, err)
 
 	tests := []struct {
 		name            string
+		txProtoBytes    []byte
 		txExists        bool
 		txExistsErr     error
 		getByHashErr    error
 		expectErr       error
 		expectIndexedTx bool
 	}{
-		{"returns indexed transaction when it exists", true, nil, nil, nil, true},
-		{"returns error when transaction doesn't exist", false, nil, nil, error(types.ErrTransactionNotCommitted()), false},
-		{"handles error from TransactionExists", false, fmt.Errorf("some error"), nil, fmt.Errorf("some error"), false},
-		{"handles error from GetByHash", true, nil, fmt.Errorf("some error"), fmt.Errorf("some error"), false},
+		{"returns indexed transaction when it exists", txProtoBytes, true, nil, nil, nil, true},
+		{"returns error when transaction doesn't exist", []byte("Does not exist"), false, nil, nil, error(types.ErrTransactionNotCommitted()), false},
+		// {"handles error from TransactionExists", false, fmt.Errorf("some error"), nil, fmt.Errorf("some error"), false},
+		// {"handles error from GetByHash", true, nil, fmt.Errorf("some error"), fmt.Errorf("some error"), false},
 	}
-
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+
 			// mockPersistenceMod := mockModules.NewMockPersistenceModule(ctrl)
 			// mockPersistenceMod.EXPECT().SetBus(gomock.Any()).AnyTimes()
 			// mockPersistenceMod.EXPECT().GetModuleName().Return(modules.PersistenceModuleName).AnyTimes()
 
-			// Preparing the environment
-			_, utilityMod := prepareEnvironmentWithPersistenceMock(t, 0, 0, 0, 0, mockPersistenceMod)
-
 			// Mock TransactionExists method
 			// mockPersistenceMod.EXPECT().TransactionExists(gomock.Eq(txString)).Return(test.txExists, test.txExistsErr)
-			mockPersistenceMod.EXPECT().TransactionExists(gomock.Any()).Return(test.txExists, test.txExistsErr)
+			// mockPersistenceMod.EXPECT().TransactionExists(gomock.Any()).Return(test.txExists, test.txExistsErr)
 
 			// Mock GetTxIndexer method
-			mockIndexer := mockPersistence.NewMockTxIndexer(ctrl)
-			mockIndexer.EXPECT().GetByHash(gomock.Any()).Return(&types.IndexedTransaction{}, test.getByHashErr)
-			mockPersistenceMod.EXPECT().GetTxIndexer().Return(mockIndexer)
+			// mockIndexer := mockPersistence.NewMockTxIndexer(ctrl)
+			// mockIndexer.EXPECT().GetByHash(gomock.Any()).Return(&types.IndexedTransaction{}, test.getByHashErr)
+			// mockPersistenceMod.EXPECT().GetTxIndexer().Return(mockIndexer)
 
 			idTx, err := utilityMod.GetIndexedTransaction(txProtoBytes)
 			if test.expectErr != nil {
