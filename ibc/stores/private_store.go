2756aeacb003805ca2bd6256472e2a923e5323e7
Harry Law <53987565+h5law@users.noreply.github.com>
2023-05-30 17:31:46 +0100

    Add comments to struct methods

--- ibc/stores/private_store.go
+++ ibc/stores/private_store.go
@@ -17,14 +17,17 @@ func (priv *PrivateStore) GetStoreKey() string {
 	return priv.storeKey
 }
 
+// Get returns a value stored in the KVStore
 func (priv *PrivateStore) Get(key []byte) ([]byte, error) {
 	return priv.KVStore.Get(key)
 }
 
+// Set sets a value in the KVStore
 func (priv *PrivateStore) Set(key, value []byte) error {
 	return priv.KVStore.Set(key, value)
 }
 
+// Delete deletes a value from the KVStore
 func (priv *PrivateStore) Delete(key []byte) error {
 	return priv.KVStore.Delete(key)
 }
