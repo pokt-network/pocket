4e8a569ce34aa030f832c2fcb512bcce3e372b4c
Harry Law <53987565+h5law@users.noreply.github.com>
2023-05-30 15:45:31 +0100

    Add populate store function

--- ibc/stores/store_manager.go
+++ ibc/stores/store_manager.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"sync"
 
+	"github.com/pokt-network/pocket/ibc/host"
 	"github.com/pokt-network/pocket/persistence/kvstore"
 	coreTypes "github.com/pokt-network/pocket/shared/core/types"
 	"github.com/pokt-network/pocket/shared/modules"
@@ -31,7 +32,7 @@ func NewTestStore(storeKey string, provable bool) (modules.Store, error) {
 		return &PrivateStore{db, storeKey}, nil
 	}
 	// Create a new SMT with no value hasher to store the unhashed value bytes in the tree
-	tree := smt.NewSparseMerkleTree(db, sha256.New(), smt.WithValueHasher(nil))
+	tree := smt.NewSparseMerkleTree(db, sha256.New(), noValueHasher)
 	return &ProvableStore{db, tree, storeKey}, nil
 }
 
@@ -47,8 +48,21 @@ func NewStore(storeKey, storePath string, provable bool) (modules.Store, error) 
 		return &PrivateStore{db, storeKey}, nil
 	}
 	// Create a new SMT with no value hasher to store the unhashed value bytes in the tree
-	tree := smt.NewSparseMerkleTree(db, sha256.New(), smt.WithValueHasher(nil))
+	tree := smt.NewSparseMerkleTree(db, sha256.New(), noValueHasher)
 	return &ProvableStore{db, tree, storeKey}, nil
+}
+
+// PopulateStore populates a store with the data provided by iterating through the map,
+// applying the store prefix to the paths and setting the value in the store to this key
+func PopulateStore(store modules.Store, data map[string][]byte) error {
+	for path, value := range data {
+		prefix := &coreTypes.CommitmentPrefix{Prefix: []byte(store.GetStoreKey())}
+		key := host.ApplyPrefix(prefix, path).GetPath()
+		if err := store.Set(key, value); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // GetStore retrieves a Store instance from the StoreManager
