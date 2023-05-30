31a421188fccae5c05f877484dd480875f653eb0
Harry Law <53987565+h5law@users.noreply.github.com>
2023-05-30 14:02:16 +0100

    Consoslidate verification helper function

--- ibc/proofs.go
+++ ibc/proofs.go
@@ -10,32 +10,21 @@ import (
 // defaultValue is the default placeholder value in a SparseMerkleTree
 var defaultValue []byte = nil
 
-// validateMembershipProof validates the proof for membership by doing basic validation
-// For a MembershipProof, the key-value pair should match the proof's embedded key-value
-// pair and the NonMembershipLeafData should be nil.
-func validateMembershipProof(proof *coreTypes.CommitmentProof, key, value []byte) error {
+// validateProof validates the proof for membership by doing basic validation. The key-value
+// pair should match the proof's embedded key-value pair and for a MembershipProof the
+// NonMembershipLeafData should be nil but for a NonMembershipProof, the value should be
+// nil and the NonMembershipLeafData should not be nil
+func validateProof(nonmembership bool, proof *coreTypes.CommitmentProof, key, value []byte) error {
 	if !bytes.Equal(proof.GetKey(), key) {
 		return coreTypes.ErrInvalidProof("provided key does not match proof key")
 	}
 	if !bytes.Equal(proof.GetValue(), value) {
 		return coreTypes.ErrInvalidProof("provided value does not match proof value")
 	}
-	if proof.NonMembershipLeafData != nil {
+	if !nonmembership && proof.NonMembershipLeafData != nil {
 		return coreTypes.ErrInvalidProof("non membership leaf data must be nil")
 	}
-	return nil
-}
-
-// validateNonMembershipProof validates the proof for non-membership by doing basic validation
-// For a NonMembershipProof, the value should be nil and the NonMembershipLeafData should not be nil
-func validateNonMembershipProof(proof *coreTypes.CommitmentProof, key []byte) error {
-	if !bytes.Equal(proof.GetKey(), key) {
-		return coreTypes.ErrInvalidProof("provided key does not match proof key")
-	}
-	if !bytes.Equal(proof.GetValue(), defaultValue) {
-		return coreTypes.ErrInvalidProof("default value does not match proof value")
-	}
-	if proof.NonMembershipLeafData == nil {
+	if nonmembership && proof.NonMembershipLeafData == nil {
 		return coreTypes.ErrInvalidProof("non membership leaf data must not be nil")
 	}
 	return nil
@@ -57,7 +46,7 @@ func verifyCommitmentProof(spec *smt.TreeSpec, proof *coreTypes.CommitmentProof,
 // and then rebuilds the root hash of the tree. If the root hash matches the one provided, the
 // key-value pair is contained in the tree.
 func VerifyMembership(spec *smt.TreeSpec, proof *coreTypes.CommitmentProof, root, key, value []byte) (bool, error) {
-	if err := validateMembershipProof(proof, key, value); err != nil {
+	if err := validateProof(false, proof, key, value); err != nil {
 		return false, err
 	}
 	return verifyCommitmentProof(spec, proof, root, key, value)
@@ -69,7 +58,7 @@ func VerifyMembership(spec *smt.TreeSpec, proof *coreTypes.CommitmentProof, root
 // position of the provided key in the tree. If the root hash matches the one provided, the
 // key is not contained in the tree.
 func VerifyNonMembership(spec *smt.TreeSpec, proof *coreTypes.CommitmentProof, root, key []byte) (bool, error) {
-	if err := validateNonMembershipProof(proof, key); err != nil {
+	if err := validateProof(true, proof, key, defaultValue); err != nil {
 		return false, err
 	}
 	return verifyCommitmentProof(spec, proof, root, key, defaultValue)
