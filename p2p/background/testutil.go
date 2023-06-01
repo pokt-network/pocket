0d49f29ffb8be4786e7d4cf89555b824c0e90623
Bryan White <bryanchriswhite@gmail.com>
2023-06-01 11:33:48 +0200

    wip bg router testing

--- p2p/background/testutil.go
+++ p2p/background/testutil.go
@@ -2,5 +2,25 @@
 
 package background
 
+import (
+	"github.com/pokt-network/pocket/internal/testutil/generics"
+	typesP2P "github.com/pokt-network/pocket/p2p/types"
+	"github.com/regen-network/gocuke"
+)
+
 // BackgroundRouter exports `backgroundRouter` for testing purposes.
 type BackgroundRouter = backgroundRouter
+
+// TOOD_THIS_COMMIT: move & dedup
+type routerHandlerProxyFactory = generics_testutil.ProxyFactory[typesP2P.RouterHandler]
+
+func (rtr *backgroundRouter) HandlerProxy(
+	t gocuke.TestingT,
+	handlerProxyFactory routerHandlerProxyFactory,
+) {
+	t.Helper()
+
+	// pass original handler to proxy factory & replace it with the proxy
+	rtr.handler = handlerProxyFactory(rtr.handler)
+}
+
