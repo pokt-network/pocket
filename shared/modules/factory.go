3a8e138997eebd70e470c1787690dcf823bdb33a
Bryan White <bryanchriswhite@gmail.com>
2023-06-08 15:46:05 +0200

    refactor: rename `T` & `K` type params to `M` &`C`

--- shared/modules/factory.go
+++ shared/modules/factory.go
@@ -5,30 +5,30 @@ package modules
 type ModuleFactoryWithOptions FactoryWithOptions[Module, ModuleOption]
 
 // Factory implements a `#Create()` factory method which takes a bus and returns
-// a value of type T and an error.
-type Factory[T any] interface {
-	Create(bus Bus) (T, error)
+// a value of type M and an error.
+type Factory[M any] interface {
+	Create(bus Bus) (M, error)
 }
 
 // FactoryWithConfig implements a `#Create()` factory method which takes a bus and
-// a required "config" argument of type K and returns a value of type T and an error.
+// a required "config" argument of type C and returns a value of type M and an error.
 // TECHDEBT: apply enforcement across applicable "sub-modules" (see: `p2p/raintree/router.go`: `raintTreeFactory`)
-type FactoryWithConfig[T any, K any] interface {
-	Create(bus Bus, cfg K) (T, error)
+type FactoryWithConfig[T any, C any] interface {
+	Create(bus Bus, cfg C) (T, error)
 }
 
 // FactoryWithOptions implements a `#Create()` factory method which takes a bus
-// and a variadic "optional" argument(s) of type O and returns a value of type T
+// and a variadic "optional" argument(s) of type O and returns a value of type M
 // and an error.
 // TECHDEBT: apply enforcement across applicable "sub-modules"
-type FactoryWithOptions[T any, O any] interface {
-	Create(bus Bus, opts ...O) (T, error)
+type FactoryWithOptions[M any, O any] interface {
+	Create(bus Bus, opts ...O) (M, error)
 }
 
 // FactoryWithConfigAndOptions implements a `#Create()` factory method which takes
-// a bus, a required "config" argument of type K, and a variadic (optional)
-// argument(s) of type O and returns a value of type T and an error.
+// a bus, a required "config" argument of type C, and a variadic (optional)
+// argument(s) of type O and returns a value of type M and an error.
 // TECHDEBT: apply enforcement across applicable "sub-modules"
-type FactoryWithConfigAndOptions[T any, K any, O any] interface {
-	Create(bus Bus, cfg K, opts ...O) (T, error)
+type FactoryWithConfigAndOptions[M any, C any, O any] interface {
+	Create(bus Bus, cfg C, opts ...O) (M, error)
 }
