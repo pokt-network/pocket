84e994db901323d72d4e174ecc71d6c9a989352e
shakezula <lott.dylan@gmail.com>
2023-06-08 18:03:37 -0600

    add stubbed out test case

---
+++ persistence/trees/trees_test.go
@@ -0,0 +1,18 @@
+package trees
+
+import "testing"
+
+func TestTreeStore_Update(t *testing.T) {
+	// TODO: Write test case for the Update method
+	t.Skip("TODO: Write test case for Update method")
+}
+
+func TestNewStateTrees(t *testing.T) {
+	// TODO: Write test case for the NewStateTrees function
+	t.Skip("TODO: Write test case for NewStateTrees function")
+}
+
+func TestTreeStore_DebugClearAll(t *testing.T) {
+	// TODO: Write test case for the DebugClearAll method
+	t.Skip("TODO: Write test case for DebugClearAll method")
+}
