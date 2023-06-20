aa56b42e423cfdcf77345f3ed2b0e4b7046707d9
Harry Law <53987565+h5law@users.noreply.github.com>
2023-06-20 16:41:11 +0100

    Allow conversion of IbcMessage to Transaction

--- ibc/messages.go
+++ ibc/messages.go
@@ -1,8 +1,13 @@
 package ibc
 
 import (
+	"fmt"
+
 	"github.com/pokt-network/pocket/ibc/types"
+	"github.com/pokt-network/pocket/shared/codec"
 	coreTypes "github.com/pokt-network/pocket/shared/core/types"
+	"github.com/pokt-network/pocket/shared/crypto"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 func CreateUpdateStoreMessage(prefix coreTypes.CommitmentPrefix, key, value []byte) *types.IbcMessage {
@@ -26,4 +31,24 @@ func CreatePruneStoreMessage(prefix coreTypes.CommitmentPrefix, key []byte) *typ
 			},
 		},
 	}
+}
+
+func ConvertIBCMessageToTx(ibcMessage *types.IbcMessage) (*coreTypes.Transaction, error) {
+	var anyMsg *anypb.Any
+	var err error
+	switch event := ibcMessage.Event.(type) {
+	case *types.IbcMessage_Update:
+		anyMsg, err = codec.GetCodec().ToAny(event.Update)
+	case *types.IbcMessage_Prune:
+		anyMsg, err = codec.GetCodec().ToAny(event.Prune)
+	default:
+		return nil, coreTypes.ErrUnknownIBCMessageType(fmt.Sprintf("%T", event))
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &coreTypes.Transaction{
+		Msg:   anyMsg,
+		Nonce: fmt.Sprintf("%d", crypto.GetNonce()),
+	}, nil
 }
