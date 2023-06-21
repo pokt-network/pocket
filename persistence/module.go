80971f81be1d852cb1ccf10ff51cc4f742be49e7
shakezula <lott.dylan@gmail.com>
2023-06-21 16:51:38 -0600

    remove dead code

--- persistence/module.go
+++ persistence/module.go
@@ -235,10 +235,6 @@ func (m *persistenceModule) GetTxIndexer() indexer.TxIndexer {
 	return m.txIndexer
 }
 
-func (m *persistenceModule) GetTreeStore() modules.TreeStoreModule {
-	return m.stateTrees
-}
-
 func (m *persistenceModule) GetNetworkID() string {
 	return m.networkId
 }
