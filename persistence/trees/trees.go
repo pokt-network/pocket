b16842965f2fe84b5401e19645b8ca163d0ed42e
harry <53987565+h5law@users.noreply.github.com>
2023-06-22 22:41:07 +0100

    Fix leftover import

--- persistence/trees/trees.go
+++ persistence/trees/trees.go
@@ -12,7 +12,6 @@ import (
 	"log"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/pokt-network/pocket/logger"
 	"github.com/pokt-network/pocket/persistence/indexer"
 	"github.com/pokt-network/pocket/persistence/kvstore"
 	"github.com/pokt-network/pocket/persistence/sql"
