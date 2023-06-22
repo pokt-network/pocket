2cf0ff29a2668b1bf3a50cf48caccbf46a626c89
harry <53987565+h5law@users.noreply.github.com>
2023-06-22 21:50:32 +0100

    Add new issue comments

--- ibc/host.go
+++ ibc/host.go
@@ -1,3 +1,8 @@
+// TECHDEBT(#854): The host should be a submodule with access to the bus, this allows for the
+// host to access the persistence module and thus the tree store in order to create local
+// copies of the IBC state tree with the GetProvableStore() function. Bus access also will
+// allow for the host to send any local changes to these stores as IbcMessage types through
+// the P2P module's Broadcast() function to allow them to be included in the next block.
 package ibc
 
 import (
@@ -18,7 +23,10 @@ func (h *host) GetTimestamp() uint64 {
 	return uint64(time.Now().Unix())
 }
 
-// GetProvableStore returns
+// GetProvableStore returns a copy of the IBC state tree where all operations happen
+// locally (in memory) and are not persisted to the database. All changes are instead
+// broadcasted to the network for inclusion in the next block.
+// TODO(#854): Implement this
 func (h *host) GetProvableStore(prefix coreTypes.CommitmentPrefix) (modules.ProvableStore, error) {
 	return nil, nil
 }
