06566581aef0ab80b2f4ff87845e75936e914633
harry <53987565+h5law@users.noreply.github.com>
2023-06-27 09:20:53 +0100

    Update next error comment

--- shared/core/types/error.go
+++ shared/core/types/error.go
@@ -38,7 +38,7 @@ func NewError(code Code, msg string) Error {
 	}
 }
 
-// NextCode: 141
+// NextCode: 142
 type Code float64 // CONSIDERATION: Should these be a proto enum or a golang iota?
 
 //nolint:gosec // G101 - Not hard-coded credentials
