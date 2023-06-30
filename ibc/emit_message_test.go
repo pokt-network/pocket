c12c42e6bff118f1169be8dd61ee4ac039704510
harry <53987565+h5law@users.noreply.github.com>
2023-06-30 22:35:59 +0100

    Add emit update store message event test

--- ibc/emit_message_test.go
+++ ibc/emit_message_test.go
@@ -2,8 +2,21 @@ package ibc
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestEmitMessage_MessageAddedToLocalMempool(t *testing.T) {
-	t.Skip()
+	_, _, utilityMod, _, ibcMod := prepareEnvironment(t, 1, 0, 0, 0)
+	ibcHost := ibcMod.GetBus().GetIBCHost()
+
+	// update store
+	store, err := ibcHost.GetProvableStore("test")
+	require.NoError(t, err)
+
+	err = store.Set([]byte("key"), []byte("value"))
+	require.NoError(t, err)
+
+	mempool := utilityMod.GetMempool()
+	require.Len(t, mempool.GetAll(), 1)
 }
