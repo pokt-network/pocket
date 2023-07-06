f886e03daf4de2b3f6cbd2fb34437568e787f1e3
Daniel Olshansky <olshansky.daniel@gmail.com>
2023-07-06 13:25:56 -0700

    Update report message

--- build/linters/blockers.go
+++ build/linters/blockers.go
@@ -15,7 +15,7 @@ func BlockInThisCommitComment(m dsl.Matcher) {
 	if !isFileExcludedForInThisComment(m) {
 		m.Match(`//$text`).
 			Where(m["text"].Text.Matches(`IN_THIS_COMMIT`)).
-			Report(`Don't use IN_THIS_COMMIT in comments`)
+			Report(`IN_THIS_COMMIT comment must be addressed before merging to main`)
 	}
 }
 
@@ -23,7 +23,7 @@ func BlockInThisPRComment(m dsl.Matcher) {
 	if !isFileExcludedForInThisComment(m) {
 		m.Match(`//$text`).
 			Where(m["text"].Text.Matches(`IN_THIS_PR`)).
-			Report(`Don't use IN_THIS_PR in comments`)
+			Report(`IN_THIS_PR comment must be addressed before merging to main`)
 	}
 }
 
