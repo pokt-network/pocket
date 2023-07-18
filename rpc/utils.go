b7016aa25cdcc45ea3ef27e5dc66018424a86c28
harry <53987565+h5law@users.noreply.github.com>
2023-07-18 19:13:28 +0100

    Add RPC support for updated block header

--- rpc/utils.go
+++ rpc/utils.go
@@ -431,7 +431,9 @@ func (s *rpcServer) blockToRPCBlock(protoBlock *coreTypes.Block) (*Block, error)
 				},
 				Transactions: qcTxs,
 			},
-			Timestamp: protoBlock.BlockHeader.GetTimestamp().AsTime().String(),
+			Timestamp:       protoBlock.BlockHeader.GetTimestamp().AsTime().String(),
+			StateTreeHashes: BlockHeader_StateTreeHashes{protoBlock.BlockHeader.GetStateTreeHashes()},
+			NextValSetHash:  protoBlock.BlockHeader.GetNextValSetHash(),
 		},
 		Transactions: txs,
 	}, nil
