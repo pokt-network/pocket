4ae3912c4d28459c1a6f2b2c9a83d53cad82dd2b
Daniel Olshansky <olshansky@pokt.network>
2023-07-19 16:35:17 -0700

    [Temp] s/TreeStore/treeStore (#920)
    Small commit to reduce visibility of a type for in progress PR

--- persistence/trees/module_test.go
+++ persistence/trees/module_test.go
@@ -142,6 +142,7 @@ func validatorId(i int) string {
 func createMockBus(t *testing.T, runtimeMgr modules.RuntimeMgr) *mockModules.MockBus {
 	t.Helper()
 	ctrl := gomock.NewController(t)
+
 	mockBus := mockModules.NewMockBus(ctrl)
 	mockModulesRegistry := mockModules.NewMockModulesRegistry(ctrl)
 
@@ -170,6 +171,5 @@ func createMockBus(t *testing.T, runtimeMgr modules.RuntimeMgr) *mockModules.Moc
 	mockBus.EXPECT().
 		PublishEventToBus(gomock.Any()).
 		AnyTimes()
-
 	return mockBus
 }
