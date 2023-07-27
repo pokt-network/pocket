bfc57eb89907932831b5730c68a09b4656c420c2
harry <53987565+h5law@users.noreply.github.com>
2023-07-27 09:36:21 +0100

    [Utility] Use TreeStore as source of truth (#937)

--- shared/modules/treestore_module.go
+++ shared/modules/treestore_module.go
@@ -31,6 +31,9 @@ type TreeStoreModule interface {
 	Update(pgtx pgx.Tx, height uint64) (string, error)
 	// DebugClearAll completely clears the state of the trees. For debugging purposes only.
 	DebugClearAll() error
+	// Prove generates and verifies a proof against the tree with the matching name using the given
+	// key and value. If value == nil, it will verify non-membership of the key, otherwise membership.
+	Prove(treeName string, key, value []byte) (bool, error)
 	// GetTree returns the specified tree's root and nodeStore in order to be imported elsewhere
 	GetTree(name string) ([]byte, kvstore.KVStore)
 	// GetTreeHashes returns a map of tree names to their root hashes
