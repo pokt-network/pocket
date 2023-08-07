9a6b89dfe8f321a121745492d2f8487ba1529f28
shakezula <lott.dylan@gmail.com>
2023-08-07 16:22:12 -0600

    [wip] gets save rpc test working

--- rpc/handlers_test.go
+++ rpc/handlers_test.go
@@ -1,29 +1,39 @@
 package rpc
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 
-	"github.com/labstack/echo/v4"
 	"github.com/pokt-network/pocket/internal/testutil"
 	"github.com/pokt-network/pocket/logger"
 	"github.com/pokt-network/pocket/runtime/test_artifacts"
 	"github.com/pokt-network/pocket/shared/modules"
+	"github.com/stretchr/testify/require"
+
+	"github.com/labstack/echo/v4"
 )
 
 func Test_RPCPostV1NodeBackup(t *testing.T) {
-	tests := []struct {
+	// THIS WORKS BUT ADJUST LATER
+	type testCase struct {
 		name    string
 		setup   func(t *testing.T, e echo.Context) *rpcServer
+		assert  func(t *testing.T, tt testCase, e echo.Context, s *rpcServer)
 		wantErr bool
-	}{
+	}
+
+	var testDir = t.TempDir()
+
+	tests := []testCase{
 		{
-			name: "should create a backup in the default directory",
+			name: "should create a backup in the specified directory",
 			setup: func(t *testing.T, e echo.Context) *rpcServer {
 				_, _, url := test_artifacts.SetupPostgresDocker()
 				pmod := testutil.NewTestPersistenceModule(t, url)
-				// context := testutil.NewTestPostgresContext(t, pmod, 0)
 
 				s := &rpcServer{
 					logger: *logger.Global.CreateLoggerForModule(modules.RPCModuleName),
@@ -31,16 +41,42 @@ func Test_RPCPostV1NodeBackup(t *testing.T) {
 
 				s.SetBus(pmod.GetBus())
 
+				e.SetParamNames("dir")
+				e.SetParamValues(testDir)
+
 				return s
+			},
+			wantErr: false,
+			assert: func(t *testing.T, tt testCase, e echo.Context, s *rpcServer) {
+				empty, err := isEmpty(testDir)
+				require.NoError(t, err)
+				require.False(t, empty)
+				f, err := os.Open(testDir)
+				require.NoError(t, err)
+				dirs, err := f.ReadDir(-1)
+				require.NoError(t, err)
+				require.True(t, len(dirs) == 12)
+
+				// assert worldstate json was written
+				_, err = os.Open(filepath.Join(testDir, "worldstate.json"))
+				require.NoError(t, err)
+
+				// assert blockstore was written
+				_, err = os.Open(filepath.Join(testDir, "blockstore.bak"))
+				require.NoError(t, err)
+
+				// cleanup the directory after each test
+				t.Cleanup(func() {
+					require.NoError(t, os.RemoveAll(testDir))
+				})
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// create a new echo Context for each test
-			tempDir := t.TempDir()
 			e := echo.New()
-			req := httptest.NewRequest(http.MethodPost, tempDir, nil)
+			req := httptest.NewRequest(http.MethodPost, "/v1/node/backup", nil)
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
 
@@ -51,6 +87,22 @@ func Test_RPCPostV1NodeBackup(t *testing.T) {
 			if err := s.PostV1NodeBackup(c); (err != nil) != tt.wantErr {
 				t.Errorf("rpcServer.PostV1NodeBackup() error = %v, wantErr %v", err, tt.wantErr)
 			}
+			tt.assert(t, tt, c, s)
 		})
 	}
+}
+
+// TECHDEBT(#796) - Organize and dedupe this function into testutil package
+func isEmpty(dir string) (bool, error) {
+	f, err := os.Open(dir)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	_, err = f.Readdirnames(1) // Or f.Readdir(1)
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err // Either not empty or error, suits both cases
 }
