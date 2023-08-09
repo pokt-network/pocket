bbb594f652ce175c5ce412c7e1d5158ae5d29470
Arash Deshmeh <adshmh@gmail.com>
2023-08-09 10:12:59 -0400

    Move actors shared modules to independent files

---
+++ shared/modules/fisherman_module.go
@@ -0,0 +1,5 @@
+package modules
+
+type FishermanModule interface {
+	Module
+}
--- shared/modules/utility_module.go
+++ shared/modules/utility_module.go
@@ -58,14 +58,6 @@ type UtilityModule interface {
 	GetValidatorModule() (ValidatorModule, error)
 }
 
-type FishermanModule interface {
-	Module
-}
-
-type ValidatorModule interface {
-	Module
-}
-
 // TECHDEBT: Remove this interface from `shared/modules` and use the `Actor` protobuf type instead
 // There will need to be some documentation or indicator that the Actor struct returned may not be
 // fully hydrated. Alternatively, we could eat the performance cost and just hydrate the entire struct
